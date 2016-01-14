ac433549be15a10673886d28a0a92b1060484b83
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-01-14 17:47:43 +0100

    play with shallow string

--- course2/meeting5/shallow_string.cc
+++ course2/meeting5/shallow_string.cc
@@ -29,7 +29,7 @@ public:
 
 	const char& operator[](int i) const { return mStr[i]; }
 
-	//char& operator[](int i) { return mStr[i]; }
+	char& operator[](int i) { return const_cast<char&>(mStr[i]); }
 
 private:
 
@@ -40,7 +40,7 @@ int main()
 {
 	//char buffer[] = "abcdefg";
 
-	ShallowString s1("abcdef");
+	ShallowString s1("12345");
 
 	
 	assert(s1[0] == 'a');
@@ -49,7 +49,7 @@ int main()
 
 	ShallowString s2 = s1;
 
-	s1[0] = 'x';
+	//s1[0] = 'x';
 
 	std::cout << "s1=" << s1 << std::endl;
 	std::cout << "s2=" << s2 << std::endl;
