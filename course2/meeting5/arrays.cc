e5d57c138a14d7e911c21696f0a5fd2b5d448a98
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-01-14 17:15:38 +0100

    party trick

---
+++ course2/meeting5/arrays.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cassert>
+
+int main()
+{
+    //double* array = new double[3]; // double*
+    
+    
+    //const double* ptr = array;
+    //ptr[0] = 7;
+    
+    //std::cout << "sizeof(array) = " <<sizeof(array) << ", sizeof(ptr)=" << sizeof(ptr) << std::endl;
+    //std::cout << "elements in array: " << (sizeof(array)/sizeof(array[0])) << std::endl;
+    
+    std::cout << "x=" << 2["hello"] << std::endl;
+}
