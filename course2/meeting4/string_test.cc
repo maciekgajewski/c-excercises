b6a7d5d219b162a421460ba092e6fb5b0ff0eb46
matthijs <matthijs.snel@gmail.com>
2016-01-17 22:54:24 +0100

    Done

--- course2/meeting4/string_test.cc
+++ course2/meeting4/string_test.cc
@@ -1,7 +1,10 @@
 #include <string>
+#include <cstring>
 #include <cassert>
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <algorithm>
 
 template<typename StringType>
 void basic_test()
@@ -92,14 +95,111 @@ void concatenation_operator()
 	assert(s3 == "aabbbaa");
 }
 
+class CustomString
+{
+public:
+	CustomString() {}
+
+	CustomString(const char* str) {
+		copyCharArray(str);
+	}
+
+	CustomString(std::vector<char> str) {
+		mChars = str;
+	}
+
+	int length() { return mChars.size(); }
+
+	/* operators on C-style strings */
+	CustomString& operator=(const char* str) {
+		mChars.clear();
+		copyCharArray(str);
+		return *this;
+	}
+
+	bool operator==(const char* str) {
+		return compareCharArray(str);
+	}
+	bool operator!=(const char* str) {
+		return !compareCharArray(str);
+	}
+
+	/* operators on CustomStrings */
+	CustomString& operator=(const CustomString& str) {
+		mChars = str.mChars;
+		return *this;
+	}
+	const CustomString operator+(const CustomString& str2) {
+		CustomString r = *this;
+		r += str2;
+		return r;
+	}
+	CustomString& operator+=(const CustomString& str) {
+		mChars.insert(mChars.end(), str.mChars.begin(), str.mChars.end());
+		return *this;
+	}
+	bool operator==(const CustomString& str) {
+		return mChars == str.mChars;
+	}
+	bool operator!=(const CustomString& str) {
+		return mChars != str.mChars;
+	}
+	bool operator<(const CustomString& str) {
+		return mChars < str.mChars;
+	}
+	char& operator[](int i) {
+		return mChars[i];
+	}
+
+	friend std::stringstream& operator<<(std::stringstream& os, const CustomString& str);
+
+
+private:
+	std::vector<char> mChars;
+
+	void copyCharArray(const char* str) {
+		for ( int i = 0; str[i] != '\0'; i++ ) {
+			mChars.push_back(str[i]);
+		}
+	}
+
+	bool compareCharArray(const char* str) {
+		//unsigned to make compiler warning go away
+		unsigned int len = std::strlen(str);
+		if ( len != mChars.size() ) {
+			return false;
+		}
+		for ( unsigned int i = 0; i < len; i++ ) {
+			if ( mChars[i] != str[i] ) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+};
+
+std::stringstream& operator<<(std::stringstream& stream, const CustomString& str) {
+	for (std::vector<char>::const_iterator it = str.mChars.begin(); it != str.mChars.end(); ++it) {
+		stream << *it;
+	}
+	return stream;
+}
+
 int main()
 {
-	basic_test<std::string>();
-	basic_operators<std::string>();
-	indexing_operator<std::string>();
-	stream_output_operator<std::string>();
-	concatenation_operator<std::string>();
-	std::cout << "std::string tests passed" << std::endl;
+//	basic_test<std::string>();
+//	basic_operators<std::string>();
+//	indexing_operator<std::string>();
+//	stream_output_operator<std::string>();
+//	concatenation_operator<std::string>();
+//	std::cout << "std::string tests passed" << std::endl;
 	
 	// TODO after implementing your string class, run the selected functions for your type here
+	basic_test<CustomString>();
+	basic_operators<CustomString>();
+	indexing_operator<CustomString>();
+	stream_output_operator<CustomString>();
+	concatenation_operator<CustomString>();
+	std::cout << "CustomString tests passed" << std::endl;
 }
