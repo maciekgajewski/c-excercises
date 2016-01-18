03d563fc80950f522f24e987ae0d7543f9c42802
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-01-18 17:12:36 +0100

    templated vector

--- course2/meeting6/vec.cc
+++ course2/meeting6/vec.cc
@@ -1,14 +1,9 @@
 #include <iostream>
 
+template<typename T>
 class Vec
 {
 public:
-	//Vec(double x, double y) : mX(x), mY(y) {}
-	//Vec() = default;
-
-	//double GetX() const { return mX; }
-	//double GetY() const { return mY; }
-
 	bool operator == (const Vec& o) const
 	{
 		return x == o.x && y == o.y;
@@ -19,13 +14,19 @@ public:
 		return Vec{x + o.x, y + o.y};
 	}
 
-	double x;
-	double y;
+	T x;
+	T y;
 };
+
+template<typename T>
+std::ostream& operator<<(std::ostream& o, const Vec<T>& vec)
+{
+	return o << "(" << vec.x << ", " << vec.y << ")";
+}
 
 int main()
 {
-	Vec v = Vec{1,1} + Vec{4,5};
+	Vec<int> v = Vec<int>{1,1} + Vec<int>{4,5};
 
-	std::cout << "v=" << v.x << ", " << v.y << std::endl;
+	std::cout << "v=" << v  << std::endl;
 }
