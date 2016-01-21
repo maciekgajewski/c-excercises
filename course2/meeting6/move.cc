f035df9888512c68eed3baf22ec62888ae926280
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-01-21 16:07:36 +0100

    move added

---
+++ course2/meeting6/move.cc
@@ -0,0 +1,12 @@
+#include "string.hh"
+
+
+int main()
+{
+    MyString a = "a";
+    MyString b = "b";
+    MyString ab = a+b;
+    std::cout << ab << std::endl;
+    ab = b+a;
+    std::cout << ab << std::endl;
+}
