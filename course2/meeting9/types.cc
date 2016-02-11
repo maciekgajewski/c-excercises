3584cec0cd19b841ab3c59bde8620ea7c71bee2a
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-02-11 17:20:42 +0100

    avg works for all containers

--- course2/meeting9/types.cc
+++ course2/meeting9/types.cc
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
-
-int avg(const std::vector<int>& data)
+template<typename Container>
+typename Container::value_type avg(const Container& data)
 {
-    int total = 0;
-    for(int i : data)
+    auto total = typename Container::value_type();
+    for(auto i : data)
         total += i;
     return total / data.size();
 }
 
 int main()
 {
-    std::vector<int> data = {1, 4, 5, 3, 66 ,7 ,8 };
     
-    std::cout << "average=" << avg(data) <<  std::endl;
+    
+    
+    
+    std::vector<const int> data = {1, 4, 5, 3, 66 ,7 ,8 };
+    std::vector<float> dataFloat = {1.3, 4.4, 5, 3.14, 6.6 ,7 ,8 };
+    
+    std::list<double> dataDouble = { 1.2, 4.5, 6.7};
+    
+    //std::cout << "average int=" << avg(data) <<  std::endl;
+    std::cout << "average float=" << avg(dataFloat) <<  std::endl;
+    std::cout << "average double=" << avg(dataDouble) <<  std::endl;
 }
