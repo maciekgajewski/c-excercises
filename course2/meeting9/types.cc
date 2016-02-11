77b600085255457c9cf2eedf2125c5c2e12659af
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-02-11 18:03:24 +0100

    lambdas used, and hopefully explained

--- course2/meeting9/types.cc
+++ course2/meeting9/types.cc
@@ -1,28 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <list>
+//#include <algorithm>
 
-template<typename Container>
-typename Container::value_type avg(const Container& data)
+
+void print(int i)
 {
-    auto total = typename Container::value_type();
-    for(auto i : data)
-        total += i;
-    return total / data.size();
+    std::cout << "i=" << i << std::endl;
+}
+void print2(int i, std::string s)
+{
+    std::cout << "i=" << i << ", s=" << s << std::endl;
+}
+
+struct Printer
+{
+    std::string boundS;
+    void operator() (int i) { print2(i, boundS); }
+};
+
+struct Counter
+{
+    int& total;
+    void operator() (int i) { total += i;}
+};
+
+template <typename it, typename action>
+void for_each(it begin, it end, action act)
+{
+   for(it i=begin; i != end; ++i)
+   {
+      act(*i);   
+   }
 }
 
 int main()
 {
+    std::vector<int> data = {1, 2, 3, 5, 7, 8};
     
+    for_each(data.begin(), data.end(),
+                    &print);
     
+    Printer p{"hello"};
     
+    for_each(data.begin(), data.end(),
+                    p);
+
+    int t = 0;
+    Counter c{t};
+    for_each(data.begin(), data.end(),
+                    c);
+    std::cout << "t=" << t << std::endl;
     
-    std::vector<const int> data = {1, 4, 5, 3, 66 ,7 ,8 };
-    std::vector<float> dataFloat = {1.3, 4.4, 5, 3.14, 6.6 ,7 ,8 };
+    std::cout << "total=" << c.total << std::endl;
+ 
+    int total = 0;
+    auto lambda = [&total](int i) { total += i; };
+    for_each(data.begin(), data.end(),
+            lambda);
     
-    std::list<double> dataDouble = { 1.2, 4.5, 6.7};
+    std::cout << "total from lambda=" << total << std::endl;
     
-    //std::cout << "average int=" << avg(data) <<  std::endl;
-    std::cout << "average float=" << avg(dataFloat) <<  std::endl;
-    std::cout << "average double=" << avg(dataDouble) <<  std::endl;
 }
