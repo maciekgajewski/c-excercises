8889ad38f22cc7ff6b100956f25b8076b2fb6449
matthijs <matthijs.snel@gmail.com>
2016-02-22 21:24:06 +0100

    Updated with PR comments

--- course2/meeting10/main.cc
+++ course2/meeting10/main.cc
@@ -5,7 +5,11 @@
 #include <deque>
 #include <iostream>
 #include <chrono>
-#include <functional>
+#include <ios>
+#include <iomanip>
+
+using namespace std::literals::chrono_literals;
+using floatMillis = std::chrono::duration<double, std::milli>;
 
 struct RNG
 {
@@ -14,15 +18,35 @@ struct RNG
 	double operator() () { return realDist(randEngine); }
 };
 
-void PrintTime(std::function<void()> func, const char* message)
+template <typename F>
+void PrintTime(F func, const char* message)
 {
 	std::chrono::time_point<std::chrono::system_clock> start, end;
 
-	start = std::chrono::system_clock::now();
-	func();
-	end = std::chrono::system_clock::now();
+	std::vector<std::chrono::nanoseconds> measurements(100);
 
-	std::cout << message << (end-start).count() << std::endl;
+	// warmup
+	for ( int i = 0; i < 10; i++ )
+		func();
+
+	//benchmark
+	for ( int i = 0; i < 100; i++ ) {
+		start = std::chrono::system_clock::now();
+		func();
+		end = std::chrono::system_clock::now();
+		measurements[i] = end-start;
+	}
+
+	std::nth_element(measurements.begin(), measurements.begin()+50, measurements.end());
+
+	std::streamsize prec = std::cout.precision();
+	std::cout << message << " Median: ";
+	std::cout << std::fixed << std::setprecision(3) << std::chrono::duration_cast<floatMillis>(measurements[50])/1ms;
+
+	std::nth_element(measurements.begin(), measurements.begin(), measurements.end());
+
+	std::cout << ", Min: " << std::chrono::duration_cast<floatMillis>(measurements[0])/1ms;
+	std::cout << std::setprecision(prec) << " ms" << std::endl;
 }
 
 
@@ -31,13 +55,22 @@ int main()
 	int N = 1000000;
 	RNG randomGen;
 
-	std::vector<double> v(N);
-	std::list<double> l(N,0);
-	std::deque<double> d(N,0);
+	std::vector<double> v;
+	std::list<double> l;
+	std::deque<double> d;
 
-	PrintTime([&]() {std::generate_n(v.begin(), N, randomGen);}, "Populate vector: ");
-	PrintTime([&]() {std::generate_n(l.begin(), N, randomGen);}, "Populate list  : ");
-	PrintTime([&]() {std::generate_n(d.begin(), N, randomGen);}, "Populate deque : ");
+	PrintTime([&]() {
+			v = std::vector<double>(N);
+			std::generate_n(v.begin(), N, randomGen);
+		}, "Populate vector: ");
+	PrintTime([&]() {
+			l = std::list<double>(N,0);
+			std::generate_n(l.begin(), N, randomGen);
+		}, "Populate list  : ");
+	PrintTime([&]() {
+			d = std::deque<double>(N,0);
+			std::generate_n(d.begin(), N, randomGen);
+		}, "Populate deque : ");
 
 	PrintTime([&]() {std::accumulate(v.begin(), v.end(), 0);}, "Sum vector: ");
 	PrintTime([&]() {std::accumulate(l.begin(), l.end(), 0);}, "Sum list  : ");
