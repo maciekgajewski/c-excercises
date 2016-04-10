f71630bab844189b8b44143b071a68c1fb3716c0
dan <dan@debian>
2016-04-10 15:44:25 +0000

    Vector homework with extra

--- course3/homework-vector/dcollier_vector.cc
+++ course3/homework-vector/dcollier_vector.cc
@@ -1,5 +1,7 @@
+// author: dancollier@optiver.com
 #include <iostream>
 #include <cassert>
+#include <cmath>
 
 using namespace std;
 
@@ -7,51 +9,57 @@ class Vector
 {
     // TODO
     public:
-	// default constructor
         Vector(){
-	     cout << "Default constructor, this=" << this << endl;
+	     std::cout << "Default constructor, this=" << this << std::endl;
 	};
-	//constructor
 	Vector(int argX, int argY) { 
     	     std::cout << "Constructor(x,y), this=" << this << std::endl; 
     	     x = argX;
              y = argY;
 	};
-        //destructor
 	~Vector(){
-             cout << "Freeing memory this=" << this << endl;
+             std::cout << "Freeing memory this=" << this << std::endl;
 	};
 
-	bool operator == ( Vector &vec){
+	bool operator == (Vector &vec){
 	    return ( this->x == vec.GetX() && this->y == vec.GetY() );
 	} 
-
-	bool operator != ( Vector &vec){
+	bool operator != (Vector &vec){
 	    if( this->x == vec.GetX() && this->y == vec.GetY() ){
 		return false;
 	    }
 	    else { return true; }
 	} 
-
-	Vector operator + ( Vector &vec){
+	Vector operator + (Vector &vec){
 	     int newX = this->x + vec.GetX() ;
 	     int newY = this->y + vec.GetY() ;	
 	     return Vector(newX,newY);
 	} 
-
-	friend ostream& operator<<( ostream& os, Vector &vec){
+	friend ostream& operator << ( ostream& os, Vector &vec){
 	     return os << "Vector(" << vec.GetX() << ", "  << vec.GetY() << ")";
 	}
 
-	int GetX(){ return x;};
-	int GetY(){ return y;};
+	double GetX(){ return x;};
+	double GetY(){ return y;};
         void SetX(int argX){ x = argX; }
-        void SetY(int argY){ y =argY; }
+        void SetY(int argY){ y = argY; }
+	double GetRadius(){ return Radius(x,y) ; };
+	double GetTheta(){ return Theta(x,y) ; };
 
    private:
 	int x;
 	int y;
 
+	double Radius(double x, double y){
+		double rad;
+		rad = sqrt((pow(x,2))+(pow(y,2)));
+		return rad;
+	}	
+	double Theta(double x, double y){
+		double tht;
+		tht = atan(y/x);
+		return tht;
+	}
 };
 
 int main()
@@ -78,4 +86,17 @@ int main()
     assert(v3.GetY() == 13);
     
     std::cout << v1 << " + " << v2 << " = " << v3 << std::endl;
+
+    // end test
+
+    std::cout << "Radius of " << v3 << " is " << v3.GetRadius() << std::endl;
+    std::cout << "Theta (\u03d1) of " << v3 << " is " << v3.GetTheta() << std::endl;
+
+    Vector testVec;
+    std::cout << "Radius of instantiated but unassigned vector " << testVec << " is " << testVec.GetRadius() << std::endl;
+    testVec.GetTheta();
+    Vector testVec2;
+    std::cout << "Radius of a 2nd instantiated but unassigned vector " << testVec2 << " is " << testVec2.GetRadius() << std::endl;
+    std::cout << "The value of X and the radius match on my machine despite values for Y that make that impossible. "<< std::endl;
+    
 }
