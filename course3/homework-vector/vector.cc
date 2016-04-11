a515c385783c7340bcdf6305f7e7cf712c06cb81
Williston Hayes <willistonhayes@optiver.com>
2016-04-11 19:19:15 +0200

    Added Vector class definition.

--- course3/homework-vector/vector.cc
+++ course3/homework-vector/vector.cc
@@ -1,9 +1,55 @@
 #include <iostream>
 #include <cassert>
+#include <cmath>
 
 class Vector
 {
-    // TODO
+    private:
+        int private_x, private_y;
+
+    public:
+        // required constructors
+        Vector(int x, int y): private_x(x), private_y(y) {}
+        Vector() {
+            private_x = 0;
+            private_y = 0;
+        }
+
+        int GetX() { return private_x; }
+        int GetY() { return private_y; }
+
+        void SetX(int x) { private_x = x; }
+        void SetY(int y) { private_y = y; }
+
+        float GetTheta() {
+            return std::atan2(private_y, private_x) * 180 / M_PI;
+        }
+
+        float GetR() {
+            return std::pow(std::pow(private_x, 2) + std::pow(private_y, 2), 0.5);
+        }
+
+        friend std::ostream& operator<< (std::ostream& stream, Vector v) {
+            stream << "{(" << v.GetX() << ", " << v.GetY() << "), (" << v.GetR() << "@" << v.GetTheta() << ")}";
+            return stream;
+        }
+
+        bool operator== (Vector rhs) {
+            if (private_x == rhs.GetX() && private_y == rhs.GetY())
+                return true;
+            return false;
+        }
+
+        bool operator!= (Vector rhs) {
+            return !operator==(rhs);
+        }
+
+        Vector operator+ (Vector rhs) {
+            Vector result;
+            result.SetX(private_x + rhs.GetX());
+            result.SetY(private_y + rhs.GetY());
+            return result;
+        }
 };
 
 int main()
