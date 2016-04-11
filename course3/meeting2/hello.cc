6a2555f140b3ca1bb557d29074eedb50b37a9f0f
Duncan Kimpton <dgkimpton@gmail.com>
2016-04-11 18:27:27 +0200

    Duncans first attempt at a simple string class

--- course3/meeting2/hello.cc
+++ course3/meeting2/hello.cc
@@ -1,13 +1,56 @@
 #include <iostream>
-#include <string>
 
+class SimpleString
+{
+  public:
+    SimpleString()
+    {
+    }
+
+    SimpleString(const char* ascii)
+    {
+      int lastCharacterIndex = BUFFER_SIZE - 1;
+
+      int i = 0;
+      while(i < lastCharacterIndex
+            && ascii[i] != 0)
+      {
+        mBuffer[i] = ascii[i];
+	++i;
+      }
+      mBuffer[i] = 0;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const SimpleString& that);
+    friend std::istream& operator>>(std::istream& is, SimpleString& target);
+
+  private:
+    static const int BUFFER_SIZE = 64;
+    char mBuffer[BUFFER_SIZE];
+};
+
+std::ostream& operator<<(std::ostream& os, const SimpleString& that)
+{
+  os << that.mBuffer;
+  return os;
+}
+
+std::istream& operator>>(std::istream& is, SimpleString& target)
+{
+  is.getline(target.mBuffer, target.BUFFER_SIZE - 1);
+  target.mBuffer[is.gcount()] = 0;
+  return is;
+}
 
 int main(int argc, char** argv)
 {
-    std::string name;
+//  std::cout << "Hi There" << std::endl;
+//  SimpleString s("Hi Back");
+//  std::cout << s << std::endl;
+
+    SimpleString name;
     std::cout << "Whats your name? > ";
     std::cin >> name;
     std::cout << "Hello, " << name << std::endl;
-    std::cout << "size of std::string is: " << sizeof(name) << std::endl;
-    
-} 
+    std::cout << "size of SimpleString is: " << sizeof(name) << std::endl;
+}
