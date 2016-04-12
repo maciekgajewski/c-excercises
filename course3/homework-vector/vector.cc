f14dc4d1c982b777b316da293eae9da3a9709ffd
Duncan Kimpton <dgkimpton@gmail.com>
2016-04-12 19:31:32 +0200

    working on the Vector homework, ran into trouble with the + operator

--- course3/homework-vector/vector.cc
+++ course3/homework-vector/vector.cc
@@ -3,8 +3,52 @@
 
 class Vector
 {
-    // TODO
+  public:
+    Vector():Vector(0,0)
+    {
+    }
+    Vector(Vector& that) : Vector(that.mX, that.mY)
+    {
+    }
+    Vector(int x, int y) : mX(x), mY(y) 
+    {
+    }
+    int GetX () const
+    {
+      return mX;
+    }
+    int GetY() const
+    {
+      return mY;
+    }
+    void SetY(int y)
+    {
+      mY = y;
+    }
+    void SetX(int x)
+    {
+      mX = x;
+    }
+
+    Vector& operator+(const Vector& that)
+    {
+      Vector result(mX + that.mX, mY + that.mY);
+      return result;
+    }
+
+  private:
+    int mX;
+    int mY;
 };
+
+bool operator==(const Vector& lhs, const Vector& rhs)
+{
+  return lhs.GetX() == rhs.GetX() && lhs.GetY() == rhs.GetY();
+}
+bool operator!=(const Vector& lhs, const Vector& rhs)
+{
+  return !(lhs == rhs);
+}
 
 int main()
 {
@@ -29,5 +73,5 @@ int main()
     assert(v3.GetX() == 10);
     assert(v3.GetY() == 13);
     
-    std::cout << v1 << " + " << v2 << " = " << v3 << std::endl;
+//    std::cout << v1 << " + " << v2 << " = " << v3 << std::endl;
 }
