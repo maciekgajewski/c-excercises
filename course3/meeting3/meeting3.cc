c4fae92226ce85352759dc41caf5fb9443dd1960
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-04-14 17:42:26 +0200

    const correctes 1

--- course3/meeting3/meeting3.cc
+++ course3/meeting3/meeting3.cc
@@ -1,33 +1,34 @@
 #include <iostream>
 
-namespace maciek
-{
-    
 class Vector
 {
-    int x = 0;
-    int y = 0;
+private:
+    int mX = 0;
+    int mY = 0;
+    
+public:
+    Vector() = default;
+    Vector(int x, int y) : mX(x), mY(y) {}
+    
+    int GetX() const { return mX; }
+    int GetY() const { return mY; }
+    void SetX(int x) { mX = x; }
+    
 };
 
-}
-
-
-namespace maciek
+std::ostream& operator<<(std::ostream& o, const Vector& v)
 {
-    class Square { int a; };
+    return o << v.GetX() << ", " << v.GetY();
 }
-
-namespace Optiver { namespace IML { namespace Eurex {
-
-    class Square { double f; };
-    
-}}}
+Vector operator+(const Vector& a, const Vector& o)
+{
+    return Vector(a.GetX()+o.GetX(), a.GetY()+o.GetY());
+}
 
 int main()
 {
-    using namespace maciek;
-    //std::cout 
-    Vector v;
-    Square s;
+    Vector v1(3, 66);
+    Vector v2(6, 7);
+    std::cout << (v1 + v2) << std::endl;
 }
 
