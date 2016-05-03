a583ed6ae568afa67eec9c14f81e8c3f9439ba7b
Igor Ustych <--global>
2016-05-03 13:33:16 +0200

    MyString initial implementation

--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,6 +1,54 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include <cstring>
 
 class MyString
 {
-	// TODO
+public:
+	MyString()	
+	{ 
+		//std::cout << "Default constructor";
+	}
+
+	MyString(MyString& other)
+	{
+		this->innerStorage = other.innerStorage;
+	}
+
+	MyString(const char* initialValue)
+	{
+		innerStorage.insert(
+			innerStorage.begin(),
+			initialValue,
+			initialValue + strlen(initialValue));
+	}
+
+	bool empty() const
+	{
+		return this->size() == 0;
+	}
+
+	int size() const
+	{
+		return innerStorage.size();
+	}
+
+	bool operator==(const MyString& other)
+	{
+    	return this->innerStorage == other.innerStorage;
+	}
+
+	bool operator!=(const MyString& other)
+	{
+		return !(*this == other);
+	}
+
+	char& operator[](int index)
+	{
+		return innerStorage[index];
+	}
+
+private:
+	std::vector<char> innerStorage; 
 };
