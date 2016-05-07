78d9b6acc292d932141bcd7f3201683f88ca52b1
Duncan Kimpton <dgkimpton@gmail.com>
2016-05-08 01:15:59 +0200

    String homework

--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <memory>
+
 class MyString
 {
-	// TODO
+  public:
+	MyString();
+	MyString(const MyString& that);
+	MyString(const char* str);
+	
+	bool empty() const;
+	int size() const;
+
+	bool operator==(const MyString& that) const;
+	bool operator!=(const MyString& that) const;
+	const char& operator[](int index) const;
+	char& operator[](int index);
+
+private:
+	MyString(const char* source, const int length);
+
+	int mLength;
+	std::unique_ptr<char[]> mBuffer;
 };
