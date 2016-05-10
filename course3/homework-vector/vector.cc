f09f5961e3e27369819bdc04f0d117905aae1c10
Karun Rao <karunrao97@gmail.com>
2016-05-10 21:45:13 +0200

    implement vector

--- course3/homework-vector/vector.cc
+++ course3/homework-vector/vector.cc
@@ -3,8 +3,44 @@
 
 class Vector
 {
-    // TODO
+private:
+    double mX;
+    double mY;
+
+public:
+    Vector() {}
+
+    Vector(double x, double y): mX(x), mY(y) {}
+
+    double GetX() const { return mX; }
+
+    double GetY() const { return mY; }
+
+    void SetX(double x) { mX = x; }
+
+    void SetY(double y) { mY = y; }
+
+    bool operator==(const Vector& vec) const
+    {
+        return mX == vec.mX && mY == vec.mY;
+    }
+
+    bool operator!=(const Vector& vec) const
+    {
+        return !operator==(vec);
+    }
+
+    Vector operator+(const Vector& vec) const
+    {
+        return Vector(mX + vec.mX, mY + vec.mY);
+    }
 };
+
+std::ostream& operator<<(std::ostream &out, const Vector& vec)
+{
+    out << "(" << vec.GetX() << "," << vec.GetY() << ")";
+    return out;
+}
 
 int main()
 {
