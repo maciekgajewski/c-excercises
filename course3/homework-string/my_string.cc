70fc2a2b7ac19ba0d94a9c94709e90cbea19cbd6
dan <dan@debian>
2016-05-10 18:31:41 +0000

    MyString Homework

---
+++ course3/homework-string/my_string.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstring>
+#include "my_string.h"
+
+MyString::MyString(){
+	numberOfChars = 0;
+}
+
+MyString::MyString(const char* theString){
+	numberOfChars = std::strlen(theString);
+	buffer = std::unique_ptr<char[]>(new char[numberOfChars]);
+	int i;
+	for (i = 0; i < numberOfChars; i++){
+		buffer[i] = theString[i];
+	}
+}
+
+MyString::MyString(const MyString& theString){
+	numberOfChars = theString.size();
+	buffer = std::unique_ptr<char[]>(new char[numberOfChars]);
+	int i;
+	for (i = 0; i < numberOfChars; i++){
+		buffer[i] = theString[i];
+	}
+}
+
+bool MyString::empty() const{
+	return numberOfChars == 0 ? true : false ;
+}
+
+
+int MyString::size() const{
+	return numberOfChars;
+}	
+
+
+const bool MyString::operator == (const MyString& theString){
+	return std::strcmp(buffer.get(), theString.buffer.get()) == 0 ? true : false;
+}
+
+const bool MyString::operator != (const MyString& theString){
+	return buffer == theString.buffer ? false : true ;
+}
+
+
+char& MyString::operator[](int index) const{
+	return buffer[index]; 
+}
+
--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,6 +1,29 @@
 #pragma once
+#include <memory>
 
 class MyString
 {
 	// TODO
+
+	public:
+		MyString();
+		
+		MyString(const char* theString);
+
+		MyString(const MyString& theString);
+
+		bool empty() const;
+
+		int size() const;	
+
+		const bool operator == (const MyString& theString);
+
+		const bool operator != (const MyString& theString);
+
+		char& operator[](int index) const;
+
+	private:
+
+     	int numberOfChars;
+	std::unique_ptr<char[]> buffer;
 };
