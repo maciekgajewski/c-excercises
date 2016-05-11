06e61454f85593ae2d5029a324e444fa79809c94
Igor Ustych <ustychigor@gmail.com>
2016-05-11 22:36:23 +0200

    string class implementation

--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,6 +1,60 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include <cstring>
 
 class MyString
 {
-	// TODO
+public:
+	MyString()	
+	{ 
+		// default constructor	
+	}
+
+	MyString(MyString& other)
+	{
+		this->innerStorage = other.innerStorage;
+	}
+
+	MyString(const char* initialValue)
+	{
+		innerStorage.insert(
+			innerStorage.begin(),
+			initialValue,
+			initialValue + strlen(initialValue));
+	}
+
+	bool empty() const
+	{
+		return this->size() == 0;
+	}
+
+	int size() const
+	{
+		return innerStorage.size();
+	}
+
+	bool operator==(const MyString& other)
+	{
+    	return this->innerStorage == other.innerStorage;
+	}
+
+	bool operator!=(const MyString& other)
+	{
+		return !(*this == other);
+	}
+
+	char& operator[](int index)
+	{
+		return innerStorage[index];
+	}
+	
+	char operator[](int index) const
+	{
+		return innerStorage[index];
+	}
+
+private:
+	std::vector<char> innerStorage; 
 };
+
