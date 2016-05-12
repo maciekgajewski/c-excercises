9bdbc1652935f69dca31c00220342fe2a1df1e33
Karun Rao <karunrao97@gmail.com>
2016-05-12 19:40:20 +0200

    add cc and fix bugs

--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,64 +1,25 @@
 #pragma once
 
-#include <cstring>
 #include <memory>
+
 
 class MyString
 {
-private:
-	std::unique_ptr<char[]> mBuffer;
 
 public:
-	MyString(): mBuffer(new char[1]) {}
+	MyString() = default;
+	MyString(const char* str);
+	MyString(const MyString& str);
 
-	MyString(const char* str): mBuffer(new char[std::strlen(str)])
-	{
-		std::strcpy(mBuffer.get(), str);
-	}
+	bool empty() const;
+	std::size_t size() const;
 
-	MyString(const MyString& str): mBuffer(new char[str.size()])
-	{
-		std::strcpy(mBuffer.get(), str.mBuffer.get());
-	}
+	bool operator==(const MyString& str) const;
+	bool operator!=(const MyString& str) const;
+	const char& operator[](int idx) const;
+	char& operator[](int idx);
 
-	bool empty() const
-	{
-		return size() == 0;
-	}
-
-	int size() const
-	{
-		return std::strlen(mBuffer.get());
-	}
-
-	bool operator==(const char* str) const
-	{
-		return std::strcmp(mBuffer.get(), str) == 0;
-	}
-
-	bool operator==(const MyString& str) const
-	{
-		return std::strcmp(mBuffer.get(), str.mBuffer.get()) == 0;
-	}
-
-	bool operator!=(const char* str) const
-	{
-		return !operator==(str);
-	}
-
-	bool operator!=(const MyString& str) const
-	{
-		return !operator==(str);
-	}
-
-	const char& operator[](int idx) const
-	{
-		return mBuffer.get()[idx];
-	}
-
-	char& operator[](int idx)
-	{
-		return mBuffer.get()[idx];
-	}
+private:
+	std::unique_ptr<char[]> mBuffer;
 
 };
