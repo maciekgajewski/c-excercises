08d244b1efb12703c9ce4deb178bd58de88807af
Williston Hayes <wshayes@gmail.com>
2016-05-16 14:23:17 +0200

    Added assignment and concatenation operators. Split out implementation from header file. Added exception handling to index operators.

---
+++ course3/homework-string/my_string.cc
@@ -0,0 +1,63 @@
+#include "my_string.h"
+#include <cstring>
+#include <stdexcept>
+
+MyString::MyString(const char* str) : buf(std::make_unique<char[]>(std::strlen(str)+1)) {
+	std::strcpy(buf.get(), str);
+}
+
+MyString::MyString(const MyString& obj) : buf(std::make_unique<char[]>(obj.size()+1)) {
+	std::strcpy(buf.get(), obj.buf.get());
+}
+
+bool MyString::operator==(const MyString& rhs) const {
+	if (!rhs.buf xor !buf)
+		return false;
+	if (!rhs.buf and !buf)
+		return true;
+	return std::strcmp(buf.get(), rhs.buf.get()) == 0;
+}
+
+bool MyString::operator!=(const MyString& rhs) const {
+	return !operator==(rhs);
+}
+
+const char& MyString::operator[](int i) const {
+	if (!buf)
+		throw std::range_error("Trying to index unallocated buffer.");
+	return buf.get()[i];
+}
+
+char& MyString::operator[](int i) {
+	if (!buf)
+		throw std::range_error("Trying to index unallocated buffer.");
+	return buf.get()[i];
+}
+
+MyString MyString::operator+(const MyString& rhs) const {
+	auto concat_string = std::make_unique<char[]>(size() + rhs.size() + 1);
+	std::strcpy(concat_string.get(), buf.get());
+	std::strcat(concat_string.get(), rhs.buf.get());
+	return MyString(concat_string.get());
+}
+
+void MyString::operator=(const MyString& rhs) {
+	buf.release();
+	if (rhs.buf) {
+		buf = std::make_unique<char[]>(rhs.size()+1);
+		std::strcpy(buf.get(), rhs.buf.get());
+	}
+	return;
+}
+
+bool MyString::empty() const {
+	if (!buf)
+		return true;
+	return buf[0] == 0;
+}
+
+int MyString::size() const {
+	if (!buf)
+		return 0;
+	return std::strlen(buf.get());
+}
--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,7 +1,5 @@
 #pragma once
-#include <iostream>
 #include <memory>
-#include <cstring>
 
 class MyString
 {
@@ -10,40 +8,14 @@ class MyString
 
 	public:
 		MyString() = default;
-
-		MyString(const char* str) : buf(std::make_unique<char[]>(std::strlen(str)+1)) {
-            std::strcpy(buf.get(), str);
-		}
-
-		MyString(const MyString& obj) : buf(std::make_unique<char[]>(obj.size()+1)) {
-            std::strcpy(buf.get(), obj.buf.get());
-		}
-
-		bool operator== (const MyString& rhs) const {
-			return std::strcmp(buf.get(), rhs.buf.get()) == 0;
-		}
-
-		bool operator!= (const MyString& rhs) const {
-			return !operator==(rhs);
-		}
-
-		const char& operator[] (int i) const {
-			return buf.get()[i];
-		}
-
-		char& operator[] (int i) {
-			return buf.get()[i];
-		}
-
-		bool empty() const {
-			if (!buf)
-				return true;
-			return std::strlen(buf.get()) == 0;
-		}
-
-		int size() const {
-			if (!buf)
-				return 0;
-			return std::strlen(buf.get());
-		}
+		MyString(const char* str);
+		MyString(const MyString& obj);
+		bool operator==(const MyString& rhs) const;
+		bool operator!=(const MyString& rhs) const;
+		const char& operator[](int i) const;
+		char& operator[](int i);
+		MyString operator+(const MyString& rhs) const;
+		void operator=(const MyString& rhs);
+		bool empty() const;
+		int size() const;
 };
