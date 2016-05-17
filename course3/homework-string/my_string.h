8b43126580db78c6b14dd409958793829eb40c5f
Karun Rao <karunrao97@gmail.com>
2016-05-17 23:28:03 +0200

    implement c_str(), operator=, operator+, operator+=

--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -13,11 +13,16 @@ public:
 
 	bool empty() const;
 	std::size_t size() const;
-
+	const char* c_str() const;
+		
+	MyString& operator=(const char* str);
+	MyString& operator=(const MyString& str);
 	bool operator==(const MyString& str) const;
 	bool operator!=(const MyString& str) const;
 	const char& operator[](int idx) const;
 	char& operator[](int idx);
+	MyString operator+(const MyString& str) const;
+	MyString& operator+=(const MyString& str);
 
 private:
 	std::unique_ptr<char[]> mBuffer;
