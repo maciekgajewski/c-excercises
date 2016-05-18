f05d04bea890ec82262664e3b1be153ca65b7ea7
Alex Popescu <mail@alexandrupopescu.com>
2016-05-19 00:39:06 +0200

    added size member added indexing and not equal operators addressed previous pull request comments added make file

---
+++ course3/homework-string/my_string.cc
@@ -0,0 +1,55 @@
+#include "my_string.h"
+
+#include <cstring>
+#include <stdexcept>
+
+MyString::MyString(): mSize(0) { }
+
+MyString::MyString(const char *inchar)
+	:mSize(std::strlen(inchar)), 
+	mBuffer(new char[mSize + 1])
+{
+	std::strcpy(mBuffer.get(), inchar);
+}
+
+MyString::MyString(const MyString& str)
+{
+	if (!str.empty())
+	{
+		mSize = str.mSize;
+		mBuffer.reset(new char[mSize + 1]);
+		std::strcpy(mBuffer.get(), str.mBuffer.get());
+	}
+}
+
+std::size_t MyString::size() const
+{
+	return mSize;
+}
+
+bool MyString::empty() const
+{
+	return !mBuffer || mSize == 0;
+}
+
+bool MyString::operator==(const MyString& other)
+{
+	if (this->empty() && other.empty()) return true;
+	if (this->empty() != other.empty()) return false;
+	return std::strcmp(this->mBuffer.get(), other.mBuffer.get()) == 0;
+}
+
+bool MyString::operator!=(const MyString& other)
+{
+	return !(*this == other);
+}
+
+char& MyString::operator[](const unsigned int i) const
+{
+	if (i > mSize)
+	{
+		throw std::out_of_range("Index cannot be larger than the size of the string");
+	}
+	return mBuffer[i];
+}
+
--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,6 +1,23 @@
 #pragma once
 
+#include <memory>
+
 class MyString
 {
-	// TODO
+public:
+
+	MyString();
+	MyString(const char*);
+	MyString(const MyString&);
+	
+	std::size_t size() const;
+	bool empty() const;
+	
+	bool operator==(const MyString& other);
+	bool operator!=(const MyString& other);
+	char& operator[](const unsigned i) const;
+	
+private:
+	std::size_t mSize;
+	std::unique_ptr<char[]> mBuffer;
 };
