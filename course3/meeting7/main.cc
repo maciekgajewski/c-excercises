6628a92ea01c5472527582fe5de690a851c0ad2e
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-06-02 17:12:12 +0200

    make pair

--- course3/meeting7/main.cc
+++ course3/meeting7/main.cc
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <stdexcept>
 
-void println()
+template<typename A, typename B>
+struct Pair
 {
-	std::cout << std::endl;
-}
+	Pair() = default;
+	Pair(const A& f, const B& s) : first(f), second(s) {}
+	
+	template<typename U0, typename U1>
+	Pair(const Pair<U0, U1>& other) : first(other.first), second(other.second) {}
+	
+	A first;
+	B second;
+};
 
-template<typename T, typename... Rest>
-void println(const T& param0, const Rest&... rest)
+template<typename T0, typename T1>
+Pair<T0, T1> MakePair(const T0& a, const T1& b)
 {
-	std::cout << param0 << ", ";
-	println(rest...);
+	return Pair<T0, T1>(a, b);
 }
 
 int main()
@@ -20,12 +27,7 @@ int main()
 	const int ci = 8;
 	const int& cref = ci;
 	
-	println(s);
-	println(i);
-	println(ci);
-	println(cref);
-	println();
-	println(5, 6);
-	println(5, 6.0);
-    
+	Pair<int, int> pair = MakePair(7, 6.6);
+	pair.first = 7;
+	pair.second = 6.6;
 }
