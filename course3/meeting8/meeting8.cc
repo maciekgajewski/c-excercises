7c5532ba105f6fee0dc4737d3b7d5ce6d95bd223
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-06-09 18:07:49 +0200

    end of meeting 8

--- course3/meeting8/meeting8.cc
+++ course3/meeting8/meeting8.cc
@@ -21,49 +21,21 @@ void print(const Range& range)
 }
 
 
+struct S
+{
+	int f;
+	S(int x) : f(x) {}
+	S() : f(7) {}
+};
+
 int main(int argc, char** argv)
 {
 	std::cout << "Hello" << std::endl;
-	auto x = argc + argv;
-	print_range(argv, x);
+	std::array<S, 5> a;
 	
-	std::vector<std::string> strings;
-	strings.push_back("One");
-	strings.push_back("Two");
-	strings.push_back("Three");
-	strings.push_back("Four");
-	
-	print_range(strings.begin() + 2, strings.begin()+4);
-
-	std::list<int> ints;
-	ints.push_back(4);
-	ints.push_back(7);
-	
-	print_range(ints.begin(), ints.end());
-	
-	std::deque<double> dbls;
-	dbls.push_back(3.14);
-	dbls.push_back(77.77);
-
-	print(dbls);
-	
-	std::array<bool, 3> bools;
-	bools[0] = true;
-	bools[1] = false;
-	bools[2] = true;
-
-	print_range(bools.begin(), bools.end());
-
-	int array[2] = { 6, 8 };
-	print(array);
-
-	//for(auto x : array)
-	for(auto it = std::begin(array); it != std::end(array); ++it)
-	{ 
-		auto x = *it;
-		
-		std::cout << x << std::endl;
-	}
-	
+	std::vector<char*> v;
+	v.reserve(argc);
+	for(int i = 0; i < argc; i++)
+		v.push_back(argv[i]);
 }
 
