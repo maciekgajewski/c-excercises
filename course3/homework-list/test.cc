c3baae242a11b6301e4bd035b7f55212d28fba27
Karun Rao <karunrao97@gmail.com>
2016-06-11 17:54:20 +0200

    begin list implementation

---
+++ course3/homework-list/test.cc
@@ -0,0 +1,17 @@
+#include "list.h"
+
+#include <cassert>
+#include <iostream>
+
+
+void list_test()
+{
+	
+}
+
+int main()
+{
+	std::cout << "Testing karun::list..." << std::endl;
+	list_test();
+	std::cout << "Tests pass :)" << std::endl;
+}
