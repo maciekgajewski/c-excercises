229f6c170bf5ebd2bb6e18e1c8169f3e2b2c6b08
Duncan Kimpton <dgkimpton@gmail.com>
2016-06-14 02:00:07 +0200

    working on a tested linked list implementation

--- course3/homework-list/linked-list.h
+++ course3/homework-list/linked-list.h
@@ -4,36 +4,106 @@
 template <typename TNode>
 class LinkedList
 {
-public:
-    
+    struct Node 
+    {
+        Node(const TNode& data, Node* next) : Data(data), Next(next)
+        {
+        }
+        const TNode& Data;
+        Node* Next;
+    };
     struct Iterator
     {
+        Iterator(Node* pointer) : mPointer(pointer)
+        {
+        }
+        
+        bool operator!=(const Iterator& other) 
+        {
+            return other.mPointer != mPointer;
+        }
+        
+        const TNode& operator*() 
+        {
+            return mPointer->Data;
+        }
+        
+        void operator++() 
+        {
+            mPointer = mPointer->Next;
+        }
+        
+    private:
+        Node* mPointer;
     };
     
-//     const Iterator begin() const
-//     {
-//     }
-//     
-//     const Iterator end() const
-//     {
-//     }
+public:
+        
+    LinkedList()
+    {
+        mHead = nullptr;
+    }
+    
+    ~LinkedList()
+    {        
+        Node* current = mHead;
+        while(current != nullptr)
+        {
+            auto tmp = current;
+            current = current->Next;
+            delete tmp;
+        }
+    }
+    
+    const Iterator begin() const
+    {
+        return Iterator(mHead);
+    }
+    
+    const Iterator end() const
+    {
+        return Iterator(nullptr);
+    }
     
     std::size_t size() const 
     {
-        return 0;
+        int size = 0;
+        
+        auto current = mHead;
+        while(current != nullptr)
+        {
+            current = current->Next;
+            ++size;
+        }
+        
+        return size;
     }
 
     bool empty() const
     {
-        return true;
+        return mHead == nullptr;
     }
     
-    void push_front(const TNode&)
+    void push_front(const TNode& data)
     {
+        if(empty())
+        {
+            mHead = new Node(data, nullptr);
+        }
+        else
+        {
+           mHead = new Node(data, mHead);
+        }
     }
     
     void pop_front()
     {
+        if(empty())
+            return;
+  
+        auto tmp = mHead;
+        mHead = mHead->Next;
+        delete tmp;
     }
     
 //     const TNode& front() const
@@ -44,12 +114,11 @@ public:
 //     {
 //     }
 //     
-    void erase(Iterator itemToRemove)
-    {
-    }
-    
+//     void erase(Iterator itemToRemove)
+//     {
+//     }
+//     
         
 private:
-    TNode* mHead;
-    TNode* mTail;
+    Node* mHead;
 };
--- course3/homework-list/tests-of-list.cc
+++ course3/homework-list/tests-of-list.cc
@@ -1,17 +1,127 @@
-
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
 #include "headers/catch/catch.h"
 
-#include <vector>
+#include "linked-list.h"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+SCENARIO("Lists maintain their size", "[list]") {
+    
+    GIVEN("an empty list") {
+        LinkedList<int> list;
+        
+        REQUIRE( list.empty() == true );
+        REQUIRE( list.size() == 0 );
+        
+        WHEN("an item is pushed") {
+            list.push_front(3);
+            
+            THEN("the capacity increases by 1 and it is no longer empty"){
+                REQUIRE( list.size() == 1 );
+                REQUIRE( list.empty() == false );
+            }       
+        
+            AND_WHEN("a second item is pushed") {
+                list.push_front(7);
+                
+                THEN("the capacity increases by 1 more"){
+                    REQUIRE( list.size() == 2 );
+                    REQUIRE( list.empty() == false );
+                }
+                
+                AND_WHEN("the second item is removed") {
+                    list.pop_front();
+                
+                    THEN("the capacity decreases by 1"){
+                        REQUIRE( list.size() == 1 );
+                        REQUIRE( list.empty() == false );
+                    }
+                    
+                    AND_WHEN("the first item is removed") {
+                        list.pop_front();
+                        
+                        THEN("the capacity returns to empty"){
+                            REQUIRE( list.size() == 0 );
+                            REQUIRE( list.empty() == true );
+                        }
+                    }
+                }  
+            }         
+        }
+    }
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+SCENARIO("Lists are iteratable", "[list]") {
+    
+     GIVEN("an empty list") {
+        LinkedList<int> list;
+               
+        WHEN("it is iterated over") {
+            
+            int count= 0;
+            for(auto element : list)
+                count++;
+            
+            THEN("no iterations are performed"){
+                REQUIRE( count == 0 );
+            }       
+        }
+     }
+          
+     GIVEN("a list of 5 items") {
+        LinkedList<int> list;        
+        list.push_front(3);
+        list.push_front(7);
+        list.push_front(9);
+        list.push_front(11);
+        list.push_front(13);
+               
+        WHEN("it is iterated over") {
+            
+            int count= 0;
+            for(auto element : list)
+                count++;
+            
+            THEN("5 iterations are performed"){
+                REQUIRE( count == 5 );
+            }
+            
+            AND_WHEN("an item is added before iteration") {
+                    list.push_front(15);
+                                        
+                    int count= 0;
+                    for(auto element : list)
+                        count++;
+                
+                    THEN("6 iterations are performed"){
+                        REQUIRE( count == 6 );
+                    }
+            }
+            
+            AND_WHEN("an item is removed before iteration") {
+                    list.pop_front();
+                                        
+                    int count= 0;
+                    for(auto element : list)
+                        count++;
+                
+                    THEN("4 iterations are performed"){
+                        REQUIRE( count == 4 );
+                    }
+            }
+            
+            AND_WHEN("all items are removed before iteration") {
+                    list.pop_front();
+                    list.pop_front();
+                    list.pop_front();
+                    list.pop_front();
+                    list.pop_front();
+                                        
+                    int count= 0;
+                    for(auto element : list)
+                        count++;
+                
+                    THEN("no iterations are performed"){
+                        REQUIRE( count == 0 );
+                    }
+            }
+        }
+     }
 }
