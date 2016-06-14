d3e8f695b5291785a28dab0a2dfc7b42abf0d56b
Duncan Kimpton <dgkimpton@gmail.com>
2016-06-14 07:13:31 +0200

    simple main

--- course3/homework-list/list-main.cc
+++ course3/homework-list/list-main.cc
@@ -5,7 +5,24 @@
 
 int main(int argc, char** argv)
 {
-    std::cout << "Start" << std::endl;
+    LinkedList<std::string> strings;
+    strings.push_front("strings");
+    strings.push_front("of");
+    strings.push_front("list");
+    strings.push_front("A");
+    
+    std::cout << "Strings in the list are: ";
+    bool isFirst = true;
+    for(auto element : strings)
+    {
+        if(!isFirst)
+        {
+            std::cout << ", ";
+        }
+        std::cout << element;
+        isFirst = false;
+    }
+    std::cout << std::endl;    
     
     return 0;
 }
