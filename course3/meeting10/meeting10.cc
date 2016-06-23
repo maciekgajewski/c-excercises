07c1d5568fa51732bc01788c3cee11d2e24cdf83
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-06-23 18:06:57 +0200

    maps

--- course3/meeting10/meeting10.cc
+++ course3/meeting10/meeting10.cc
@@ -1,13 +1,38 @@
-#include "get_stats.hh"
-
 #include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <iterator>
 
 int main(int argc, char** argv)
 {
-	double avg, median;
+	using namespace std::literals;
 	
-	std::tie(avg, median) = get_stats(argc-1, argv+1);
+	std::unordered_map<std::string, int> args = {
+		{"One", 1},
+		{"Two", 2},
+	};
 	
-	std::cout << "Avg: " << avg << std::endl;
-	std::cout << "Median: " << median << std::endl;
+	std::transform(argv, argv+argc, std::inserter(args, args.end()),
+		[&](char* in) { return std::make_pair(in, args.size()); });
+	
+	for(const auto& a : args)
+		std::cout << a.first << " => " << a.second << std::endl;
+	
+	auto it = args.find("Wally");
+	if (it == args.end())
+		std::cout << "no wally" << std::endl;
+	else
+		std::cout << "Wally at pos " << it->second << std::endl;
+	
+	//args.insert(std::make_pair("maciek", 77));
+	args.insert({"maciek"s, 77});
+	int maciek = args["maciek"];
+	std::cout << "maciek at pos " << maciek << std::endl;
+	
+	
+	
 }
