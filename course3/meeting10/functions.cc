ff32e2113a5bc8287f508b9c79075f6bb482822a
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-06-23 10:52:08 +0200

    -first stage of algo speedup

--- course3/meeting10/functions.cc
+++ course3/meeting10/functions.cc
@@ -1,13 +1,26 @@
 #include "functions.hh"
 
 #include <string>
+#include <stdexcept>
+
+static double str2dbl(const char* s)
+{
+	char* str_end = const_cast<char*>(s);
+	double r = std::strtod(s, &str_end);
+	if (str_end == s)
+	{
+		throw std::invalid_argument("Failed to convert string to double");
+	}
+	return r;
+}
+
 
 std::vector<double> paramsToNumbers(int argc, const char** argv)
 {
 	std::vector<double> out;
 	for(int i = 0; i < argc; i++)
 	{
-		out.push_back(std::stod(argv[i]));
+		out.push_back(str2dbl(argv[i]));
 	}
 	return out;
 }
