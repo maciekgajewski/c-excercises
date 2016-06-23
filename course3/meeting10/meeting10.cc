c9f68cfc24ae63a24fcd4b0853a722d5cebdb093
Maciej Gajewski <maciejgajewski@Optiver.com>
2016-06-23 17:10:15 +0200

    job well done!

--- course3/meeting10/meeting10.cc
+++ course3/meeting10/meeting10.cc
@@ -1,5 +1,13 @@
+#include "get_stats.hh"
+
 #include <iostream>
 
 int main(int argc, char** argv)
 {
+	double avg, median;
+	
+	std::tie(avg, median) = get_stats(argc-1, argv+1);
+	
+	std::cout << "Avg: " << avg << std::endl;
+	std::cout << "Median: " << median << std::endl;
 }
