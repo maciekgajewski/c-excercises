9bf2a3e23fc69b398a3870a4b2c1bf94f7054ff1
Karun Rao <karunrao97@gmail.com>
2016-06-27 00:04:41 +0200

    add benchmark

--- course3/homework-list/benchmark.cc
+++ course3/homework-list/benchmark.cc
@@ -1,7 +1,24 @@
+#include "list.h"
+
+#include <list>
+#include <forward_list>
+#include <algorithm>
 #include <benchmark/benchmark.h>
 
 
-int main()
+template<template<typename...> class ListType>
+void ListBenchmark(benchmark::State& state)
 {
-	
+	ListType<int> l;
+	for (int i=0; i < state.range_x(); ++i)
+		l.push_front(i);
 }
+
+//BENCHMARK_TEMPLATE(ListBenchmark, Karun::List)->Range(1<<0, 1<<10);
+//BENCHMARK_TEMPLATE(ListBenchmark, std::list)->Range(1<<0, 1<<10);
+//BENCHMARK_TEMPLATE(ListBenchmark, std::forward_list)->Range(1<<0, 1<<10);
+
+//BENCHMARK_MAIN();
+
+// Placeholder for BENCHMARK_MAIN
+int main() {}
