2a132afc2f9ff74954bd84009b77d1a054cc1cd3
Karun Rao <karunrao97@gmail.com>
2016-06-28 20:39:00 +0200

    benchmark lists

--- course3/homework-list/benchmark.cc
+++ course3/homework-list/benchmark.cc
@@ -2,6 +2,7 @@
 
 #include <list>
 #include <forward_list>
+#include <deque>
 #include <algorithm>
 #include <benchmark/benchmark.h>
 
@@ -9,16 +10,17 @@
 template<template<typename...> class ListType>
 void ListBenchmark(benchmark::State& state)
 {
-	ListType<int> l;
-	for (int i=0; i < state.range_x(); ++i)
-		l.push_front(i);
+	while (state.KeepRunning())
+	{
+		ListType<int> l;
+		for (int i=0; i < state.range_x(); ++i)
+			l.push_front(i);
+	}
 }
 
-//BENCHMARK_TEMPLATE(ListBenchmark, Karun::List)->Range(1<<0, 1<<10);
-//BENCHMARK_TEMPLATE(ListBenchmark, std::list)->Range(1<<0, 1<<10);
-//BENCHMARK_TEMPLATE(ListBenchmark, std::forward_list)->Range(1<<0, 1<<10);
+BENCHMARK_TEMPLATE(ListBenchmark, std::deque)->Range(1<<0, 1<<20);
+BENCHMARK_TEMPLATE(ListBenchmark, std::list)->Range(1<<0, 1<<20);
+BENCHMARK_TEMPLATE(ListBenchmark, std::forward_list)->Range(1<<0, 1<<20);
+BENCHMARK_TEMPLATE(ListBenchmark, Karun::List)->Range(1<<0, 1<<20);
 
-//BENCHMARK_MAIN();
-
-// Placeholder for BENCHMARK_MAIN
-int main() {}
+BENCHMARK_MAIN();
