6397af5d62bca09e6f77345245a7e4d228e431a2
Alex Popescu <mail@alexandrupopescu.com>
2016-06-28 23:37:57 +0200

    refactored (merged implementation to the .h file) fixed build and tests

--- course3/homework-linked-list/linked_list.h
+++ course3/homework-linked-list/linked_list.h
@@ -8,29 +8,66 @@ class LinkedList
 {
 public:
 
-    LinkedList();
+    LinkedList(): head(nullptr), tail(nullptr), length(0) { }
 
-    const T* begin() const;
-    T* begin();
-    const T* end() const;
-    T* end();
+    const T* begin() const {
+        return this->head ? this->head->data : nullptr;
+    }
 
-    std::size_t size() const;
-    bool empty() const;
+    T* begin() {
+        return this->head ? this->head->data : nullptr;
+    }
 
-    void push_front(const T&);
-    void pop_front();
+    const T* end() const {
+        return this->tail ? this->tail->data : nullptr;
+    }
 
-    const T& front() const;
-    T& front();
+    T* end() {
+        return this->tail ? this->tail->data : nullptr;
+    }
+
+    std::size_t size() const {
+        return this->length;
+    }
+
+    bool empty() const {
+        return this->length == 0;
+    }
+
+    void push_front(const T& input) {
+        auto n = Node(input);
+        n.next = this->head;
+        this->head = &n;
+        this->length++;
+    }
+
+    void pop_front() {
+        if (this->empty()) {
+            throw std::out_of_range("The list is empty");
+        }
+        auto topop = this->head;
+        this->head = this->head->next;
+        delete topop;
+        this->length--;
+    }
+
+    const T& front() const {
+        return *this->head;
+    }
+
+    T& front() {
+        return *this->head;
+    }
 
 private:
     class Node {
     public:
-        T data;
+        T* data;
         Node* next;
 
-        Node(T input);
+        Node(T data) {
+            this->data = new int(data);
+        }
     };
 
     Node* head;
