de0326103cc0094c06c4df8e356a1c2f4f6ec547
Karun Rao <karunrao97@gmail.com>
2016-06-29 22:02:01 +0200

    create skeleton

---
+++ course3/homework-transcoder/test.cc
@@ -0,0 +1,16 @@
+#include "transcoder.h"
+
+#include <iostream>
+
+
+void transcoder_test()
+{
+	
+}
+
+int main()
+{
+	std::cout << "Testing Karun::Transcoder..." << std::endl;
+	transcoder_test();
+	std::cout << "Tests pass :)" << std::endl;
+}
