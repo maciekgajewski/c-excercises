296a938c382eaf48d648b24285c9222b3413a787
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-07-15 14:15:30 +0200

    course3 - meeting14 - final version

--- course3/meeting13/meeting13.cc
+++ course3/meeting13/meeting13.cc
@@ -1,6 +1,34 @@
 #include <stdexcept>
+#include <iostream>
+#include <string>
+
+struct S
+{
+public:
+	S(const std::string& name) : mName(name) {}
+	~S() { std::cout << "Destroying " << mName << std::endl; }
+private:
+	std::string mName;
+};
+
+void fun(int argc) noexcept(true)
+{
+	S f1("f1");
+	if (argc > 1)
+		throw std::runtime_error("hello");
+	S f2("f2");
+}
 
 int main(int argc, char** argv)
 {
-	throw std::runtime_error("Hello, world");
+	try{
+		S s("main");
+		fun(argc);
+		throw std::logic_error("7");
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Exception : " << e.what() << std::endl;
+		throw;
+	}
 }
