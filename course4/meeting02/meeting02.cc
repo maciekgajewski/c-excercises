a3f76591ebfbed27f706c821cb27b0417d38ee32
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-09-15 16:59:08 +0200

    meeting 2, part 1

--- course4/meeting02/meeting02.cc
+++ course4/meeting02/meeting02.cc
@@ -1,28 +1,20 @@
 #include <iostream>
-//#include <stdint>
-#include <limits>
-
-void print(int x) { std::cout << "int=" << x << std::endl; }
-void print(float x) { std::cout << "float=" << x << std::endl; }
+#include <string>
 
 int main(int argc, char** argv)
 {
-	int i = 7;
-	std::int8_t c = 8;
-	std::uint8_t uc = 8;
-	std::int64_t ll = 334455;
+	auto a = 33ll;
+	auto b = 4.5;
+	auto c = "hel\0lo"; // const char*
 	
-	std::cout << "Max value of int = " << std::numeric_limits<int>::max() << std::endl;
+	auto d = *c; // const char
+	auto e = c + 4;
+	auto f = c[1];
+	auto g = std::string("hello");
+	auto h = g[1];
 	
-	double f = 7;
-	
-	int* p = &i;
-	double* r = reinterpret_cast<float*>(&i);
-	
-	print(i);
-	print(f);
-	print(*p);
-	print(*r);
+	std::cout << g << std::endl;
+	std::cout << 3["hello"] << std::endl;
 	
 }
 
