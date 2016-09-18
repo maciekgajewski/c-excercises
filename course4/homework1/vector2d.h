e123bc9f2ef3e4f4bbb63675de82a28dbdcf8af9
arccth <arccth@gmail.com>
2016-09-18 18:55:21 +0200

    homework one

--- course4/homework1/vector2d.h
+++ course4/homework1/vector2d.h
@@ -4,6 +4,41 @@
 
 struct Vector2d
 {
-	// TODO
+private:
+   double mX, mY;
+
+public:
+
+   Vector2d(const double& x, const double& y)
+   {
+     mX = x;
+     mY = y;
+   }
+
+   double GetX() const
+   {
+       return mX;
+   }
+
+   double GetY() const
+   {
+       return mY;
+   }
 };
+
+std::ostream& operator<<(std::ostream& os, const Vector2d& target)
+{
+   os << "(" << target.GetX() << ", " << target.GetY() <<")";
+   return os;
+}
+
+bool operator == (const Vector2d& right, const Vector2d& left)
+{
+    return right.GetX() == left.GetX() && right.GetY() == left.GetY();
+}
+
+Vector2d operator +(const Vector2d& right, const Vector2d& left)
+{
+    return Vector2d(right.GetX() + left.GetX(), right.GetY() + left.GetY());
+}
 
