bb618fc994b4ad850781e516ddc881e11107ce31
dev <dev@localhost.localdomain>
2016-09-21 22:15:06 +0200

    vector2d implementation

--- course4/homework1/vector2d.h
+++ course4/homework1/vector2d.h
@@ -4,6 +4,24 @@
 
 struct Vector2d
 {
-	// TODO
+	double x;
+	double y;
+
+	Vector2d(double x, double y) : x(x), y(y) {}
 };
 
+Vector2d operator+(const Vector2d& a, const Vector2d& b)
+{
+	return {a.x + b.x, a.y + b.y};
+}
+
+bool operator==(const Vector2d& a, const Vector2d& b)
+{
+	return (a.x == b.x) && (a.y == b.y);
+}
+
+std::ostream& operator<<(std::ostream& os, const Vector2d& obj)
+{
+	os << "("<< obj.x << ", " << obj.y << ")";
+	return os;
+}
