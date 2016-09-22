8fd0d13d91cb050656b9be5a27aeac831d35a299
Ningyao Zhang <ningyaozhang@optiver.com>
2016-09-22 12:02:00 +0200

    Ningyao finished homework1

--- course4/homework1/vector2d.h
+++ course4/homework1/vector2d.h
@@ -5,5 +5,23 @@
 struct Vector2d
 {
 	// TODO
+private:
+	double x;
+	double y;
+public:
+	Vector2d() = delete;
+	Vector2d(double a, double b) : x(a), y(b) {}
+	Vector2d operator+ (const Vector2d& v2) 
+	{
+		return{ x + v2.x, y + v2.y };
+	};
+	bool operator== (const Vector2d& v2)
+	{
+		return (x == v2.x) && (y == v2.y) ? true : false;
+	}
+	friend std::ostream& operator<< (std::ostream& os, const Vector2d& v) 
+	{
+		os << "(" << v.x << ", " << v.y << ")";
+		return os;
+	}
 };
-
