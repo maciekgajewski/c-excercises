704dae173b91fdc2b8e982bb3d7117d4a1e8740d
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-09-22 17:34:56 +0200

    more examples of the big 6

--- course4/meeting03/meeting03.cc
+++ course4/meeting03/meeting03.cc
@@ -1,43 +1,48 @@
 #include <iostream>
 #include <string>
 
-class Vector
+struct Class
 {
-	double x = 0;
-	double y = 0;
-public:
+	// Default 
+	Class() { std::cout << this << " Default constructed" << std::endl; }
+	// Copy constructor
+	Class(const Class& source) { std::cout << this << " Copied from " << &source << std::endl; }
+	// Move constructor
+	Class(Class&& source) { std::cout << this << " Moved from " << &source << std::endl; }
+	
+	// User-define
+	Class(const char* v) { std::cout << this << " Initialized with v=" << v << std::endl; }
 
-	Vector(double ax, double ay)
-	{
-		x = ax;
-		y = ay;
+	// copy assignment
+	Class& operator=(const Class& source) {
+		std::cout << this << " Copy-assigned from " << &source << std::endl;
+		return *this;
+	}
+	// move assignment
+	Class& operator=(Class&& source) {
+		std::cout << this << " Move-assigned from " << &source << std::endl;
+		return *this;
 	}
 	
-	void setX(double x)
-	{
-		this->x = x;
-	}
-	double getX() const
-	{
-		return x;
-	}
+	~Class() { std::cout << this << " Destroyed" << std::endl; }
 };
-
-void print(const Vector& v)
-{
-	std::cout << "x=" << v.getX() << std::endl;
-}
 
 int main(int argc, char** argv)
 {
-	std::string s1; // default construction
-	std::string s2("hey!"); // initilizing constructor
-	std::string s3(s2); // copy constructor
-	std::string s4 = s2; // copy constructor
-	s3 = s4; // copy assignment
-
-	s1 = std::move(s2); // move assignment
+	Class s1; // default construction
+	Class s2("hey!"); // initilizing constructor
+	Class s3(s2); // copy constructor
+	Class* p = nullptr;
+	{
+		Class s4 = s2; // copy constructor
+		s3 = s4; // copy assignment
+		
+		s1 = std::move(s2); // move assignment
+		
+		Class s5(std::move(s3)); // move construction
+		p = &s5;
+	}
+	std::cout << "hello!!!, p=" << p << std::endl;
 	
-	std::string s5(std::move(s3)); // move construction
 }
 
