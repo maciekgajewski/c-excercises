85ad2ce3debb7e467ca8b8b5c9e28d4ea145f8e1
Tom Buskens <tom@tom.work>
2016-09-26 19:50:00 +0200

    homework 2

--- course4/homework1/vector2d.h
+++ course4/homework1/vector2d.h
@@ -1,11 +1,41 @@
 #pragma once
 
+#include <cmath>
 #include <ostream>
 
 struct Vector2d
 {
 public:
-    Vector2d(int x, int y) : mX(x), mY(y) {}
+    // named ctors
+    static Vector2d fromCarthesian(const double x, const double y) {
+        return { x, y };
+    }
+    static Vector2d fromPolar(const double rho, const double phi) {
+        return { rho * cos(phi), rho * sin(phi) };
+    }
+    // ctor
+    Vector2d(double x, double y) : mX(x), mY(y) {}
+    // setters
+    void X(const double x) {
+        mX = x; 
+    }
+    void Y(const double y) {
+        mY = y;
+    }
+    // getters 
+    double X() const {
+        return mX;
+    }
+    double Y() const {
+        return mY;
+    }
+    double Rho() const {
+        return sqrt(mX*mX + mY*mY);
+    }
+    double Phi() const {
+        return atan2(mY, mX);
+    }
+    // operators
     bool operator==(const Vector2d& other) const {
         return mX == other.mX && mY == other.mY;
     }
@@ -16,8 +46,8 @@ public:
         return { mX - other.mX, mY - other.mY };
     }
 private:
-    const int mX;
-    const int mY;
+    double mX;
+    double mY;
 
     friend std::ostream& operator<<(std::ostream& out, const Vector2d& vector) {
         return out << '(' << vector.mX << ", " << vector.mY << ')';
