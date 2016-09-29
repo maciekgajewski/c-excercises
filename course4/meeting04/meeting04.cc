86ec55062040ab58634ea25673493dcd3b836398
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-09-29 17:00:25 +0200

    Introduction to new/delete

--- course4/meeting04/meeting04.cc
+++ course4/meeting04/meeting04.cc
@@ -1,7 +1,37 @@
-#include <stdlib.h>
+#include <iostream>
+
+struct Class
+{
+	// Default 
+	Class() { std::cout << this << " Default constructed" << std::endl; }
+	// Copy constructor
+	Class(const Class& source) { std::cout << this << " Copied from " << &source << std::endl; }
+	// Move constructor
+	Class(Class&& source) { std::cout << this << " Moved from " << &source << std::endl; }
+	
+	// User-define
+	explicit Class(const char* v) { std::cout << this << " Initialized with v=" << v << std::endl; }
+	explicit Class(int a, int b) { std::cout << this << " Initialized with a=" << a << ", b=b" << std::endl; }
+	// copy assignment
+	Class& operator=(const Class& source) {
+		std::cout << this << " Copy-assigned from " << &source << std::endl;
+		return *this;
+	}
+	// move assignment
+	Class& operator=(Class&& source) {
+		std::cout << this << " Move-assigned from " << &source << std::endl;
+		return *this;
+	}
+	
+	~Class() { std::cout << this << " Destroyed" << std::endl; }
+};
 
 int main(int argc, char** argv)
 {
-	return ::system("banner hello");
+	Class* c = new Class();
+	Class b;
+	
+	//delete c;
+	
 }
 
