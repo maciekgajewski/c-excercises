49ecbb4d95e89c1ebd2b02241cc053ef90f254c7
arccth <arccth@gmail.com>
2016-10-05 23:14:30 +0200

    string implementation

--- course4/homework-string/string.cc
+++ course4/homework-string/string.cc
@@ -6,4 +6,76 @@ string::string()
 {
 }
 
+string::string(const char * c)
+{
+    int i = 0;
+    while(c[i] != '\0') // I guess we are not supposed to use strlen
+    {
+        chars.push_back(c[i]);
+        ++i;
+    }
+}
+
+string::string(const string& source)
+{
+    chars = source.chars;
+}
+
+string::string(string&& source)
+{
+    chars = std::move(source.chars);
+}
+
+bool string::empty() const
+{
+    return chars.empty();
+}
+
+int string::size() const
+{
+    return chars.size();
+}
+
+bool string::operator == (const string& other) const
+{
+    if(other.chars.size() != chars.size())
+    {
+        return false;
+    }
+
+    for(int i=0; i< chars.size(); ++i)
+    {
+        if(chars[i]!= other.chars[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool string::operator != (const string& other) const
+{
+    return !(*this == other);
+}
+
+const char& string::operator [](const int index) const
+{
+    return chars[index];
+}
+
+char& string::operator [](const int index)
+{
+    return chars[index];
+}
+
+string& string::operator =(const string& source)
+{
+    chars = source.chars;
+}
+
+string& string::operator =(string&& source)
+{
+    chars = std::move(source.chars);
+}
 }
--- course4/homework-string/string.hh
+++ course4/homework-string/string.hh
@@ -1,10 +1,47 @@
 #pragma once
+#include <ostream>
+#include<vector>
 
 namespace course {
 
 class string
 {
-	string();
-};
+private:
+	std::vector<char> chars;
 
+public:
+    string();
+
+    string(const char * c);
+
+    string(const string& source);
+
+    string(string&& source);
+
+    bool empty() const;
+
+    int size() const;
+
+    bool operator == (const string& other) const;
+
+    bool operator != (const string& other) const;
+
+    const char& operator [](const int index) const;
+
+    char& operator [](const int index);
+
+    string& operator =(const string& source);
+
+    string& operator=(string&& source);
+
+    friend std::ostream& operator<<(std::ostream& os, const string& s)
+    {
+        for(int i=0; i < s.chars.size(); ++i)
+        {
+            os << s.chars[i];
+        }
+
+        return os;
+    }
+};
 }
