1badc03268c6ee2071aa08359d4d1b5438c2f5e7
arccth <arccth@gmail.com>
2016-10-09 19:21:21 +0200

    unque ptr string implementation

--- course4/homework-string/string.cc
+++ course4/homework-string/string.cc
@@ -1,9 +1,98 @@
 #include "string.hh"
+#include <cstring>
+#include <memory>
 
 namespace course {
 
 string::string()
 {
+    mSize = 0;
 }
 
+string::string(const char * c)
+{
+    mSize = std::strlen(c);
+    copyChars(c);
+}
+
+string::string(const string& source)
+{
+    mSize = source.mSize;
+    copyChars(source.chars.get());
+}
+
+string::string(string&& source)
+{
+    stealChars(std::move(source));
+}
+
+bool string::empty() const
+{
+    return !mSize;
+}
+
+int string::size() const
+{
+    return mSize;
+}
+
+bool string::operator == (const string& other) const
+{
+    if(other.mSize != mSize)
+    {
+        return false;
+    }
+
+    for(int i=0; i< mSize; ++i)
+    {
+        if(chars[i]!= other.chars[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool string::operator != (const string& other) const
+{
+    return !(*this == other);
+}
+
+const char& string::operator [](const int index) const
+{
+    return chars[index];
+}
+
+char& string::operator [](const int index)
+{
+    return chars[index];
+}
+
+string& string::operator =(const string& source)
+{
+   mSize = source.mSize;
+   copyChars(source.chars.get());
+}
+
+string& string::operator =(string&& source)
+{
+   stealChars(std::move(source));
+}
+
+void string::stealChars(string&& source)
+{
+    mSize = source.mSize;
+    chars = std::move(source.chars);
+    source.chars = nullptr;
+}
+
+void string::copyChars(const char *c)
+{
+    chars = std::make_unique<char[]>(mSize);
+    for(int i=0; i < mSize; ++i)
+    {
+        chars[i]= c[i];
+    }
+}
 }
--- course4/homework-string/string.hh
+++ course4/homework-string/string.hh
@@ -1,10 +1,51 @@
 #pragma once
+#include <ostream>
+#include<vector>
+#include <memory>
 
 namespace course {
 
 class string
 {
-	string();
-};
+private:
+    std::unique_ptr<char[]> chars;
+    int mSize;
+    void copyChars(const char * c);
+    void stealChars(string&& source);
 
+public:
+    string();
+
+    string(const char * c);
+
+    string(const string& source);
+
+    string(string&& source);
+
+    bool empty() const;
+
+    int size() const;
+
+    bool operator == (const string& other) const;
+
+    bool operator != (const string& other) const;
+
+    const char& operator [](const int index) const;
+
+    char& operator [](const int index);
+
+    string& operator =(const string& source);
+
+    string& operator=(string&& source);
+
+    friend std::ostream& operator<<(std::ostream& os, const string& s)
+    {
+        for(int i=0; i < s.size(); ++i)
+        {
+            os << s.chars[i];
+        }
+
+        return os;
+    }
+};
 }
