f816ec07131d8ff76bf48379106722a8026a106b
Ningyao Zhang <ningyaozhang@optiver.com>
2016-10-10 11:17:41 +0200

    clean up homework3

--- course3/homework-string/my_string.h
+++ course3/homework-string/my_string.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <memory>
+#include <string.h>
 #include <stdexcept>
-#include <algorithm>
 #include <iostream>
 
 class MyString
@@ -13,17 +13,19 @@ private:
 
 public:
 	MyString();
+	MyString(size_t n);
 	MyString(const char* s1);
 	MyString(const MyString& s1);
 	MyString(MyString&& s1);
+	bool empty() const;
+	size_t size() const;
+	const char* c_str() const;
 	bool operator== (const MyString& str) const;
 	bool operator != (const MyString& str) const;
 	MyString& operator= (const MyString& str);
 	MyString& operator+= (const MyString& str);
-	char& operator[] (size_t n) const;
-	bool empty() const;
-	size_t size() const;
-	const char* c_str() const;
+	char operator[] (size_t n) const;
+	char& operator[] (size_t n);
 	friend MyString operator+ (const MyString& str1, const MyString& str2);
 	friend bool operator< (const MyString& str1, const MyString& str2);
 	friend std::ostream& operator<< (std::ostream os, const MyString& str);
@@ -36,15 +38,18 @@ MyString::MyString()
 	mStrSize = 0;
 }
 
+MyString::MyString(size_t n)
+{
+	mStr = std::make_unique<char[]>(n + 1);
+	mStr[n] = NULL;
+	mStrSize = n;
+}
+
 MyString::MyString(const char* s1)
 {
 	mStrSize = strlen(s1);
 	mStr = std::make_unique<char[]>(mStrSize + 1);
-	for (size_t i = 0; i < mStrSize; i++)
-	{
-		mStr[i] = s1[i];
-	}
-	mStr[mStrSize] = NULL;
+	strcpy(mStr.get(), s1);
 }
 
 MyString::MyString(const MyString& s1)
@@ -52,72 +57,13 @@ MyString::MyString(const MyString& s1)
 	mStrSize = s1.mStrSize;
 	mStr.reset();
 	mStr = std::make_unique<char[]>(mStrSize + 1);
-	for (size_t i = 0; i < mStrSize; i++)
-	{
-		mStr[i] = s1.mStr[i];
-	}
-	mStr[mStrSize] = NULL;
+	strcpy(mStr.get(), s1.c_str());
 }
 
 MyString::MyString(MyString&& s1)
 {
 	mStrSize = s1.size();
 	mStr = std::move(s1.mStr);
-}
-
-bool MyString::operator== (const MyString& str) const
-{
-	if (mStrSize != str.mStrSize)
-	{
-		return false;
-	}
-	else
-	{
-		for (size_t i = 0; i < mStrSize; i++)
-		{
-			if (mStr[i] != str.mStr[i])
-			{
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-bool MyString::operator != (const MyString& str) const
-{
-	return !(*this == str);
-}
-
-MyString& MyString::operator= (const MyString& str)
-{
-	mStrSize = str.mStrSize;
-	mStr.reset();
-	mStr = std::make_unique<char[]>(mStrSize + 1);
-	for (size_t i = 0; i < mStrSize; i++)
-	{
-		mStr[i] = str[i];
-	}
-	mStr[mStrSize] = NULL;
-	return *this;
-}
-
-MyString& MyString::operator+= (const MyString& str)
-{
-	*this = *this + str;
-	return *this;
-}
-
-char& MyString::operator[] (size_t n) const
-{
-	if (n < mStrSize)
-	{
-		return mStr[n];
-	}
-	else
-	{
-		throw std::runtime_error("index exceeds string size");
-	}
 }
 
 bool MyString::empty() const
@@ -135,45 +81,70 @@ const char* MyString::c_str() const
 	return mStr.get();
 }
 
+bool MyString::operator== (const MyString& str) const
+{
+	return (mStrSize == str.mStrSize) && (strcmp(mStr.get(), str.c_str()) == 0);
+}
+
+bool MyString::operator != (const MyString& str) const
+{
+	return !(*this == str);
+}
+
+MyString& MyString::operator= (const MyString& str)
+{
+	mStrSize = str.mStrSize;
+	mStr.reset();
+	mStr = std::make_unique<char[]>(mStrSize + 1);
+	strcpy(mStr.get(), str.c_str());
+	return *this;
+}
+
+char MyString::operator[] (size_t n) const
+{
+	if (n < mStrSize)
+	{
+		return mStr[n];
+	}
+	else
+	{
+		throw std::runtime_error("index exceeds string size");
+	}
+}
+
+char& MyString::operator[] (size_t n)
+{
+	if (n < mStrSize)
+	{
+		return mStr[n];
+	}
+	else
+	{
+		throw std::runtime_error("index exceeds string size");
+	}
+}
+
 MyString operator+ (const MyString& str1, const MyString& str2)
 {
-	MyString str;
-	str.mStrSize = str1.mStrSize + str2.mStrSize;
-	str.mStr = std::make_unique<char[]>(str.mStrSize + 1);
-	for (size_t i = 0; i < str1.mStrSize; i++)
-	{
-		str.mStr[i] = str1[i];
-	}
-	for (size_t i = 0; i < str2.mStrSize; i++)
-	{
-		str.mStr[str1.mStrSize + i] = str2[i];
-	}
-	str.mStr[str.mStrSize] = NULL;
+	MyString str(str1.mStrSize + str2.mStrSize);
+	strcpy(str.mStr.get(), str1.c_str());
+	strcat(str.mStr.get() + str1.size(), str2.c_str());
 	return str;
+}
+
+MyString& MyString::operator+= (const MyString& str)
+{
+	*this = *this + str;
+	return *this;
 }
 
 bool operator< (const MyString& str1, const MyString& str2)
 {
-	size_t m = std::min(str1.size(), str2.size());
-	for (size_t i = 0; i < m; i++)
-	{
-		if (str1[i] == str2[i])
-		{
-			continue;
-		}
-		else
-		{
-			return str1[i] < str2[i];
-		}
-	}
-	return str1.size() < str2.size();
+	return strcmp(str1.c_str(), str2.c_str()) < 0;
 }
 
 std::ostream& operator<< (std::ostream os, const MyString& str)
 {
-	for (size_t i = 0; i < str.size(); i++)
-	{
-		os << str[i];
-	}
+	os << str.c_str();
 	return os;
 }
