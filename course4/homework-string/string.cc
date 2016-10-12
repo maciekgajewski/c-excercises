6c09bfdeecd507a922480f379638df0055f0d8ed
camiloOMesa <camilo_orejuela@hotmail.com>
2016-10-12 20:10:01 +0200

    string homework 2

--- course4/homework-string/string.cc
+++ course4/homework-string/string.cc
@@ -1,9 +1,134 @@
 #include "string.hh"
+#
+#include <cstring>
+#include <sstream>
+#include <iostream>
 
 namespace course {
 
 string::string()
 {
+    mLength = 0;
 }
 
+string::string(const char* chars)
+{
+    mLength = std::strlen(chars);
+    mData = std::make_unique<char[]>(mLength+1);
+    std::copy(chars,chars+mLength, mData.get());
+}
+
+string::string(const string& source)
+{
+    mLength = source.size();
+    mData = std::make_unique<char[]>(mLength+1);
+    std::copy(source.mData.get(),source.mData.get()+mLength, mData.get());
+}
+
+
+char& string::operator[](int index) const
+{
+    return mData[index];
+}
+
+
+string& string::operator=(const string& source)
+{
+    mLength = source.size();
+    mData = std::make_unique<char[]>(mLength+1);
+    std::copy(source.mData.get(),source.mData.get()+mLength, mData.get());
+
+    return *this;
+}
+
+bool string::operator==(const string& a)
+{
+    if(mLength!=a.mLength)
+        return false;
+
+    return (std::strcmp(a.mData.get(), mData.get()) == 0);
+}
+
+bool string::operator ==(const char* chars)
+{
+    int charsLength = std::strlen(chars);
+    if(mLength != charsLength)
+        return false;
+
+    return (std::strcmp(chars, mData.get()) == 0);
+}
+
+bool string::operator!=(const string& a) const
+{
+    return mData.get() != a.mData.get();
+}
+
+string& string::operator+=(const string& a)
+{
+    auto totaLength = mLength + a.mLength;
+    auto temp = std::make_unique<char[]>(totaLength+1);
+    std::copy(mData.get(),mData.get()+mLength, temp.get());
+
+    for(int j=0;j<=a.mLength;++j)
+    {
+        temp[j+mLength] = a.mData[j];
+    }
+
+    mLength = totaLength;
+    mData = nullptr;
+    mData = std::move(temp);
+    return *this;
+}
+
+string string::operator+(const string& a) const
+{
+    string result(*this);
+    result+=a;
+    return result;
+}
+
+bool string::empty() const
+{
+    if(mData==nullptr)
+        return true;
+
+    return false;
+}
+
+int string::size() const
+{
+    return mLength;
+}
+
+int string::length() const
+{
+    return this->size();
+}
+
+const char* string::c_str() const
+{
+    if(mData==nullptr)
+        return "\0";
+
+    return mData.get();
+}
+
+ void string::clear()
+ {
+     mData = nullptr;
+     mLength = 0;
+ }
+
+string::~string()
+{
+}
+
+ std::stringstream& operator <<(std::stringstream& ss, const course::string &s)
+ {
+     for(int j=0;j<s.size();++j)
+     {
+         ss<<s[j];
+     }
+     return ss;
+ }
 }
--- course4/homework-string/string.hh
+++ course4/homework-string/string.hh
@@ -1,10 +1,39 @@
 #pragma once
+#include <ostream>
+#include <memory>
 
 namespace course {
-
 class string
 {
-	string();
+public:
+    string();
+    ~string();
+    string(const char* chars);
+    string(const string& source);
+
+    string& operator=(const string& source);
+    bool operator ==(const string& a);
+    bool operator ==(const char* chars);
+    bool operator !=(const string& a) const;
+    string& operator+=(const string& source);
+    string operator+(const string& b) const;
+    char& operator[](int index) const;
+
+    const char* c_str() const;
+    bool empty() const;
+    int size() const;
+    int length() const;
+    void clear();
+
+private:
+    std::unique_ptr<char[]> mData;
+    int mLength;
+    int getLenghtOfChars(const char* chars);
+    void deepCopy(const char* chars, int lenght);
+    bool deepCompare(const char* chars, int length);
 };
 
+std::stringstream& operator <<(std::stringstream& os, const course::string &s);
+
 }
+
