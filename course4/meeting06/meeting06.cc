9bf2adcb030f50aa6faf4627edf3c8126320df74
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-10-13 16:44:52 +0200

    before templates

--- course4/meeting06/meeting06.cc
+++ course4/meeting06/meeting06.cc
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <cstring>
+
+void print(const char* text)
+{
+	std::cout << text << std::endl;
+}
+
+void print(int i)
+{
+	std::cout << i << std::endl;
+}
+
+void print(double i)
+{
+	std::cout << i << std::endl;
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello!" << std::endl;
-	
+	print("Hello");
+	print(argc);
+	print(3.14);
+	print(2.77f);
 }
 
