3b9472b3380f4040d3956c483d93449b637ddb66
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-10-13 17:35:28 +0200

    full specialization example

--- course4/meeting06/meeting06.cc
+++ course4/meeting06/meeting06.cc
@@ -1,26 +1,63 @@
 #include <iostream>
-#include <cstring>
+#include <limits>
+#include <string>
 
-void print(const char* text)
+template<typename T>
+void print(const T& v)
 {
-	std::cout << text << std::endl;
+	std::cout << v << std::endl;
 }
 
-void print(int i)
+void print(const char* t)
 {
-	std::cout << i << std::endl;
+	std::cout << "c-string: " << t << std::endl;
 }
 
-void print(double i)
+template<typename T>
+struct Vector
 {
-	std::cout << i << std::endl;
+	T x;
+	T y;
+	
+	T sum() const;
+	
+	template<typename X>
+	T sumX(const X& c) const;
+};
+
+template<>
+struct Vector<double>
+{
+	double a, b;
+	int sum() const { return int(a/b); }
+};
+
+template<typename T>
+T Vector<T>::sum() const { return x + y; }
+
+
+template<typename T>
+template<typename X>
+T Vector<T>::sumX(const X& c) const
+{
+	return x + y + c;
 }
 
 int main(int argc, char** argv)
 {
 	print("Hello");
-	print(argc);
-	print(3.14);
-	print(2.77f);
+	
+	Vector<int> v1{3, 5};
+	Vector<std::string> v2{"aaa", "bbbb"};
+
+	print(v1.sum());
+	print(v2.sum());
+	
+	print(v2.sumX("abc"));
+	print(v2.sumX(std::string("zzzz")));
+	
+	Vector<double> v3{ 100.0, 2.5 };
+	print(v3.sum());
+	
 }
 
