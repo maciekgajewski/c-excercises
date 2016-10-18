730e9487dc34a2d7a4d52e4ff5b8ba04f419ab92
Florian Kloeck <florian.kloeck@gmail.com>
2016-10-18 21:42:14 +0200

    take into account Maciek's comments on course::string

--- course4/homework-string/string.hh
+++ course4/homework-string/string.hh
@@ -8,29 +8,32 @@ namespace course {
 class string
 {
 public:
-	string() : string("") {}
-	string(const char* init_str) : my_ptr(new char[std::strlen(init_str)+1])
+	string() {}
+	string(const char* init_str)
 	{
-		std::strcpy(my_ptr.get(), init_str);
+		if (std::strcmp(init_str, "") != 0)
+		{
+			my_ptr = std::make_unique<char[]>(std::strlen(init_str)+1);
+			std::strcpy(my_ptr.get(), init_str);
+		}
 	}
-	string(const string& other_string) : my_ptr(new char[other_string.size()+1])
+	string(const string& other_string) 
 	{
-		std::strcpy(my_ptr.get(), other_string.my_ptr.get());
-	}
-	string(const char* init_str1, const char* init_str2) : my_ptr(new char[std::strlen(init_str1)+std::strlen(init_str2)+1])
-	{
-		std::strcpy(my_ptr.get(), init_str1);
-		std::strcat(my_ptr.get(), init_str2);
+		if (!other_string.empty())
+		{
+			my_ptr = std::make_unique<char[]>(other_string.size()+1);
+			std::strcpy(my_ptr.get(), other_string.my_ptr.get());
+		}
 	}
 
 	bool empty() const
 	{
-		return size()==0;
+		return !my_ptr;
 	}
 
 	int size() const
 	{
-		return std::strlen(my_ptr.get());
+		return empty() ? 0 : std::strlen(my_ptr.get());;
 	}
 
 	int length() const
@@ -40,14 +43,15 @@ public:
 
 	void clear()
 	{
-		char* new_ptr = new char[1];
-		std::strcpy(new_ptr, "");
-		my_ptr.reset(new_ptr);
+		my_ptr.reset();
 	}
 
 	bool operator==(const string& other_string) const
 	{
-		return std::strcmp(my_ptr.get(), other_string.my_ptr.get()) == 0;
+		if (empty() || other_string.empty())
+			return empty() && other_string.empty();
+		else
+			return std::strcmp(my_ptr.get(), other_string.my_ptr.get()) == 0;
 	}
 
 	char operator[](std::size_t idx) const
@@ -67,43 +71,59 @@ public:
 
 	string operator+(const string& other_string) const
 	{
-		return string(my_ptr.get(), other_string.my_ptr.get());
+		if (empty())
+			return string(other_string);
+		else if (other_string.empty())
+			return string(*this);
+		else
+			return string(my_ptr.get(), other_string.my_ptr.get());
 	}
 
 	string& operator+=(const string & other_string)
 	{
 		if (!other_string.empty())
 		{
-			char* new_ptr = new char[size()+other_string.size()+1];
-			std::strcpy(new_ptr, my_ptr.get());
-			std::strcat(new_ptr, other_string.my_ptr.get());
-			my_ptr.reset(new_ptr);
+			std::unique_ptr<char[]> new_ptr = std::make_unique<char[]>(size()+other_string.size()+1);
+			std::strcpy(new_ptr.get(), my_ptr.get());
+			std::strcat(new_ptr.get(), other_string.my_ptr.get());
+			my_ptr = std::move(new_ptr);
 		}
 		return *this;
 	}
 
 	char* c_str() const
 	{
-		return my_ptr.get();
+		if (empty())
+			return "";
+		else
+			return my_ptr.get();
 	}
 
 	string& operator=(const string& other_string)
 	{
 		if (this != &other_string)
 		{
-			my_ptr.reset(new char[other_string.size()+1]);
-			std::strcpy(my_ptr.get(), other_string.my_ptr.get());
+			if (other_string.empty())
+			{
+				my_ptr.reset();
+			}
+			else
+			{
+				my_ptr = std::make_unique<char[]>(other_string.size()+1);
+				std::strcpy(my_ptr.get(), other_string.my_ptr.get());
+			}
 		}
 		return *this;
 	}
 
-	bool operator<(const string& other_string)
-	{
-		return std::strcmp(my_ptr.get(), other_string.my_ptr.get()) < 0;
-	}
-
 private:
 	std::unique_ptr<char[]> my_ptr;
+
+	string(const char* init_str1, const char* init_str2) : my_ptr(std::make_unique<char[]>(std::strlen(init_str1)+std::strlen(init_str2)+1))
+	{
+		std::strcpy(my_ptr.get(), init_str1);
+		std::strcat(my_ptr.get(), init_str2);
+	}
 };
 
 std::ostream& operator<<(std::ostream& s, const string& strng);
