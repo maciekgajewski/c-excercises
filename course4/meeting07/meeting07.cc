ee7a7c5627a0674838053d4de719c9e3ae9713a6
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-10-20 17:46:23 +0200

    variadic templates

--- course4/meeting07/meeting07.cc
+++ course4/meeting07/meeting07.cc
@@ -1,31 +1,50 @@
 #include <iostream>
 #include <string>
 
-template<typename T>
-struct PtrDetector
+void print()
 {
-	static const bool IsPointer = false;
-	using WithoutPtr = T;
-};
-
-template<typename P>
-struct PtrDetector<P*>
-{
-	static const bool IsPointer = true;
-	using WithoutPtr = P;
-};
-
-template<typename V>
-void foo(V v)
-{
-	using VNotPtr = PtrDetector<int>::WithoutPtr;
+	std::cout << std::endl;
 }
+
+
+template<typename Arg0, typename... Args>
+void print(const Arg0& arg0, const Args&... aa)
+{
+	std::cout << arg0;
+	print(aa...);
+}
+
+double add()
+{
+	return 0;
+}
+
+template<typename Arg0, typename... Args>
+double add(const Arg0& arg0, const Args&... aa)
+{
+	return arg0 + add(aa...);
+}
+
 
 int main()
 {
-	auto b1 = PtrDetector<int>::IsPointer;
-	auto b2 = PtrDetector<int*>::IsPointer;
+ 	print(7, "Hello", 6.6);
+	std::cout << 7;
+	std::cout << "Hello";
+	std::cout << 6.6;
+	std::cout << std::endl;
 	
-	using T1 = PtrDetector<int>::WithoutPtr;
-	using T2 = PtrDetector<int*>::WithoutPtr;
+	
+	std::cout << add(7, 8, 9.9, 0) <<std::endl;
+	
+	double x = 6.6;
+	
+	print("addr of x = " , &x);
+	
+	std::string s = "qwert";
+	print("length of ", s, " is ", s.length());
+	
+	print();
+	
+	
 }
