9d0c09a2d3f85360ea6cf9ca8be27949e64e2600
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-10-21 09:07:26 +0200

    Some more metaprogramming

--- course4/meeting07/meeting07.cc
+++ course4/meeting07/meeting07.cc
@@ -1,50 +1,40 @@
 #include <iostream>
 #include <string>
 
-void print()
+void stupidWrite(char b)
 {
-	std::cout << std::endl;
+	std::cout << int(b) << std::endl;
 }
 
 
-template<typename Arg0, typename... Args>
-void print(const Arg0& arg0, const Args&... aa)
-{
-	std::cout << arg0;
-	print(aa...);
-}
 
-double add()
+template<typename T1, typename T2>
+struct Buffer
 {
-	return 0;
-}
+	char bytes[sizeof(T)];
+	
+	void Set(const T1& v)
+	{
+		T1* asT = reinterpret_cast<T1*>(bytes);
+		*asT = v;
+	}
+	
+	void Send() const
+	{
+		for(int i = 0; i < sizeof(T1); i++)
+		{
+			stupidWrite(bytes[i]);
+		}
+	}
+};
 
-template<typename Arg0, typename... Args>
-double add(const Arg0& arg0, const Args&... aa)
-{
-	return arg0 + add(aa...);
-}
 
 
 int main()
 {
- 	print(7, "Hello", 6.6);
-	std::cout << 7;
-	std::cout << "Hello";
-	std::cout << 6.6;
-	std::cout << std::endl;
+	Buffer<int, double> b;
 	
-	
-	std::cout << add(7, 8, 9.9, 0) <<std::endl;
-	
-	double x = 6.6;
-	
-	print("addr of x = " , &x);
-	
-	std::string s = "qwert";
-	print("length of ", s, " is ", s.length());
-	
-	print();
-	
-	
+	b.Set(77);
+	b.Send();
+
 }
