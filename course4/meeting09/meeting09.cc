d4b5acc2cf4bf1740db913f6577c2f415b93f74d
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-11-03 17:11:29 +0100

    intro to vector

--- course4/meeting09/meeting09.cc
+++ course4/meeting09/meeting09.cc
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
 
 int main(int argc, char** argv)
 {
 	std::cout << "Hello, World!" << std::endl;
+	
+	std::vector<std::string> s(argv, argv+argc);
+	std::cout << "size=" << s.size() << ", capacity=" << s.capacity() << std::endl;
+	
+	std::list<std::string> l(s.rbegin(), s.rend());
+	
+	for(const std::string& str : s)
+	{
+		std::cout << s << std::endl;
+	}
+	
+// 	for(int i = 0; i < argc; i++)
+// 	{
+// 		//std::cout << s[i] << std::endl;
+// 		std::cout << s.at(i) << std::endl;
+// 	}
+	
+// 	s.reserve(argc);
+// 	for(int i = 0; i < argc; i++)
+// 	{
+// 		s.push_back(argv[i]);
+// 		std::cout << "size=" << s.size() << ", capacity=" << s.capacity() << std::endl;
+// 	}
 }
+
