51b713fe2adb079619a2d955599cd2ccd1e1b95a
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-11-22 18:03:09 +0100

    asserts

--- course4/meeting11/meeting11.cc
+++ course4/meeting11/meeting11.cc
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <stdexcept>
 #include <memory>
+#include <cassert>
+
+struct Price
+{
+	int mValue;
+	std::string currency;
+	
+};
+static_assert(sizeof(Price) == sizeof(int), "Price should be the size of one integer");
 
 int divide(int a, int b)
 {
+	assert(b != 0 && "Attempt to divide by zero");
 	if (b != 0)
 		return a/b;
 	else
 		throw std::runtime_error("Divison by zero");
-	
 }
 
 struct S
@@ -17,13 +26,25 @@ struct S
 	~S() { std::cout << "S destroyed" << std::endl; }
 };
 
+void log_exception()
+{
+	std::cerr << "Error in divide_strings" << std::endl;
+	throw;
+}
+
 // convers c-strings to int and divide
 int divide_strings(const char* a, const char* b)
 {
-	auto s = std::make_unique<S>();
-	int res = divide(std::stoi(a), std::stoi(b));
-	std::cout << "foo!" << std::endl;
-	return res;
+	try{
+		auto s = std::make_unique<S>();
+		int res = divide(std::stoi(a), std::stoi(b));
+		std::cout << "foo!" << std::endl;
+		return res;
+	}
+	catch(...)
+	{
+		log_exception();
+	}
 }
 
 int main(int argc, char** argv)
@@ -39,14 +60,9 @@ int main(int argc, char** argv)
 		std::cout << divide_strings(argv[1], argv[2]) << std::endl;
 		std::cout << "boo!" << std::endl;
 	}
-	catch(const std::exception& e)
+	catch(const std::logic_error& e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
-		return 1;
-	}
-	catch(const std::runtime_error& re)
-	{
-		std::cerr << "Runtime error: " << re.what() << std::endl;
 		return 1;
 	}
 	
