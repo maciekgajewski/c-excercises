bf766f17d8354630f01f3fde254a0356af542d77
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-11-22 16:46:24 +0100

    Simple catching

--- course4/meeting11/meeting11.cc
+++ course4/meeting11/meeting11.cc
@@ -1,10 +1,20 @@
 #include <iostream>
-
+#include <stdexcept>
+#include <memory>
 
 int divide(int a, int b)
 {
-	return a/b;
+	if (b != 0)
+		return a/b;
+	else
+		throw std::runtime_error("Divison by zero");
 }
+
+struct S
+{
+	S() { std::cout << "S created" << std::endl; }
+	~S() { std::cout << "S destroyed" << std::endl; }
+};
 
 int main(int argc, char** argv)
 {
@@ -14,7 +24,18 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	
-	std::cout << divide(std::stoi(argv[1]), std::stoi(argv[2])) << std::endl;
+	try
+	{
+		auto s = std::make_unique<S>();
+		std::cout << divide(std::stoi(argv[1]), std::stoi(argv[2])) << std::endl;
+		std::cout << "boo!" << std::endl;
+	}
+	catch(...)
+	{
+		std::cerr << "Error!" << std::endl;
+		return 1;
+	}
+	
 	return 0;
 }
 
