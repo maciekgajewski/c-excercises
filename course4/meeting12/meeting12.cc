b8048c377f1aea5112eba3b56770538ca3939111
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-12-01 17:27:41 +0100

    virtual

--- course4/meeting12/meeting12.cc
+++ course4/meeting12/meeting12.cc
@@ -9,7 +9,7 @@ public:
 
 	using NameType = std::string;
 
-	void print() const
+	virtual void print() const
 	{
 		std::cout << "Person: " << mName << ", age: " << mAge << std::endl;
 		std::cout << "this=" << this << std::endl;
@@ -19,24 +19,51 @@ public:
 	
 	Person(const NameType& name, int age) : mName(name), mAge(age) {}
 
+	virtual bool yes() = 0;
+	
 private:
 	
 	NameType mName;
 	int mAge;
 };
 
+
+
 class Employee : public Person
 {
+public:
+	
+	Employee(const std::string& p, const NameType& n, int a) : Person(n, a), mPosition(p) {}
+	
+	void print() const override final
+	{
+		std::cout << "Employee: position: " << mPosition << " ";
+		Person::print();
+	}
+	
+	virtual void paySalary() { std::cout << "yay!" << std::endl; }
+	
 private:
 	
 	NameType mPosition;
 };
+
+void report(const Person& person)
+{
+	std::cout << "=== REPORT ==" << std::endl;
+	person.print();
+	std::cout << "=============" << std::endl;
+}
 
 int main(int argc, char** argv)
 {
 	Person p("Maciek", 36);
 	p.print();
 	
-	Employee e;
+	Employee e("QT guy", "Ilya", 28);
+	e.print();
+
+	report(p);
+	report(e);
 }
 
