2054d068d50e0ebf78bcb81801d79819982772e3
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-12-01 17:45:13 +0100

    dyanmic cast, multiple inheritance

--- course4/meeting12/meeting12.cc
+++ course4/meeting12/meeting12.cc
@@ -18,22 +18,25 @@ public:
 	}
 	
 	Person(const NameType& name, int age) : mName(name), mAge(age) {}
-
-	virtual bool yes() = 0;
-	
 private:
 	
 	NameType mName;
 	int mAge;
 };
 
-
-
-class Employee : public Person
+class Record
 {
 public:
-	
-	Employee(const std::string& p, const NameType& n, int a) : Person(n, a), mPosition(p) {}
+	Record(int id) : mId(id) {}
+	int GetId() const { return mId; }
+private:
+	int mId;
+};
+
+class Employee : public Person, public Record
+{
+public:
+	Employee(const std::string& p, const NameType& n, int a, int id) : Person(n, a), Record(id), mPosition(p) {}
 	
 	void print() const override final
 	{
@@ -42,9 +45,7 @@ public:
 	}
 	
 	virtual void paySalary() { std::cout << "yay!" << std::endl; }
-	
 private:
-	
 	NameType mPosition;
 };
 
@@ -52,6 +53,14 @@ void report(const Person& person)
 {
 	std::cout << "=== REPORT ==" << std::endl;
 	person.print();
+	
+	const Employee* e = dynamic_cast<const Employee*>(&person);
+	if (e) 
+	{
+		std::cout << "this is an Employee, BTW" << std::endl;
+	}
+	const Employee& er = dynamic_cast<const Employee&>(person);
+	
 	std::cout << "=============" << std::endl;
 }
 
@@ -60,9 +69,18 @@ int main(int argc, char** argv)
 	Person p("Maciek", 36);
 	p.print();
 	
-	Employee e("QT guy", "Ilya", 28);
+	Employee e("QT guy", "Ilya", 28, 666);
 	e.print();
 
+	std::cout << "Ilya's ID=" << e.GetId() << std::endl;
+	
+	std::cout << "&e=" << &e << std::endl;
+	std::cout << "&e as person=" << static_cast<Person*>(&e) << std::endl;
+	std::cout << "&e as record=" << static_cast<Record*>(&e) << std::endl;
+	Record* r = reinterpret_cast<Record*>(&e);
+	std::cout << "r=" << r << std::endl;
+	std::cout << "r->id=" << r->GetId() << std::endl;
+	
 	report(p);
 	report(e);
 }
