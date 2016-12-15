65db1ceffa1411b0b901194cdfa732815621c423
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-12-15 17:17:21 +0100

    boost::any

--- course4/meeting14/meeting14.cc
+++ course4/meeting14/meeting14.cc
@@ -1,8 +1,11 @@
 #include <boost/optional.hpp>
+#include <boost/variant.hpp>
+#include <boost/any.hpp>
 
 #include <random>
 #include <iostream>
 #include <string>
+#include <tuple>
 
 using namespace std::literals;
 
@@ -24,8 +27,15 @@ boost::optional<std::string> get_greeting()
 
 int main(int argc, char** argv)
 {
-	auto greeting = get_greeting();
-	if (greeting)
-		std::cout << *greeting << std::endl;
+// 	auto greeting = get_greeting();
+// 	if (greeting)
+// 		std::cout << *greeting << std::endl;
+	
+	boost::any a = 6.6;
+	auto b = a;
+	
+	int x = boost::any_cast<int>(b);
+	std::cout << x << std::endl;
+	
 }
 
