3ffd14b3efa63e691a8e16b5c8e632a478dc4b9a
arccth <arccth@gmail.com>
2016-12-19 22:47:30 +0100

    First version of matching engine

---
+++ course4/homework-matching-engine/main.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+#include "models.h"
+#include <sstream>
+
+int main()
+{
+    course::MatchingEngine mathingEngine;
+    std::string input;
+    while(std::getline(std::cin, input))
+    {
+        std::stringstream ss;
+        ss.str(input);
+        std::string command;
+        ss >> command;
+        if(command == "insert")
+        {
+              course::Order order;
+              std::string side;
+              ss >> side;
+              order.side = side == "buy" ? course::Buy : course::Sell;
+              ss >> order.instrument >> order.id >> order.price >> order.volume;
+              if(ss.fail())
+              {
+                  std::cout << "input error" << std::endl;
+                  continue;
+              }
+
+              mathingEngine.InsertOrder(order);
+        }
+        else if(command == "delete")
+        {
+            std::string instrument;
+            unsigned long orderId;
+            ss >> instrument >> orderId;
+            if(ss.fail())
+            {
+                std::cout << "input error" << std::endl;
+                continue;
+            }
+
+            mathingEngine.DeleteOrder(instrument, orderId);
+        }
+        else
+        {
+            std::cout << "input error" << std::endl;
+        }
+    }
+
+}
