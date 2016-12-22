69a0af18bc13cb39c98bef625c570416e6ea3a1e
Maciek Gajewski <maciej.gajewski0@gmail.com>
2016-12-22 17:29:39 +0100

    simple interthread using cond var

--- course4/meeting15/meeting15.cc
+++ course4/meeting15/meeting15.cc
@@ -7,36 +7,68 @@
 #include <atomic>
 #include <array>
 #include <future>
+#include <condition_variable>
+#include <mutex>
 
 using namespace std::literals;
 namespace bi = boost::interprocess;
 
 const char* SHM = "course-shm";
 
+template<typename T>
+class communicator
+{
+public:
+	
+	void set(const T& v)
+	{
+		{
+			std::lock_guard<std::mutex> l(mutex_);
+			buffer_ = v;
+		}
+		cv_.notify_all();
+	}
+	
+	T wait()
+	{
+		std::unique_lock<std::mutex> l(mutex_);
+		cv_.wait(l);
+		return buffer_;
+	}
+	
+private:
+	
+	std::mutex mutex_;
+	T buffer_;
+	//int wrtie_Seq
+	std::condition_variable cv_;
+};
+
 int main(int argc, char** argv)
 {
 	std::cout << "start" << std::endl;
-	int x = 0;
+	communicator<int> c;
 
-	std::future<int> res = std::async(std::launch::async, [&] 
+	std::thread t([&] 
 	{
-		for(int i = 0; i < 10; i++)
+		while(true)
 		{
-			std::cout << "another thread: " << i << ", x=" << x << std::endl;
-			std::this_thread::sleep_for(500ms);
+			int v = c.wait();
+			std::cout << "got: " << v << std::endl;
+			if (v == -1)
+				break;
 		}
-		return 66;
 	});
 
-		for(int i = 0; i < 5; i++)
-		{
-			std::cout << "main thread: " << i << std::endl;
-			x = i;
-			std::this_thread::sleep_for(500ms);
-		}
+	for(int i = 0; i < 5; i++)
+	{
+		std::cout << "main thread: " << i << std::endl;
+		std::this_thread::sleep_for(500ms);
+		c.set(i);
+	}
+	c.set(-1);
 	
-	
-	std::cout << "result = " << res.get() << std::endl;
+	t.join();
 	
 	std::cout << "end" << std::endl;
 }
