9905116f5b61d6aed42a237d0b744636a2b926db
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-02-02 17:46:52 +0100

    the operator&

--- course5/meeting01/meeting01.cc
+++ course5/meeting01/meeting01.cc
@@ -1,8 +1,19 @@
 #include <iostream>
 
+double global = 6.6;
+
 int main(int argc, char** argv)
 {
-	std::cout << "Hello, world!" << std::endl;
+	int x = 7;
+	float y = 5.6;
+	x = y;
+	std::cout << "x=" << x << std::endl;
 	
+	int* xp = &x;
+	float* yp = &y;
+	
+	std::cout << "xp=" << xp << std::endl;
+	std::cout << "yp=" << yp << std::endl;
+	std::cout << "&global=" << &global << std::endl;
 }
 
