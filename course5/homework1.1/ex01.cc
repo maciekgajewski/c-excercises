eb8a30281be88daf5d7c28f6e8920ddc699d256a
anilsukul <anil_sukul@hotmail.com>
2017-02-06 23:38:34 +0100

    Add files via upload

--- course5/homework1.1/ex01.cc
+++ course5/homework1.1/ex01.cc
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+// ARS - 20170206
+// qsort example from qsort manual
+
+static int cmpstringp(const void *p1, const void *p2)
+{
+   /* The actual arguments to this function are "pointers to
+              pointers to char", but strcmp(3) arguments are "pointers
+              to char", hence the following cast plus dereference */
+
+   return strcmp(* (char * const *) p1, * (char * const *) p2);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc > 1)
+    {
+	qsort(&argv[1], argc - 1, sizeof(char *), cmpstringp);
+	for (int i = 1; i < argc; i ++)
+	{
+		std::cout << argv[i] << std::endl;
+	}
+    }
+    return 0;
+}
 
