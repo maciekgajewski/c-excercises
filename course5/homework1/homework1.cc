bb615cc974a11d4a3a93c7fdc182f87a784adcc2
ktii <koj@nurfuerspam.de>
2017-02-08 17:55:51 +0100

    fixed warnings cmake/make were hiding somehow

--- course5/homework1/homework1.cc
+++ course5/homework1/homework1.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-  for (size_t i = 0; i < argc; i++) {
+  for (int i = 0; i < argc; i++) {
     std::cout << i << std::endl;
   }
   return 0;
