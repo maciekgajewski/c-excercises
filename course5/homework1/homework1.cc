9571bf510ee6e75c806704d54008060ff3cb50c3
Jens de Smit <jfdsmit@gmail.com>
2017-02-09 15:17:54 +0100

    restoring state of homework1 for homework1.1 pull request

--- course5/homework1/homework1.cc
+++ course5/homework1/homework1.cc
@@ -1,9 +1,1 @@
-#include <iostream>
-
-int main(int argc, char** argv)
-{
-        for (auto i = 0; i < argc; i++)
-        {
-                std::cout << i << std::endl;
-        }
-}
+// your code goes here
