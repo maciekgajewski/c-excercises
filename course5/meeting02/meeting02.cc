f3933705a6e4b7c915727eb60d78409dfe0eadc1
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-02-09 16:34:29 +0100

    pointers again

--- course5/meeting02/meeting02.cc
+++ course5/meeting02/meeting02.cc
@@ -1,13 +1,34 @@
 #include <iostream>
 
+int fun()
+{
+	int i;
+	if (i == 42)
+		return 1;
+	else
+		return 7;
+}
+
 int main(int argc, char** argv)
 {
 	std::cout << "Hello again!" << std::endl;
 
 	float x = 7;
 	int y;
+	int g = 88;
 	
-	auto p = &y;
+	auto p = &y; // int*
+	y = 5;
+	
+	const int z = y;
+	//z = 4; ERROR
+	auto p2 = &z; // const int*
+	//*p2 = 6; ERROR
+	p2 = &y;
+	
+	int* const p3 = &y;
+	*p3 = 8;
+	//p3 = &g;// ERROR
 	
 }
 
