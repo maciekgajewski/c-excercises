ede748368442b11a003341205e13b52c44a76f42
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-02-09 18:02:56 +0100

    references

--- course5/meeting02/meeting02.cc
+++ course5/meeting02/meeting02.cc
@@ -1,28 +1,20 @@
 #include <iostream>
 
-struct S
-{
-};
 
 int main(int argc, char** argv)
 {
 	int i = 6;
-	int* p = &i;
+	int& r = i;
+	auto x = r;
 	
-	float f = 6.66;
-	float* pf = &f;
+	r = 7;
 	
-	*p = 77;
-	*pf = *p; // convertion int -> float
+	int x = r;
 	
-	float* p2 = reinterpret_cast<float*>(&i);
+	auto p = &r;
 	
-	//*p2 = 77;
-	
-	//S s = *p2;
-	
-	std::cout << "i= " << i << std::endl;
-	std::cout << "*p2= " << *p2 << std::endl;
-	std::cout << "f= " << f << std::endl;
+	std::cout << "i=" << i << std::endl;
+	std::cout << "r=" << r << std::endl;
+	std::cout << "*p=" << *p << std::endl;
 }
 
