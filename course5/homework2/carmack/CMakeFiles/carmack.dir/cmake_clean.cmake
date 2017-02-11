5045c9e461f9660518ed839deb6c28b21cd1dfeb
Balazs <Balazs Meszena>
2017-02-11 12:41:08 +0100

    homework2

---
+++ course5/homework2/carmack/CMakeFiles/carmack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/carmack.dir/carmack.cc.o"
+  "carmack.pdb"
+  "carmack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/carmack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ course5/homework2/carmack/carmack.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+#include <time.h>
+
+float inv_sqrt( float x )
+{
+    int i;
+    float x2 = 0.5 * (x);
+    float y = x;
+    const int magic = 0x5f3759df;
+
+    float* p = &y;
+    int* p2 = reinterpret_cast<int*>(p);
+    i = *p2;
+    i = magic - ( i >> 1);
+
+    y = *reinterpret_cast<float*>(&i);
+    y = y * ( 1.5 - ( x2 *y *y) );
+ //   y = y * ( 1.5 - ( x2 *y *y) );
+    return y;
+}
+
+int main(int argc, char** argv)
+{
+    float x = std::atof(argv[1]);
+    clock_t t;
+    t = clock();
+    std::cout << "Carmack_sqrt" << argv[1] << "=" << 1/inv_sqrt(x) << std::endl;
+    t = clock() - t;
+    std::cout << "Carmack_time" << argv[1] << "=" << t << std::endl;
+    t = clock();
+    std::cout << "math_sqrt"  << "=" << sqrt(x) << std::endl;
+    t = clock() - t;
+    std::cout << "math_time"  << "=" << t << std::endl;
+}
