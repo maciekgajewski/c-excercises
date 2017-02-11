608f36aca7acf4a4996a39495af18cc18cec70a5
Balazs <Balazs Meszena>
2017-02-11 15:44:50 +0100

    stack information display added

--- course5/homework2/newton/newton.cc
+++ course5/homework2/newton/newton.cc
@@ -1,18 +1,30 @@
 #include <iostream>
 #include <stdio.h>
+#include <time.h>
 
 int i = 0;
 float x;
+bool* start = nullptr;
+bool* end = nullptr;
 
-float function( float y, int it )
+float function( float y, int it)
 {
+    if( i == 0)
+    {
+        bool a;
+        start = &a;
+    }
+    else if( i == it )
+    {
+        bool a;
+        end = &a;
+    }
+
     while( i < it)
     {
         i++;
         x -= ( x * x - y )/(2 * x);
-        std::cout << x << std::endl;
-        std::cout << i << std::endl;
-        function(y, it);
+        function( y, it);
     }
 
     return x;
@@ -21,9 +33,25 @@ float function( float y, int it )
 
 int main(int argc, char** argv)
 {
-    const float y = std::atof(argv[1]);
-    x = std::atof(argv[2]);
-    const int it = std::atoi(argv[3]);
+    const float y = std::atof(argv[1]); // input number
+    x = std::atof(argv[2]); //initial guess
+    const int it = std::atoi(argv[3]); // number of iterations
 
-    std::cout << function(y, it) << std::endl;
+    clock_t t = clock();
+    std::cout << "sqrt of " << argv[1] << " is " << function(y, it) << std::endl;
+    t = clock() - t;
+
+    std::cout << "depth" << "=" << i + 1 << std::endl;
+    int size = (start - end)/i;
+    std::cout << "size of stack frame" << "=" << (start - end)/i << std::endl;
+    if (size > 0)
+    {
+        std::cout << "stack grows downwards" << std::endl;
+    }
+    else if (size < 0)
+    {
+        std::cout << "stack grows upwards" << std::endl;
+    }
+
+    std::cout << "calculation time: " << t << std::endl;
 }
