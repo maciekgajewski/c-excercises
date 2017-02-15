79b30d03c74b178d7bd1412a99789ff134174fbd
Sophia Davis <scdgrapefruit@gmail.com>
2017-02-15 21:54:22 +0100

    Ignore the binary name

--- course5/homework1.1/homework1.1.cc
+++ course5/homework1.1/homework1.1.cc
@@ -6,14 +6,12 @@ int main(int argc, char** argv)
 {
     std::list<std::string> arg_list;
 
-    for (int i = 0; i < argc; i++)
+    for (int i = 1; i < argc; i++) // ignore argv[0], the binary
     {
         arg_list.push_back(argv[i]);
     }
 
-    arg_list.pop_front(); // remove binary name
     arg_list.sort();
-
     for (auto a : arg_list) {
         std::cout << a << std::endl;
     }
