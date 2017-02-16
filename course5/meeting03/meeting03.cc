f85c16ed5c0a6d5dc1dfe044086501f9aaa0d5f9
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-02-16 17:46:47 +0100

    function pointers

--- course5/meeting03/meeting03.cc
+++ course5/meeting03/meeting03.cc
@@ -1,44 +1,31 @@
 #include <iostream>
 #include <string>
 
-namespace course
+double foo()
 {
-
-double foo(int x)
-{
-	std::cout << "I'm course::foo, and x=" << x << std::endl;
-	return x * 3;
+	std::cout << "I'm foo" << std::endl;
+	return 3.3;
 }
 
-}
-
-// int foo(int i)
-// {
-// 	std::cout << "I'm foo, and i=" << i << std::endl;
-// 	if (i > 0)
-// 		return i*2.0;
-// 	else
-// 		return i;
-// }
-
-namespace course 
+double bar()
 {
-	namespace inner
-	{
-		
-		double bar()
-		{
-			return foo(6);
-		}
-	}
+	std::cout << "bar" << std::endl;
+	return 6.6;
+}
 	
-	
-}
 
+void hey(int a, int b)
+{
+}
+	
 int main(int argc, char** argv)
 {
-	using namespace course;
+	double(*p1)() = &foo;
 	
-	auto x = ::foo(argc);
-	std::cout << "x = " << x << std::endl;
+	auto x = p1();
+	p1 = &bar;
+	std::cout << p1() << std::endl;
+	
+	p1 = &hey;
+	std::cout << p1() << std::endl;
 }
