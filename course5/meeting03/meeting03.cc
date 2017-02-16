80ea97fb4283aa660daeaf381173825554f3d06f
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-02-16 17:05:33 +0100

    End of part 1

--- course5/meeting03/meeting03.cc
+++ course5/meeting03/meeting03.cc
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
 
+struct Vector
+{
+	std::int32_t x;
+	std::int32_t y;
+};
+
 int main(int argc, char** argv)
 {
 	std::string h = "Hello, worl";
 	std::string w = "d!";
 	
-	std::cout << h << w << std::endl;
-}
+	
+	for(int i =0 ; i < 5; i++)
+	{
+		std::string s = std::to_string(i);
+	}
+	
 
+	if(w == "d!")
+	{
+		std::string x = "X";
+	}
+	
+	std::cout << h << w << std::endl;
+	std::cout << "The size of std::string is: " << sizeof(std::string) << std::endl;
+}
