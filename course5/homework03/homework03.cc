17c87b34e564377f0b15337ef986866ae4245312
Balazs <Balazs Meszena>
2017-02-18 12:28:59 +0100

    experimenting with templates

--- course5/homework03/homework03.cc
+++ course5/homework03/homework03.cc
@@ -1,5 +1,37 @@
 #include <cstdio>
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <type_traits>
 
+template <typename T>
+void print(T p, int prec = 4)
+{
+	auto a = p;
+	if ( std::is_same<T, int>::value | std::is_same<T, long>::value )
+	{
+		std::printf("%d\n", a);
+		std::printf("%s\n", "int");
+	} else if ( std::is_same<T, float>::value | std::is_same<T, double>::value )
+	{
+		std::printf("%f\n", a);
+		std::printf("%s\n", "float");
+	} else if ( std::is_same<T, std::string>::value )
+	{
+		std::printf("%s\n", a);
+		std::printf("%s\n", "string");
+	} else if ( std::is_same<T, bool>::value )
+	{
+		if (a == 1)
+		{
+			std::printf("%s\n", "true");
+		}
+	//	std::printf("%s\n", a ? "true" : "false");
+	} else
+	{
+		std::printf("%s\n", "unsupported type");
+	}
+}
 // TODO your code goes here
 
 int main()
@@ -9,10 +41,15 @@ int main()
 	print(true);// true
 	print(1.0/3.0, 2);// 0.33
 	
-	std::string s = "world!";
-	ptrint("hello, ") // hello, 
+	std::string s = "word!";
+	print("hello, "); // hello, 
 	print(s); // world!
-	
+
 	float f = 6;
 	print(f); // 6
+//	std::cout << typeid(typeid(7).name()).name() << std::endl;
+//	std::cout << typeid(3.3f).name() << std::endl;
+//	std::cout << typeid("hello4").name()[0] << std::endl;
+//	std::cout << typeid(ch).name() << std::endl;
+//	std::cout << typeid(true).name() << std::endl;
 }
