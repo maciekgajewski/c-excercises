484e50481e165368601283c52aec9565a48cb12e
anilsukul <anil_sukul@hotmail.com>
2017-02-20 22:57:04 +0100

    homework03

---
+++ course5/homework03/print.h
@@ -0,0 +1,11 @@
+#include <string>
+
+void print(int x);
+void print(long l);
+void print(bool b);
+void print(char x);
+void print(float f, int precision=0);
+void print(double d, int precision=0);
+void print(const char* str);
+void print(std::string s);
+
