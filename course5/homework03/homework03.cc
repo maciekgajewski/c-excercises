34c4811c899ede664b7d6042b362e25ccec2c2ed
AJIEIIIA <alexeyushka.popov@gmail.com>
2017-02-22 20:50:05 +0100

    Update homework03.cc
    implementation

--- course5/homework03/homework03.cc
+++ course5/homework03/homework03.cc
@@ -1,6 +1,32 @@
+// Example program
+#include <string>
 #include <cstdio>
 
-// TODO your code goes here
+void print(std::string value)
+{
+	std::printf("%s", value.c_str());
+}
+
+void print(const char* value)
+{
+	std::printf(value);   
+}
+
+void print(bool value)
+{
+	std::printf("%s", value ? "true" : "false");
+}
+
+void print(int value)
+{
+	std::printf("%d", value);
+}
+
+void print(float value, uint precision = 0)
+{
+    	std::string format = std::string("%.") + std::to_string(precision).c_str() + std::string("f");
+	std::printf(format.c_str(), value);
+}
 
 int main()
 {
