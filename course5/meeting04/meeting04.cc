afe46c6d2fbd9709b3baa9f9ce23e5f79e6837a4
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-02-23 17:50:39 +0100

    the big 6

--- course5/meeting04/meeting04.cc
+++ course5/meeting04/meeting04.cc
@@ -4,6 +4,56 @@
 class Vector
 {
 public:
+	
+	// Constructors
+	Vector() // default contructor
+	{
+		std::cout << "Vector::Vector()" << std::endl;
+	}
+	
+	Vector(const Vector& src) // copy
+	{
+		std::cout << "Vector::Vector(const Vector&)" << std::endl;
+		x = src.x;
+		y = src.y;
+	}
+	Vector(Vector&& src) // move
+	{
+		std::cout << "Vector::Vector(Vector&&)" << std::endl;
+		x = src.x;
+		y = src.y;
+	}
+	// user-defined constructors
+	Vector(int x, int y)
+	{
+		std::cout << "Vector::Vector(int, int)" << std::endl;
+		this->x = x;
+		this->y = y;
+	}
+	
+	// Destructor
+	~Vector()
+	{
+		std::cout << "Vector::~Vector()" << std::endl;
+	}
+	
+	// assignment operators
+	Vector& operator=(const Vector& src)
+	{
+		std::cout << "Vector::operator=(const Vector&)" << std::endl;
+		x = src.x;
+		y = src.y;
+		return *this;
+	}
+	
+	Vector& operator=(Vector&& src)
+	{
+		std::cout << "Vector::operator=(Vector&&)" << std::endl;
+		x = src.x;
+		y = src.y;
+		return *this;
+	}
+
 	void Init(int x, int y) // Vector::Init(Vector* this, int xx, int yy)
 	{
 		this->x = x;
@@ -15,8 +65,7 @@ public:
 	
 	Vector operator + (Vector rhs)
 	{
-		Vector v; 
-		v.Init(this->x + rhs.x, this->y + rhs.y);
+		Vector v(this->x + rhs.x, this->y + rhs.y); 
 		return v;
 	}
 	
@@ -38,12 +87,13 @@ void print(Vector vec)
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	Vector v; // constructor call
+	Vector v = {55, 77}; // constructor call
 	print(v);
-	v.Init(55, 77);
+	std::cout << "Checkpoint #1" << std::endl;
 	
-	Vector x;
-	x.Init(6, 8);
+	Vector x(6, 8);
+	v = x;
+	v = v+x;
 	
 	print(v + x);
 } // destructor 
