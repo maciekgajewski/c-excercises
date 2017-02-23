0f520dfd2c4fe22c3a02c1ba114c3e64e384bf4f
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-02-23 16:32:55 +0100

    declaration

--- course5/meeting04/meeting04.cc
+++ course5/meeting04/meeting04.cc
@@ -1,17 +1,37 @@
 #include <iostream>
 
+struct Vector;
+
+void print(const Vector& );
+
+struct Carrier;
+
+struct Plane
+{
+	Carrier* carrier;
+};
+
+struct Carrier
+{
+	std::vector<Plane> planes;
+};
+
+
 struct Vector
 {
 	int x;
 	int y;
 };
 
+
 int main(int /*argc*/, char** /*argv*/)
 {
-	Vector v = {4, 6};
-	Vector& r = v;
-	r.x = 8;
+	Vector v = {1, 55};
 	
-	std::cout << "Vector = {" << v.x << ", " << v.y << ")" << std::endl;
+	print(v);
 }
 
+void print(const Vector& vec)
+{
+	std::cout << "(" << vec.x << ", " << vec.y << ")" << std::endl;
+}
