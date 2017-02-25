4924ecab5c6c207fb77a97772d226e23636756d6
Balazs <Balazs Meszena>
2017-02-25 12:43:33 +0100

    homework4 done

--- course5/homework4/string.cc
+++ course5/homework4/string.cc
@@ -2,15 +2,115 @@
 #include <string>
 #include <stdexcept>
 #include <cstdio>
+#include <cstdlib>
+
+class Flush
+{
+public:
+        Flush(const char* end)
+        {
+            this->last = end;
+        }
+
+        const char* Get()
+        {
+            return this->last;
+        }
+
+private:
+        const char* last;
+};
 
 class Stream
 {
 public:
 
+        Stream()
+        {
+            throw std::runtime_error("No file or filename provided");
+        }
+
         Stream(const char* filename)
         {
             this->file = std::fopen(filename, "w");
+            if (!this->file)
+            {
+                throw std::runtime_error("File opening failed");
+            }
         }
+
+        Stream(std::FILE* file)
+        {
+            this->file = file;
+            if (!this->file)
+            {
+                throw std::runtime_error("File opening failed");
+            }
+        }
+
+        Stream(const Stream&)
+        {
+            throw std::runtime_error("Stream is not copyable");
+        }
+
+        Stream(Stream&&)
+        {
+            throw std::runtime_error("Stream is not movable");
+        }
+
+        Stream& operator=(const Stream&)
+        {
+            throw std::runtime_error("Stream is not copyable");
+        }
+
+        Stream& operator=(Stream&&)
+        {
+            throw std::runtime_error("Stream is not moveable");
+        }        
+
+        Stream& operator<<(const std::string& x)
+        {
+            std::fprintf(this->file, "%s", x.c_str());
+            return *this;
+        }
+
+        Stream& operator<<(const char* x)
+        {
+            std::fprintf(this->file, "%s", x);
+            return *this;
+        }
+
+        Stream& operator<<(int x)
+        {
+            std::fprintf(this->file, "%d", x);
+            return *this;
+        }
+
+        Stream& operator<<(long x)
+        {
+            std::fprintf(this->file, "%ld", x);
+            return *this;
+        }
+
+        Stream& operator<<(bool x)
+        {
+            std::fprintf(this->file, "%s", x ? "true" : "false");
+            return *this;
+        }
+
+        Stream& operator<<(double x)
+        {
+            std::fprintf(this->file, "%f", x);
+            return *this;
+        }
+
+        Stream& operator<<(Flush endl)
+        {
+            std::fprintf(this->file, "%s", endl.Get());
+            std::fflush(this->file);
+            return *this;
+        }
+
 
         ~Stream()
         {
@@ -19,9 +119,21 @@ public:
 
 private:
         std::FILE* file;
-}
+};
+
+Stream Cout(stdout);
+Flush Endl("\n");
 
 int main(int, char**)
 {
-    Stream f;
+    std::string w =  "World!";
+    Stream s("new_file.txt");
+    s << "Hello, " << w; // content does not appear in the file due to quick_exit
+    
+    Cout << "2 + 2 = " << 2 + 2 << "\n";
+
+    Stream f("text.txt");
+    f << "hey" << Endl; //content does appear in the file because of Endl
+    
+    std::quick_exit(1); //to test Endl
 }
