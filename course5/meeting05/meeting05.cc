2108d555ce97700c3149ef369654fef198f4d663
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-02 17:58:30 +0100

    error handling, enums

--- course5/meeting05/meeting05.cc
+++ course5/meeting05/meeting05.cc
@@ -1,35 +1,61 @@
 #include <iostream>
 #include <string>
 #include <cassert>
+#include <cstdio>
+#include <cstring>
 
-struct Person
+class File
 {
-	Person() = delete;
+public:
 	
-	Person(std::string n, unsigned a)
-		: name(n), age(a)
+	enum class Mode { READ, WRITE, APPEND };
+	
+	File(const std::string& path, Mode mode)
 	{
+		switch(mode)
+		{
+		case Mode::READ:
+			mFile = std::fopen(path.c_str(), "r");
+			break;
+		case Mode::WRITE:	
+			mFile = std::fopen(path.c_str(), "w");
+			break;
+		case Mode::APPEND:	
+			mFile = std::fopen(path.c_str(), "a");
+			break;
+		}
+		if (!mFile)
+		{
+			std::string msg = std::strerror(errno);
+			throw std::runtime_error("Failed to open a file:" + msg);
+		}
+	}
+	
+	~File()
+	{
+		std::fclose(mFile);
+	}
+	
+	void write(const std::string& s)
+	{
+		std::fprintf(mFile, "%s", s.c_str());
+	}
+	
+	void write(const char* s)
+	{
+		std::fprintf(mFile, "%s", s);
 	}
 
-	std::string GetName() const { return name; }
-	unsigned GetAge() const { return age; }
-	
-	void SetAge(unsigned a) { age = a; }
-	
 private:
-	std::string name;
-	unsigned age = 0;
+	
+	FILE* mFile;
 };
-
-std::ostream& operator<<(std::ostream& s, const Person& p)
-{
-	s << p.GetName() << " is " << p.GetAge() << " years old";
-	return s;
-}
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	Person p("Maciek", 36);
-	std::cout << p << std::endl;
+	File f("/output.txt", File::Mode::APPEND);
+	f.write("hey\n");
+	std::string str = "you!";
+	f.write(str);
 }
 
