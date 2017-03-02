726944712947f85f034d92e4b1435461e9cd043c
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-02 16:39:56 +0100

    create/assign/destroy excercise

--- course5/meeting05/meeting05.cc
+++ course5/meeting05/meeting05.cc
@@ -6,10 +6,16 @@
 int main(int /*argc*/, char** /*argv*/)
 {
 	std::string a; // default constructor std::string::string()
-	std::string b = "Hello"; // std::string::string(const char*)
+	std::string b = "Hello"; // std::string::string(const char*)	
 	std::string c = b; // copy-construction
 	
-	((std::cout << b) << " world!") << std::endl;
+	c = "12345";
+	
+	b = c;
+	
+	a = b + c; // +, std::string created, a.operator=(std::string&&), ~std::string
+
+	
 	
 }
 
