530633b77ebc1eab4de40ea02e2875a590c91ca6
Josef Schneider <chnaideur@gmail.com>
2017-03-05 21:26:53 +0100

    Re-adding homework03.cc for cleaner future pull requests

--- course5/homework03/homework03.cc
+++ course5/homework03/homework03.cc
@@ -1,26 +1,6 @@
-#include <string>
 #include <cstdio>
 
-void print(int value)
-{
-	std::printf("%d\n", value);
-}
-
-void print(std::string value)
-{
-	std::printf("%s\n", value.c_str());
-}
-
-void print(bool value)
-{
-	value ? std::printf("true\n") : std::printf("false\n");
-}
-
-// The ANSI C standard specifies that the default precision is 6
-void print(double value, int precision=6)
-{
-	std::printf("%.*f\n", precision, value);
-}
+// TODO your code goes here
 
 int main()
 {
@@ -28,11 +8,11 @@ int main()
 	print(7); // 7
 	print(true);// true
 	print(1.0/3.0, 2);// 0.33
-
+	
 	std::string s = "world!";
 	print("hello, ") // hello, 
 	print(s); // world!
-
+	
 	float f = 6;
 	print(f); // 6
 }
