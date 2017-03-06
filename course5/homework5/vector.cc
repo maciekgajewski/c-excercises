231b4db3df032aa71a7c8cf397d4c898cfe429f3
Balazs <Balazs Meszena>
2017-03-06 21:02:24 +0100

    main function as separate file

--- course5/homework5/vector.cc
+++ course5/homework5/vector.cc
@@ -103,29 +103,3 @@ namespace vector{
     }
 
 }
-
-int main(int, char**)
-{
-    using namespace vector;
-
-    auto v = 1.0_x + 2.2_y;
-
-    std::cout << "v = "<< v << std::endl;
-    
-    auto  u = 2*45.0_phi;
-
-    std::cout << "u = " << u << std::endl;
-
-    u.SetY(8.0);
-
-    std::cout << "u = " << u << std::endl;
-
-    v += u;
-
-    std::cout << "v = " << v << std::endl;
-
-    std::cout << "v == u: " << (v == u) << std::endl;
-
-    std::cout << "v != u: " << (v != u) << std::endl;
-    
-}
