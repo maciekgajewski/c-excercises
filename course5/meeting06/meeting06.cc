d433765701cbc369dab40470011b6c9ef0bae193
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-09 16:49:01 +0100

    the most basic unique ptr

--- course5/meeting06/meeting06.cc
+++ course5/meeting06/meeting06.cc
@@ -1,8 +1,61 @@
 #include <iostream>
+#include <memory>
+
+class Vector
+{
+public:
+	Vector() = default;
+	Vector(double a, double b)
+	{
+		x = a;
+		y = b;
+	}
+
+	double x = 0;
+	double y = 0;
+};
+
+class Vector3D : public Vector
+{
+public:
+	
+	Vector3D(double xx, double yy, double zz)
+	{
+		x = xx;
+		y = yy;
+		z = zz;
+	}
+	
+	double z = 0;
+}
+
+std::unique_ptr<Vector> MakeVector(double x, double y)
+{
+	return std::make_unique<Vector>(x, y);
+}
 
 int main(int /*argc*/, char** /*argv*/)
 {
 	std::cout << "Hello, world!" << std::endl;
+
+	std::unique_ptr<Vector> p = std::make_unique<Vector>();
+	std::cout << "vector: " << p->x << ", " << p->y << std::endl;
 	
+	Vector v2 = *p;
+	
+	p = std::make_unique<Vector3D>(6, 6, 7);
+	std::cout << "vector: " << p->x << ", " << p->y << std::endl;
+	
+	p = MakeVector(7, 8);
+	std::cout << "vector: " << p->x << ", " << p->y << std::endl;
+	
+	Vector* raw = p.get();
+	
+	std::unique_ptr<Vector> p2 = std::move(p);
+	
+	if(p2)
+	{
+		std::cout << "vector2: " << p2->x << ", " << p2->y << std::endl;
+	}
 }
 
