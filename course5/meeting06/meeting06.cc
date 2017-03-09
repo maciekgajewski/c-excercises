ea35ef165770fc0ecc317ef22a6468a377842389
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-09 17:23:23 +0100

    leak and exceptions

--- course5/meeting06/meeting06.cc
+++ course5/meeting06/meeting06.cc
@@ -34,16 +34,30 @@ std::unique_ptr<Vector> MakeVector(double x, double y)
 	return std::make_unique<Vector>(x, y);
 }
 
+void PrintVector(const Vector& v)
+{
+	std::cout << "vector: " << v.x << ", " << v.y << std::endl;
+	throw std::runtime_error("Error!!!");
+}
+
+void DoSomething()
+{
+	//Vector* p = new Vector(5, 6); // old C++
+	auto p = std::make_unique<Vector>(5, 6);
+	PrintVector(*p);
+	//delete p;
+}
+
 int main(int /*argc*/, char** /*argv*/)
 {
 	std::cout << "Hello, world!" << std::endl;
-
-	for(int i = 0; i < 1000; i++)
+	try
 	{
-		//Vector* p = new Vector(5*i, 6); // old C++
-		auto p = std::make_unique<Vector>(5*i, 6);
-		std::cout << "vector: " << p-> x << ", " << p->y << std::endl;
+		DoSomething();
 	}
-	//delete p;
+	catch(const std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 }
 
