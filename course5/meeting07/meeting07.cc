f2d0def0a4b80579863114404108cfb045a4af96
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-14 17:31:06 +0100

    template functions 1

--- course5/meeting07/meeting07.cc
+++ course5/meeting07/meeting07.cc
@@ -1,18 +1,36 @@
 #include <iostream>
 #include <memory>
 #include <cassert>
+#include <string>
 
 #include "my_class.hh"
 
+template<typename T>
+T Add(const T& a, const T& b)
+{
+	std::cout << "TT" << std::endl;
+	return a + b;
+}
+
+template<typename T, typename U>
+auto Add(const T& a, const U& b)
+{
+	std::cout << "TU" << std::endl;
+	return a + b;
+}
+
+std::string Add(const char* a, const char* b)
+{
+	return std::string(a) + std::string(b);
+}
+
+using namespace std::literals;
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	Course::MyClass o1, o2;
-	o1.x = 7;
-	o2.x = 5;
+	auto a = Add/*<std:string>*/(std::string("Hello, "), "World!"s);
+	auto b = Add(5, 66.6);
+	auto c = Add("one", "two");
 	
-	
-	std::cout << (o1 + o2).GetX() << std::endl; // 
-		// Course::MyClass::GetX() const
 }
 
--- course5/meeting07/my_class.hh
+++ course5/meeting07/my_class.hh
@@ -6,9 +6,10 @@ namespace Course
 struct MyClass
 {
 	int GetX() const;
-	
+	double length() const;
 	int x;
 };
+
 
 MyClass operator+(MyClass a, MyClass b);
 // Course::operator+(Course::MyClass, Course::MyClass)
