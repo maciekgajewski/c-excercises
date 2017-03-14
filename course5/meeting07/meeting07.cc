b1db7cbaeeebafb1b4e3be70543407bbd3f1a663
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-14 17:38:39 +0100

    Simple println

--- course5/meeting07/meeting07.cc
+++ course5/meeting07/meeting07.cc
@@ -5,32 +5,25 @@
 
 #include "my_class.hh"
 
-template<typename T>
-T Add(const T& a, const T& b)
-{
-	std::cout << "TT" << std::endl;
-	return a + b;
-}
-
-template<typename T, typename U>
-auto Add(const T& a, const U& b)
-{
-	std::cout << "TU" << std::endl;
-	return a + b;
-}
-
-std::string Add(const char* a, const char* b)
-{
-	return std::string(a) + std::string(b);
-}
 
 using namespace std::literals;
 
+template<typename T>
+void println(const T& v)
+{
+	std::cout << v << std::endl;
+}
+
+struct MyString
+{
+	bool empty() { return false; }
+};
+
 int main(int /*argc*/, char** /*argv*/)
 {
-	auto a = Add/*<std:string>*/(std::string("Hello, "), "World!"s);
-	auto b = Add(5, 66.6);
-	auto c = Add("one", "two");
+	println(7);
+	println(8.8);
+	println("Hello!");
 	
 }
 
