88a988d57bd1fd65f11aca0e9bd3e2c013d0287f
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-14 16:15:14 +0100

    quick unique_ptr/move reminder

--- course5/meeting07/meeting07.cc
+++ course5/meeting07/meeting07.cc
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <memory>
+#include <cassert>
 
 #include "my_class.hh"
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	std::cout << "Hello, world!" << std::endl;
+	std::unique_ptr<char[]> a;
+	assert(a.get() == nullptr);
+	
+	std::unique_ptr<char[]> b = std::make_unique<char[]>(7);
+	std::unique_ptr<char[]> c = std::make_unique<char[]>(4);
+	
+	c = std::move(b);
+	
 }
 
