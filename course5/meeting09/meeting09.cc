dd41724e3930346e3718fc4f4082d30665bc6b5f
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-03-28 17:03:34 +0200

    template function specialization

--- course5/meeting09/meeting09.cc
+++ course5/meeting09/meeting09.cc
@@ -6,8 +6,27 @@
 using namespace std::literals;
 
 template<typename T>
+std::string type_name()
+{
+	return "unknown";
+}
+
+template<typename T>
+int something(const T& a)
+{
+	if (type_name<T>() == "int")
+		return a;
+		
+	if (type_name<T>() == "std::string")
+	{
+		return a.length();
+	}
+}
+
+template<typename T>
 bool is_larger(const T& a, const T& b)
 {
+	std::cout << "comparing two objects of type " << type_name<T>() << std::endl;
 	return a > b;
 }
 
@@ -16,11 +35,16 @@ bool is_larger(const char* a, const char* b)
 	return std::strcmp(a, b) > 0;
 }
 
-template<typename T>
-std::string type_name();
+template<>
+std::string type_name<int>() { return "int"; }
+template<>
+std::string type_name<double>() { return "double"; }
 
 int main(int /*argc*/, char** /*argv*/)
 {
+	std::cout << type_name<int>() << std::endl;
+	std::cout << type_name<double>() << std::endl;
+	std::cout << type_name<char>() << std::endl;
 
 	std::cout << std::boolalpha;
 	std::cout << is_larger(6, 8) << std::endl;
@@ -30,10 +54,6 @@ int main(int /*argc*/, char** /*argv*/)
 	std::cout << is_larger("aaa"s, std::string("zzz")) << std::endl;
 	std::cout << is_larger("aaa", "zzz") << std::endl;
 	std::cout << is_larger("zzz", "aaa") << std::endl;
-
-
-	std::cout << type_name<int>() << std::endl;
-
 
 
 	/*
