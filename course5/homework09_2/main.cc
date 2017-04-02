54c35d9aa14e8bab332cfb7e99e87e24c89a37a5
Sophia Davis <scdgrapefruit@gmail.com>
2017-04-02 23:17:40 +0200

    Make move construction & assignment work

--- course5/homework09_2/main.cc
+++ course5/homework09_2/main.cc
@@ -19,12 +19,11 @@ struct MoreUniquePtr
 	}
 	MoreUniquePtr(const MoreUniquePtr&) = delete; // copy
 
-	// MoreUniquePtr(MoreUniquePtr&& src) // move
-	// {
-	// 	std::cout << "Move construction" << std::endl;
-	// 	ptr = new T;
-	// 	ptr = src.ptr;
-	// }
+	MoreUniquePtr(MoreUniquePtr&& src) // move
+	{
+		ptr = src.ptr;
+		src.ptr = nullptr;
+	}
 
 	~MoreUniquePtr()
 	{
@@ -35,13 +34,14 @@ struct MoreUniquePtr
 	// copy assignment operator
 	MoreUniquePtr& operator=(const MoreUniquePtr& src) = delete; // copy assignmt
 
-	// MoreUniquePtr& operator=(MoreUniquePtr&& src) // move assignmt
-	// {
-	// 	std::cout << "Move assignment" << std::endl;
-	// 	if (ptr)
-	// 		delete ptr;
-	// 	ptr = src.ptr;
-	// }
+	MoreUniquePtr& operator=(MoreUniquePtr&& src) // move assignmt
+	{
+		if (ptr)
+			delete ptr;
+		ptr = src.ptr;
+		src.ptr = nullptr;
+		return *this;
+	}
 
 	T get()
 	{
@@ -55,6 +55,8 @@ private:
 
 int main(int /*argc*/, char** /*argv*/)
 {
+	/* Test construction/destruction & get method */
+
 	MoreUniquePtr<int> default_constructed;
 
 	MoreUniquePtr<int> int_constructor(8);
@@ -69,14 +71,47 @@ int main(int /*argc*/, char** /*argv*/)
 	const char * more_yolo = "more yolo";
 	MoreUniquePtr<const char *> char_star_constructor(more_yolo);
 	std::cout << "Got " << char_star_constructor.get() << std::endl;
-	assert(std::string(char_star_constructor.get()) == "more yolo");
+	assert(std::string(char_star_constructor.get()) == more_yolo);
 
 	MoreUniquePtr<double> double_constructor(3.14159);
 	std::cout << "Got " << double_constructor.get() << std::endl;
 	assert(double_constructor.get() == 3.14159);
 
-	// MoreUniquePtr<int> for_move_construction(2);
-	// MoreUniquePtr<int> move_constructed = std::move(for_move_construction);
-	// std::cout << "Got " << move_constructed.get() << std::endl;
-	// assert(move_constructed.get() == 2);
+
+	/* Test move assignment between same types */
+
+	MoreUniquePtr<int> for_move_construction(2);
+	MoreUniquePtr<int> move_constructed = std::move(for_move_construction);
+	std::cout << "Got " << move_constructed.get() << std::endl;
+	assert(move_constructed.get() == 2);
+
+	MoreUniquePtr<int> for_move_assignment(3);
+	MoreUniquePtr<int> move_assigned(5);
+	move_assigned = std::move(for_move_assignment);
+	std::cout << "Got " << move_assigned.get() << std::endl;
+	assert(move_assigned.get() == 3);
+
+	/* Test move assignment between types -- shouldn't compile */
+	// MoreUniquePtr<const char *> for_move_construction_combatible_type(more_yolo);
+	// MoreUniquePtr<std::string> move_constructed_combatible_type = std::move(for_move_construction_combatible_type);
+	// std::cout << "Got " << move_constructed_combatible_type.get() << std::endl;
+	// assert(std::string(move_constructed_combatible_type.get()) == more_yolo);
+	//
+	// MoreUniquePtr<const char *> for_move_assignment_combatible_type(more_yolo);
+	// MoreUniquePtr<std::string> move_assigned_combatible_type(yolo);
+	// move_assigned_combatible_type = std::move(for_move_assignment_combatible_type);
+	// std::cout << "Got " << move_assigned_combatible_type.get() << std::endl;
+	// assert(std::string(move_assigned_combatible_type.get()) == more_yolo);
+	//
+	// MoreUniquePtr<std::string> for_move_construction_other_type(yolo);
+	// MoreUniquePtr<double> move_constructed_other_type = std::move(for_move_construction_other_type);
+	// std::cout << "Got " << move_constructed_other_type.get() << std::endl;
+	// assert(std::string(move_constructed_other_type.get()) == yolo);
+	//
+	// MoreUniquePtr<std::string> for_move_assignment_other_type(yolo);
+	// MoreUniquePtr<double> move_assigned_other_type(3.86196357865);
+	// move_assigned_other_type = std::move(for_move_assignment_other_type);
+	// std::cout << "Got " << move_assigned_other_type.get() << std::endl;
+	// assert(std::string(move_assigned_other_type.get()) == yolo);
+
 }
