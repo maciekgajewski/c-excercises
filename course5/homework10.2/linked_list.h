f4252d01d3ba9f4610b75b3ead06fdf468f2e570
Jens de Smit <jfdsmit@gmail.com>
2017-04-12 22:09:23 +0200

    a linked list container

--- course5/homework10.2/linked_list.h
+++ course5/homework10.2/linked_list.h
@@ -11,17 +11,18 @@ class LinkedList
 private:
 	struct Node
 	{
-		std::unique_ptr<T> value{nullptr};
+		T value;
 		std::unique_ptr<Node> next{nullptr};
 	};
 
 	std::unique_ptr<Node> head{nullptr};
 public:
-	class iterator {
+	class iterator: public std::forward_iterator_tag {
 	public:
 		iterator() = default;
 		iterator(Node * initial): current(initial) { }
 		iterator(iterator const & that) = default;
+		iterator & operator=(iterator const & that) { this->current = that.current; return *this; }
 		iterator & operator++() {
 			current = current->next.get();
 			return *this;
@@ -31,23 +32,29 @@ public:
 			current = current->next.get();
 			return old;
 		}
-		T & operator*() const { return *(current->value); }
+		T & operator*() const { return current->value; }
 		bool operator==(iterator const & that) const { return this->current == that.current; }
 		bool operator!=(iterator const & that) const { return this->current != that.current; }
 	private:
 		LinkedList<T>::Node * current{nullptr};
 	};
-	class const_iterator {
+
+	class const_iterator: public std::forward_iterator_tag {
 	public:
 		const_iterator() = default;
 		const_iterator(const_iterator const & that) = default;
+		const_iterator & operator=(const_iterator const & that) { this->current = that.current; return *this; }
 		const_iterator(Node const * initial): current(initial) { }
+		const_iterator & operator++() {
+			current = current->next.get();
+			return *this;
+		}
 		const_iterator operator++(int) {
 			const_iterator old = *this;
 			current = current->next.get();
 			return old;
 		}
-		T & operator*() const { return *(current->value); }
+		T const & operator*() const { return current->value; }
 		bool operator==(const_iterator const & that) const { return this->current == that.current; }
 		bool operator!=(const_iterator const & that) const { return this->current != that.current; }
 	private:
@@ -55,37 +62,87 @@ public:
 	};
 
 	LinkedList() = default;
-	LinkedList(LinkedList const & that) { *(this->value) = *(that.value); *(this->next) = *(that.next); }
-	LinkedList(LinkedList && that) { this->value = std::move(that.value); this->next = std::move(that.next); }
-	LinkedList(iterator const & first, iterator const & last)
+	LinkedList(LinkedList const & that): LinkedList(that.begin(), that.end()) { }
+	LinkedList(LinkedList && that): head(std::move(that.head)) { }
+
+	template <class InputIt>
+	LinkedList(InputIt const & first, InputIt const & last)
 	{
-		std::unique_ptr<Node> * current = &head;
-		for(auto it = first; it != last; it++)
+		std::unique_ptr<Node> * current{&head};
+		for (auto it = first; it != last; ++it)
 		{
 			*current = std::make_unique<Node>();
-			(*current)->value = std::make_unique<T>(*it);
-			current = &(head->next);
+			(*current)->value = *it;
+			current = &((*current)->next);
 		}
 	}
-	void operator=(LinkedList const & that) { *(this->value) = *(that.value); *(this->next) = *(that.next); }
-	void operator=(LinkedList && that) { this->value = std::move(that.value); this->next = std::move(that.next); }
-	//LinkedList operator+(LinkedList const & that) const;
-	//LinkedList& operator+=(LinkedList const & that);
-	bool operator==(LinkedList const & that) const { return this->value == nullptr && that.value == nullptr || this->value == that.value && *(this->next) == *(that.next); }
-	//bool operator!=(LinkedList const & that) const;
+
+	LinkedList & operator=(LinkedList const & that)
+	{
+		this->head = nullptr;
+		std::unique_ptr<Node> * current{&head};
+		for (auto it = that.begin(); it != that.end(); ++it)
+		{
+			*current = std::make_unique<Node>();
+			(*current)->value = *it;
+			current = &((*current)->next);
+		}
+		return *this;
+	}
+		
+	LinkedList & operator=(LinkedList && that) { this->head = std::move(that.head); return *this; }
+
+	bool operator==(LinkedList const & that) const
+	{
+		auto itl = begin(), itr = that.begin();
+		for (; itl != end() && itr != that.end(); ++itl, ++itr)
+		{
+			if (*itl != *itr)
+				return false;
+		}
+		return itl == end() && itr == that.end();
+	}
+	bool operator!=(LinkedList const & that) const { return !(*this == that); }
+
 	LinkedList::iterator begin() { return iterator(head.get()); }
-	LinkedList::const_iterator begin() const { const_iterator(head.get()); }
+	LinkedList::const_iterator begin() const { return const_iterator(head.get()); }
 	LinkedList::iterator end() { return iterator(nullptr); }
 	LinkedList::const_iterator end() const { return const_iterator(nullptr); }
+
 	void push_front(T const & element)
 	{
 		std::unique_ptr<LinkedList<T>::Node> newNode = std::make_unique<LinkedList<T>::Node>();
-		newNode->value = std::make_unique<T>(element);
+		newNode->value = element;
+		newNode->next = std::move(head);
+		head = std::move(newNode);
+	}
+	void push_front(T && element)
+	{
+		std::unique_ptr<LinkedList<T>::Node> newNode = std::make_unique<LinkedList<T>::Node>();
+		newNode->value = std::move(element);
 		newNode->next = std::move(head);
 		head = std::move(newNode);
 	}
 
-	//template <typename... Args> LinkedList & emplace(Args&& ... args);
+	template <class... Args>
+	void emplace_front(Args&& ... args)
+	{
+		std::unique_ptr<LinkedList<T>::Node> newNode = std::make_unique<LinkedList<T>::Node>();
+		newNode->value = T(std::forward<Args>(args)...);
+		newNode->next = std::move(head);
+		head = std::move(newNode);
+	}
+
+	void pop_front() { if (head) head = std::move(head->next); }
+	T & front() const { return head->value; }
+	std::size_t size()
+	{
+		std::size_t result{0};
+		for (auto it = begin(); it != end(); ++it)
+			++result;
+		return result;
+	}
+		
 };
 
 }
--- course5/homework10.2/main.cc
+++ course5/homework10.2/main.cc
@@ -1,15 +1,68 @@
+#include <algorithm>
+#include <cassert>
 #include <iostream>
+#include <string>
 
 #include "linked_list.h"
 
 int main(int, char**)
 {
-	jds::LinkedList<int> list;
-	list.push_front(1);
-	list.push_front(4);
-	for (jds::LinkedList<int>::iterator it = list.begin(); it != list.end(); ++it)
+	// default construction
+	jds::LinkedList<std::string> list;
+	// push_front() and front()
+	list.push_front(std::string("1"));
+	assert(list.front() == "1");
+	// push_front(T&&)
+	std::string s("foo");
+	list.push_front(std::move(s));
+	list.push_front("banana");
+	// emplace_front
+	list.emplace_front(5, 'z');
+	assert(list.front() == "zzzzz");
+	// size()
+	assert(list.size() == 4);
+	// check ordering
+	char const * expected_order[] = {"zzzzz", "banana", "foo", "1"};
+	std::size_t index = 0;
+	for (auto it = list.begin(); it != list.end(); ++it, ++index)
 	{
-		std::cout << "Next element: " << *it << std::endl;
+		assert(*it == expected_order[index]);
 	}
+
+	// copy constructor and copy assignment
+	jds::LinkedList<std::string> list_copy(list);
+	assert(list_copy == list);
+	list_copy = list;
+	assert(list_copy == list);
+	
+	// construct from iterators
+	jds::LinkedList<std::string> list_from_it(list.begin(), list.end());
+	assert(list_from_it == list);
+
+	// move construction and move assignment
+	jds::LinkedList<std::string> list_from_move = std::move(list_from_it);
+	assert(list_from_move == list);
+	assert(list_from_it.size() == 0);
+	assert(!(list_from_it == list));
+	assert(list_from_it != list);
+	list_from_it = jds::LinkedList<std::string>(list.begin(), list.end());
+	list_from_move = std::move(list_from_it);
+	assert(list_from_move == list);
+
+	// pop_front()
+	list_from_move.pop_front();
+	assert(list_from_move.front() == "banana");
+	assert(list_from_move.size() == 3);
+	// pop_front() from empty list
+	for (std::size_t i = 0; i < list_from_move.size() + 5; ++i)
+		list_from_move.pop_front();
+	assert(list_from_move.size() == 0);
+
+	// std algorithms
+	auto max_element = std::max_element(list.begin(), list.end());
+	assert(*max_element == "zzzzz");
+	std::string everything = std::accumulate(list.begin(), list.end(), std::string());
+	assert(everything == "zzzzzbananafoo1");
+
 	return 0;
 }
