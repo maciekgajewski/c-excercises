3a8590c5db68344241fb67e61c5206774de3ecf1
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-04-13 17:20:57 +0200

    vector type reqs

--- course5/meeting11/meeting11.cc
+++ course5/meeting11/meeting11.cc
@@ -1,14 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
+
+#include <memory>
+
+// type requirements
+
+struct FixedInMemory
+{
+	FixedInMemory(const FixedInMemory&) = delete;
+	FixedInMemory(FixedInMemory&&) = delete;
+	
+	int mNum;
+};
 
 int main(int /*argc*/, char** /*argv*/)
 {
-	char arr[] = "Hello, world!";
+	std::vector<FixedInMemory> v1;
 	
-	std::vector<char> chars(std::begin(arr), std::end(arr));
-	for(char c : chars)
-		std::cout << c;
-	std::cout << std::endl;
+	FixedInMemory f{6};
+	
+	v1.emplace_back(8);
+	
+	std::vector<int> v;
+	
+	v.reserve(10);
+	for(int i = 0; i < 12; i++)
+	{
+		std::cout << "data when size=" << v.size()
+			<< " : " << v.data()
+			<< ", capacity=" << v.capacity()
+			<< std::endl;
+		v.push_back(i);
+	}
+	
+	v.shrink_to_fit();
+	
+	std::cout << "final size=" << v.size()
+		<< ", capacity=" << v.capacity()
+		<< ", data=" << v.data()
+		<< std::endl;
+	
 }
 
 
