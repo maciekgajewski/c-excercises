dd58ea2bebb44bafcdc7fcc4e5b9345ad04aaa9a
Sophia Davis <scdgrapefruit@gmail.com>
2017-04-15 18:33:47 +0200

    Move to test "framework"

--- course5/homework10/main.cc
+++ course5/homework10/main.cc
@@ -1,5 +1,7 @@
 #include "linked_list.h"
 #include <cassert>
+#include <functional>
+
 
 /***
 Please, write a singly-linked list, that behaves like a well-behaved container should.
@@ -22,6 +24,29 @@ Test your container with few std algorithms.
 ***/
 
 enum TestResult { PASS, FAIL };
+template<typename execT, typename T>
+void test(const char* test_name, const execT& exp, const T& expected, bool should_succeed=true)
+{
+    if (should_succeed)
+    {
+        assert (exp() == expected);
+    }
+    else
+    {
+        TestResult outcome;
+        try
+        {
+            exp();
+            outcome = TestResult::FAIL;
+        }
+        catch(...)
+        {
+            outcome = TestResult::PASS;
+        }
+        assert (outcome == TestResult::PASS);
+    }
+    std::cout << "Passed: " << test_name << std::endl;
+}
 
 int main(int, char**)
 {
@@ -30,20 +55,17 @@ int main(int, char**)
 
     /* Basic tests for construction and assignment */
     LinkedList<int> empty_data;
-    TestResult outcome;
-    try
-    {
-        empty_data.front();
-        outcome = TestResult::FAIL;
-    }
-    catch(...)
-    {
-        outcome = TestResult::PASS;
-    }
-    assert (outcome == TestResult::PASS);
-    assert (empty_data.size() == 0);
+
+    auto call_empty_front = std::bind(&LinkedList<int>::front, &empty_data);
+    test("calling front on empty linked list raises error", call_empty_front, false, false);
+
+    auto call_empty_size = std::bind(&LinkedList<int>::size, &empty_data);
+    test("empty linked list has size 0", call_empty_size, 0);
 
     LinkedList<double> pi = 3.14159;
-    assert (pi.front() == 3.14159);
-    assert (pi.size() == 1);
+    auto call_front = std::bind(&LinkedList<double>::front, &pi);
+    test("basic construction sets first value", call_front, 3.14159);
+
+    auto call_size = std::bind(&LinkedList<double>::size, &pi);
+    test("basic construction increments size", call_size, 1);
 }
