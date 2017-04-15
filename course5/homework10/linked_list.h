085858d0124d832fd1dde9a7b00191636af614bf
Sophia Davis <scdgrapefruit@gmail.com>
2017-04-15 18:05:12 +0200

    Move to Node based approach

--- course5/homework10/linked_list.h
+++ course5/homework10/linked_list.h
@@ -7,27 +7,34 @@ namespace linked_list
 {
 
 template<class T, class Allocator = std::allocator<T>>
+struct Node
+{
+    std::unique_ptr<T> data;
+    std::unique_ptr<Node> next;
+};
+
+template<class T, class Allocator = std::allocator<T>>
 class LinkedList
 {
 public:
+    // Basic assignment constructors
     LinkedList() = default;
     LinkedList(T src_data)
     {
-        data = std::make_unique<T>(src_data);
+        head.data = std::make_unique<T>(val);
         mSize += 1;
     }
 
     int size() { return mSize; }
     T front()
     {
-        if (data)
-            return *data.get();
+        if (mSize > 0)
+            return *head.data.get();
         throw std::range_error("Calling front on empty LinkedList is undefined.");
     }
 
 private:
-    std::unique_ptr<T> data;
-    std::unique_ptr<LinkedList> next;
+    Node<T> head;
     int mSize = 0;
 
 }; // LinkedList
