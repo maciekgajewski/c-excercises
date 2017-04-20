e72524755301841604230502dffc246605aea7e2
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-04-20 16:09:10 +0200

    nested template class

--- course5/meeting12/meeting12.cc
+++ course5/meeting12/meeting12.cc
@@ -6,4 +6,30 @@ int main(int /*argc*/, char** /*argv*/)
 }
 
 
+template<typename T>
+class Outer
+{
+	template<typename Q>
+	class Inner
+	{
+		int Fun();
+	};
+	
+	template<typename It>
+	void Read(It first, It last);
+};
+
+template<typename T>
+template<typename Q>
+int Outer<T>::Inner<Q>::Fun()
+{
+}
+
+template<typename T>
+template<typename It>
+void Outer<T>::Read(It first, It last)
+{
+}
+
+
 
