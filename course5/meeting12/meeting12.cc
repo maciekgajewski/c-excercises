dd2272f81be154353312e9614bf1faaf1d1aeb03
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-04-20 16:47:14 +0200

    find_if with functions

--- course5/meeting12/meeting12.cc
+++ course5/meeting12/meeting12.cc
@@ -3,16 +3,32 @@
 #include <vector>
 #include <string>
 
-// functor
+// functions:
 
+bool is_waldo(const std::string& s) { return s == "Waldo"; }
+bool is_waldo_no_case(const std::string& s) { return s == "Waldo" || s == "waldo"; }
+
+// functors:
+
+struct IsWaldo
+{
+	bool operator() (const std::string& s) { return s == "Waldo"; }
+};
 
 int main(int argc, char** argv)
 {
 	std::vector<std::string> strings(argv, argv+argc);
 	
-	auto it = std::find(strings.begin(), strings.end(), "Waldo");
+	auto it = std::find_if(strings.begin(), strings.end(), is_waldo);
 	if (it == strings.end())
 		std::cout << "No Waldo :(" << std::endl;
 	else
 		std::cout << "Waldo found at pos " << it - strings.begin() << std::endl;
+	
+	auto it2 = std::find_if(strings.begin(), strings.end(), is_waldo_no_case);
+	if (it2 == strings.end())
+		std::cout << "(case insensitive) No Waldo :(" << std::endl;
+	else
+		std::cout << "(case insensitive) Waldo found at pos " << it2 - strings.begin() << std::endl;
+	
 }
