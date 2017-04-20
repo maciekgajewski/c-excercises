fbdcf5638bbfb1a696742e98498006e006c75f17
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-04-20 16:58:32 +0200

    functor, capture by value and ref

--- course5/meeting12/meeting12.cc
+++ course5/meeting12/meeting12.cc
@@ -12,23 +12,23 @@ bool is_waldo_no_case(const std::string& s) { return s == "Waldo" || s == "waldo
 
 struct IsWaldo
 {
-	bool operator() (const std::string& s) { return s == "Waldo"; }
+	int len;
+	int& count;
+	bool operator() (const std::string& s) { count++; return s.length() == len; }
 };
 
 int main(int argc, char** argv)
 {
 	std::vector<std::string> strings(argv, argv+argc);
 	
-	auto it = std::find_if(strings.begin(), strings.end(), is_waldo);
+	int counter = 0;
+	IsWaldo pred{7, counter};
+	auto it = std::find_if(strings.begin(), strings.end(), pred);
+
 	if (it == strings.end())
 		std::cout << "No Waldo :(" << std::endl;
 	else
 		std::cout << "Waldo found at pos " << it - strings.begin() << std::endl;
 	
-	auto it2 = std::find_if(strings.begin(), strings.end(), is_waldo_no_case);
-	if (it2 == strings.end())
-		std::cout << "(case insensitive) No Waldo :(" << std::endl;
-	else
-		std::cout << "(case insensitive) Waldo found at pos " << it2 - strings.begin() << std::endl;
-	
+	std::cout << "The predicate has been invoked " << counter << " times" << std::endl;
 }
