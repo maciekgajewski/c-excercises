c72372cae37b31f02853668902361a669c75ac45
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-04-20 17:21:54 +0200

    sort

--- course5/meeting12/meeting12.cc
+++ course5/meeting12/meeting12.cc
@@ -2,31 +2,17 @@
 #include <algorithm>
 #include <vector>
 #include <string>
-
-// functions:
-
-bool is_waldo(const std::string& s) { return s == "Waldo"; }
-bool is_waldo_no_case(const std::string& s) { return s == "Waldo" || s == "waldo"; }
-
-// functors:
+#include <cassert>
+#include <list>
+#include <deque>
 
 int main(int argc, char** argv)
 {
-	std::vector<std::string> strings(argv, argv+argc);
+	std::deque<std::string> strings(argv, argv+argc);
 	
-	int counter = 0;
-	int len = 7;
+	std::sort(strings.begin(), strings.end());
 	
-	auto it = std::find_if(strings.begin(), strings.end(), 
-					[&counter, len](const std::string& s)
-					{
-						counter++; return s.length() == len;
-					});
-
-	if (it == strings.end())
-		std::cout << "No Waldo :(" << std::endl;
-	else
-		std::cout << "Waldo found at pos " << it - strings.begin() << std::endl;
+	for(const auto& s : strings)
+		std::cout << s << std::endl;
 	
-	std::cout << "The predicate has been invoked " << counter << " times" << std::endl;
 }
