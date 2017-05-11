f5f209b19d79ee3cf446c1feabacd96c8e705f56
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-05-11 17:01:09 +0200

    Stupid type erasure

--- course5/meeting14/meeting14.cc
+++ course5/meeting14/meeting14.cc
@@ -8,9 +8,12 @@
 
 using namespace std::literals;
 
+enum class PersonTypes { Persons, Employees, Servicemen };
+
 struct Person
 
 {
+	PersonTypes type;
 	std::string name;
 	int age;
 	
@@ -22,6 +25,8 @@ struct Person
 	{
 		return this->age > p2.age;
 	}
+	
+	std::string GetType() const { return "Person"; }
 };
 
 std::ostream& operator<<(std::ostream& s, const Person& p)
@@ -29,14 +34,25 @@ std::ostream& operator<<(std::ostream& s, const Person& p)
 	return s << "{" << p.name << ", aged " << p.age << "}";
 }
 
-struct Employee : public Person
+struct Record
+{
+	int id;
+};
+
+struct Employee : Record, Person
 {
 	std::string role;
+	std::string GetType() const { return "Employee"; }
 };
 
 std::ostream& operator<<(std::ostream& s, const Employee& p)
 {
-	return s << "{" << p.name << ", aged " << p.age << ", role: " << p.role << "}";
+	return s << "{ id=" << p.id << " " << p.name << ", aged " << p.age << ", role: " << p.role << "}";
+}
+
+void PrintRecord(const Record& r)
+{
+	std::cout << "Record id=" << r.id << std::endl;
 }
 
 int main(int /*argc*/, char** /*argv*/)
@@ -45,6 +61,8 @@ int main(int /*argc*/, char** /*argv*/)
 	e.name = "John Doe";
 	e.age = 66;
 	e.role = "Janitor";
+	e.id = 7777;
+	e.type = PersonTypes::Employees;
 	
 	Person* p = &e;
 	
@@ -55,5 +73,22 @@ int main(int /*argc*/, char** /*argv*/)
 	std::cout << "e = " << e << std::endl;
 	std::cout << "*p = " << *p << std::endl;
 	
+	std::vector<Person*> ppl;
 	
+	Person josef { PersonTypes::Persons, "Josef", 28 };
+	ppl.push_back(&e);
+	ppl.push_back(&josef);
+	
+	PrintRecord(e);
+	
+	for(Person* p : ppl)
+	{
+		if (p->type == PersonTypes::Employees)
+		{
+			Employee* pe = static_cast<Employee*>(p);
+			std::cout << pe->GetType() << " : " << *pe << std::endl;
+		}
+		else
+			std::cout << p->GetType() << " : " << *p << std::endl;
+	}
 }
