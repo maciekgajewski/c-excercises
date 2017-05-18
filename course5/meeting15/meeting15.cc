75819f8d9c8c20507a48735a4e2d25af06d90506
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-05-18 17:34:36 +0200

    Meeting 15 - initial state

--- course5/meeting15/meeting15.cc
+++ course5/meeting15/meeting15.cc
@@ -1,7 +1,103 @@
 #include <iostream>
 #include <stdexcept>
 
+struct Cls
+{
+	Cls(int i) : id(i) {}
+	~Cls() { std::cout << "Cls id=" << id << " destroyed" << std::endl; }
+	
+	int id;
+};
+
+void fun()
+{
+	Cls x(8);
+	std::cout << "I'm in a fun" << std::endl;
+	throw new std::runtime_error("Hello, world!");
+	//throw 77;
+	
+}
+
+void intermediate()
+{
+	Cls i(22);
+	
+	try
+	{
+		fun();
+	} catch (const std::logic_error& e)
+	{
+		std::cout << "Logic error in fun: " << e.what() << std::endl;
+	}
+	catch(...)
+	{
+		std::cout << "some error in intermediate" << std::endl;
+		throw;
+	}
+}
+
 int main(int /*argc*/, char** /*argv*/)
 {
-	throw std::runtime_error("Hello, world!");
+	Cls top(0);
+	try
+	{
+		Cls inTry(1);
+		intermediate();
+		std::cout << "Hello again!" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	catch(const std::runtime_error& e)
+	{
+		std::cout << "Runtime error: " << e.what() << std::endl;
+	}
+	catch(...)
+	{
+		Cls inCatch(5);
+		std::cout << "Something's wrong..." << std::endl;
+	}
+	intermediate();
 }
+
+
+void c()
+{
+	T a;
+	T b;
+}
+
+void a()
+{
+	a = new T;
+	b = new T;
+}
+
+void d()
+{
+	auto a = std::make_unique<T>():
+	auto b = std::make_unique<T>():
+}
+
+
+template<typename T>
+class List
+{
+private:
+	std::uniquye_ptr<Node<T>> mFirst;
+	
+public:
+	
+	void push_front(const T& v)
+	{
+		auto rest = std::move(mFirst);
+		mFirst = std::make_unique<Node<T>>(v);
+		mFirst->next = std::move(rest);
+	}
+	
+};
+
+
+
+
