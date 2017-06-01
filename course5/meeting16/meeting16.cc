569d5f196c5027502eba41f0e4948fc22376d6b2
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-06-01 17:00:07 +0200

    thread, atomic, random

--- course5/meeting16/meeting16.cc
+++ course5/meeting16/meeting16.cc
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <chrono>
+#include <thread>
+#include <atomic>
+#include <random>
+
+using namespace st	std::thread t;
+d::literals;
 
 int main(int /*argc*/, char** /*argv*/)
 {
@@ -10,10 +17,45 @@ int main(int /*argc*/, char** /*argv*/)
 	
 	namespace bfs = boost::filesystem;
 	
+	auto start = std::chrono::high_resolution_clock::now();
 	std::copy(
-			bfs::recursive_directory_iterator("."),
+			bfs::recursive_direct	std::future<int> res = std::async(fun);
+	// some stuf...
+	
+	int r = res.wait();
+ory_iterator("."),
 			bfs::recursive_directory_iterator(), 
 			std::ostream_iterator<bfs::directory_entry>(std::cout, ", "));
+	auto finish = std::chrono::high_resolution_clock::now();
+	auto duration = finish - start;
+	
+	std::cout << "Listing files took " << duration / 1ms << "ms" << std::endl;
+	
+	std::cout << "sleeping..." << std::endl;
+	
+	std::atomic<int> x = 0;
+	std::thread t([&]()
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			x = i;
+			std::this_thread::sleep_for(1s);
+			std::cout << "boo!" << std::endl;
+		}
+	});
+
+	std::this_thread::sleep_for(5s);
+	
+	std::cout << "x=" << x << std::endl;
+	t.join();
+	
+	// devices
+	// distributions
+	
+	std::random_device rd;
+	std::uniform_int_distribution<int> dist(-1, 1);
+	
+	int r = dist(rd);
 }
 
 
