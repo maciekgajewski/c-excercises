0ed5dab9b5227eb638c08b85ee3e6cf80dc5220c
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-06-08 15:12:57 +0200

    Meeting 17 - initial state

---
+++ course5/meeting17/meeting17.cc
@@ -0,0 +1,52 @@
+#include <boost/variant.hpp>
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <cstdint>
+#include <iomanip>
+
+using param_t = boost::variant<std::string, double, std::int64_t, bool>;
+using param_map = std::map<std::string, param_t>;
+
+void to_json(std::ostream& os, const param_map& map);
+
+int main(int /*argc*/, char** /*argv*/)
+{
+	param_map params;
+	
+	params["strategy"] = "AEX Basket";
+	params["reference-price"] = 100.2;
+	params["enabled"] = true;
+	params["algoid"] = std::int64_t(4455);
+	//params["algoid"] = 4455ll;
+	
+	to_json(std::cout, params);
+	std::cout << std::endl;
+}
+
+struct param_to_json
+{
+	std::ostream& m_stream;
+	
+	void operator()(const std::string& s) { m_stream << '"' << s << '"'; }
+	
+	template<typename T>
+	void operator()(const T& v) { m_stream << std::boolalpha << v; }
+};
+
+void to_json(std::ostream& os, const param_map& map)
+{
+	param_to_json visitor{os};
+	
+	os << "{";
+	for(const auto& pair : map)
+	{
+		os << '"' << pair.first << "\":";
+		boost::apply_visitor(visitor, pair.second);
+		os << ',';
+	}
+	os << "}";
+}
+
+
