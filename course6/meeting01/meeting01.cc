38a4b1274f398c6c7d6f854105749d563c20f394
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-08-17 18:07:28 +0200

    stack and recursion

--- course6/meeting01/meeting01.cc
+++ course6/meeting01/meeting01.cc
@@ -1,13 +1,25 @@
 #include <iostream>
-#include <stdint>
+#include <cstdint>
+
+int g = 77;
+
+void fun(int arg)
+{
+	if (arg == 0)
+		return;
+	int a = 7;
+	int b = 7;
+
+	std::cout << "&a=" << &a << std::endl;
+	std::cout << "&b=" << &b << std::endl;
+	std::cout << "&arg=" << &arg << std::endl;
+	std::cout << "&g=" << &g << std::endl;
+	std::cout << std::endl;
+	fun(arg-1);
+}
 
 int main(int argc, char** argv)
 {
-	using mynumber = double;
-
-	mynumber a = 3;
-	mynumber b = 7;
-
-	auto c = a+b;
+	fun(argc);
 }
 
