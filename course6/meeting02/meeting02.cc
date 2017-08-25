87fbc1f18ea146c3d7d4ddfb99e14250a84b19d7
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-08-25 09:41:13 +0200

    meeeting2 - final state

--- course6/meeting02/meeting02.cc
+++ course6/meeting02/meeting02.cc
@@ -3,22 +3,42 @@
 
 int g = 77;
 
+class Vector
+{
+public:
+	int x;
+	double y;
+
+	void print();
+};
+
 void print(int i) { std::cout << "int: " << i << std::endl; }
 void print(double d) { std::cout << "double: " << d << std::endl; }
 
-// using value = double;
-// int print(value d) { std::cout << "value: " << d << std::endl; return 6; }
+void print(Vector v) { v.print(); }
 
 int main(int argc, char** argv)
 {
-	int a = 7;
-	std::uint64_t b = 88;
-	double c = 3.14;
-	float d = 6.77;
+	// int a = 7;
+	// std::uint64_t b = 88;
+	// double c = 3.14;
+	// float d = 6.77;
+	Vector s;
+	
+	s.x = 6;
+	s.y = 7.7;
 
-	print(a);
-	print(b);
-	print(c);
-	print(d);
+	auto p = &s;
+	p->x = 88;
+
+	print(s.x);
+	print(s.y);
+	print(s);
+	p->print();
+}
+
+void Vector::print(/* Vector* this */)
+{
+	std::cout << "Vector(" << this->x << ", " << y << ")" << std::endl;
 }
 
