d0f11e1a0346a55bfcad119f1486298b97879537
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2017-08-26 12:09:17 +0200

    Add toyapp-mehrdad

---
+++ course6/toyapp-mehrdad/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <displaylib/window.h>
+#include <displaylib/functions.h>
+#include <displaylib/surface.h>
+
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+
+	Display::Window win("Hello", 10, 10, 800, 600);
+	Display::Surface surf(200, 150);
+	for(int x = 0; x < 100; x++)
+	{
+		surf.Clear(0, 0, 255); // blue background
+		surf.SetPixel(10+x, 10, 255, 0, 0); // red pixel at 10x10
+		win.Display(surf);
+		Display::Delay(50);
+	}
+
+	Display::Delay(1000);
+
+	return 0;
+}
