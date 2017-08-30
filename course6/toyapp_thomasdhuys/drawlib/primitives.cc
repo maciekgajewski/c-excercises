0902940adc5bd444ba3de7b0c3f5cd098e999378
thomasdhuys <thomasdhuys@optiver.com>
2017-08-30 10:36:00 +0200

    Fix CMakeLists and add primitive constructors

--- course6/toyapp_thomasdhuys/drawlib/primitives.cc
+++ course6/toyapp_thomasdhuys/drawlib/primitives.cc
@@ -3,9 +3,14 @@
 namespace Draw {
 namespace Primitives {
 
-Point::Point(Index& idx): Point(&idx) {}
+Point::Point(Index& idx0): Point(&idx0) {}
+Point::Point(Index* idx0): idx0{idx0} {}
 
-Point::Point(Index* idx): idx0{idx} {}
+Line::Line(Index& idx0, Index& idx1): Line(&idx0, &idx1) {}
+Line::Line(Index* idx0, Index* idx1): idx0{idx0}, idx1{idx1} {}
+
+Triangle::Triangle(Index& idx0, Index& idx1, Index& idx2): Triangle(&idx0, &idx1, &idx2) {}
+Triangle::Triangle(Index* idx0, Index* idx1, Index* idx2): idx0{idx0}, idx1{idx1}, idx2{idx2} {}
 
 }
 }
--- course6/toyapp_thomasdhuys/drawlib/primitives.h
+++ course6/toyapp_thomasdhuys/drawlib/primitives.h
@@ -9,17 +9,24 @@ class Point {
 public:
 	Point(Index& idx0);
 	Point(Index* idx0);
-
 private:
 	Index* idx0;
 };
 
 class Line {
+public:
+	Line(Index& idx0, Index& idx1);
+	Line(Index* idx0, Index* idx1);
+private:
 	Index* idx0;
 	Index* idx1;
 };
 
 class Triangle {
+public:
+	Triangle(Index& idx0, Index& idx1, Index& idx2);
+	Triangle(Index* idx0, Index* idx1, Index* idx2);
+private:
 	Index* idx0;
 	Index* idx1;
 	Index* idx2;
