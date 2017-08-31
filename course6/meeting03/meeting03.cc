1c7a0e34b517e180a8c3fe2b32f3760579a83d4c
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-08-31 16:37:02 +0200

    static local variables

--- course6/meeting03/meeting03.cc
+++ course6/meeting03/meeting03.cc
@@ -4,23 +4,45 @@
 class Vector
 {
 public:
-	double x;
-	double y;
 
-	void print();
+	using coord = double;
+
+	coord x;
+	coord y;
+
+	void print(int x);
+	static void hello() { std::cout << "hello" << std::endl; }
+
+	static int v;
 };
+
+void hey()
+{
+	static int x = 0;
+	std::cout << "X=" << x << std::endl;
+	x++;
+}
 
 int main(int argc, char** argv)
 {
+	hey();
+	hey();
+	hey();
+
 	Vector s;
 	
 	s.x = 6;
 	s.y = 7.7;
 
-	s.print();
+	s.print(8);
+
+	s.hello();
+	Vector::hello();
+
+	Vector::coord f = 7;
 }
 
-void Vector::print(/* Vector* this */)
+void Vector::print(/* Vector* this */ int x)
 {
 	std::cout << "Vector(" << this->x << ", " << y << ")" << std::endl;
 }
