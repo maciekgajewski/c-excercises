6aa38a701b56bd1979fd31aa4f36510e9f171869
Mikhail Shamaev <m.shamaev@gmail.com>
2017-09-13 23:08:07 +0200

    First variant which compiles / links / runs

--- course6/toyapp_mikhail/displaylib/surface.h
+++ course6/toyapp_mikhail/displaylib/surface.h
@@ -14,8 +14,20 @@ namespace Display {
 		double x, y;
 	};
 
+	class Vector3D
+	{
+	public:
+		double x, y, z;
+	};
+
 	class Matrix
-	{};
+	{
+	public:
+		Vector3D operator * (Vector3D v)
+		{
+			return v;
+		}
+	};
 
 	class RGB
 	{
@@ -23,72 +35,66 @@ namespace Display {
 		uint8_t r, g, b;
 	};
 
-	class Vector3D
+	class Surface : private SDLObject
 	{
 	public:
-		double x, y, z;
+		Surface(int w, int h);
+		~Surface();
+
+		void Clear(std::uint8_t r, std::uint8_t g, std::uint8_t b);
+
+		void SetPixel(Vector2D p, RGB color) { SetPixel((int)p.x, (int)p.y, color.r, color.g, color.b); }
+
+		void SetPixel(int x, int y, std::uint8_t r, std::uint8_t g, std::uint8_t b);
+
+		void DrawLine(Vector2D start, Vector2D end, RGB color) {}
+
+	private:
+
+		friend class Window;
+
+		SDL_Surface* mSurface;
 	};
 
-	Vector3D operator * (Matrix m, Vector3D v)
+	class Surface3D
 	{
-		return v;
-	}
+	private:
+		Surface* surface;
+		Matrix cameraTransform;
+		Vector2D Transform(Vector3D p)
+		{
+			Vector3D transformed = cameraTransform * p;
+			return { transformed.x / transformed.z, transformed.y / transformed.z };// + plus sscaling and shift
+		}
+	public:
+		void SetPixel(Vector3D p, RGB color)
+		{
+			surface->SetPixel(Transform(p), color);
+		}
 
-class Surface : private SDLObject
-{
-public:
-	Surface(int w, int h);
-	~Surface();
+		void DrawLine(Vector3D start, Vector3D end, RGB color)
+		{
+			surface->DrawLine(Transform(start), Transform(end), color);
+		}
+	};
 
-	void Clear(std::uint8_t r, std::uint8_t g, std::uint8_t b);
-
-	void SetPixel(Vector2D p, RGB color) { SetPixel(p.x, p.y, color.r, color.g, color.b); }
-
-	void SetPixel(int x, int y, std::uint8_t r, std::uint8_t g, std::uint8_t b);
-
-	
-	void DrawLine(Vector2D start, Vector2D end, RGB color);
-
-private:
-
-	friend class Window;
-
-	SDL_Surface* mSurface;
-};
-
-class Surface3D
-{
-private:
-	Surface* surface;
-	Matrix cameraTransform;
-	Vector2D Transform(Vector3D p)
+	class Cube
 	{
-		Vector3D transformed = cameraTransform * p;
-		return { transformed.x / transformed.z, transformed.y / transformed.z };// + plus sscaling and shift
-	}
-public:
-	void SetPixel(Vector3D p, RGB color)
-	{
-		surface->SetPixel(Transform(p), color);
-	}
+		Matrix transformation;
+		RGB color;
 
-	void DrawLine(Vector3D start, Vector3D end, RGB color)
-	{
-		surface->DrawLine(Transform(start), Transform(end), color);
-	}
-};
+		void Draw(Surface3D& surface)
+		{
+			Vector2D edges[] = { Vector2D {-1, -1}, Vector2D{ -1, +1 }, Vector2D{ +1, -1 }, Vector2D{ +1, +1 } };
 
-class Cube
-{
-	Matrix transformation;
-	RGB color;
-
-	void Draw(Surface3D& surface)
-	{
-		surface.DrawLine(transformation * Vector3D{-1, -1, -1}, transformation * Vector3D{-1, -1, +1}, color);
-		// etc
-	}
-};
-
+			for (auto i = 0; i < 4; ++i)
+			{
+				auto v = edges[i];
+				surface.DrawLine(transformation * Vector3D{ v.x, v.y, -1 }, transformation * Vector3D{ v.x, v.y, +1 }, color);
+				surface.DrawLine(transformation * Vector3D{ v.x, -1, v.y }, transformation * Vector3D{ v.x, +1, v.y }, color);
+				surface.DrawLine(transformation * Vector3D{ -1, v.x, v.y }, transformation * Vector3D{ +1, v.x, v.y }, color);
+			}
+		}
+	};
 
 };
