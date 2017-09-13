06114217bbc9a28cea02c8142b7b7c735aeab363
Mikhail Shamaev <m.shamaev@gmail.com>
2017-09-13 22:39:26 +0200

    Fix compillation errors in initial repository
    Add mock definitions of classes used

--- course6/toyapp_mikhail/displaylib/surface.h
+++ course6/toyapp_mikhail/displaylib/surface.h
@@ -8,6 +8,32 @@
 
 namespace Display {
 
+	class Vector2D
+	{
+	public:
+		double x, y;
+	};
+
+	class Matrix
+	{};
+
+	class RGB
+	{
+	public:
+		uint8_t r, g, b;
+	};
+
+	class Vector3D
+	{
+	public:
+		double x, y, z;
+	};
+
+	Vector3D operator * (Matrix m, Vector3D v)
+	{
+		return v;
+	}
+
 class Surface : private SDLObject
 {
 public:
@@ -15,7 +41,12 @@ public:
 	~Surface();
 
 	void Clear(std::uint8_t r, std::uint8_t g, std::uint8_t b);
-	void SetPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b);
+
+	void SetPixel(Vector2D p, RGB color) { SetPixel(p.x, p.y, color.r, color.g, color.b); }
+
+	void SetPixel(int x, int y, std::uint8_t r, std::uint8_t g, std::uint8_t b);
+
+	
 	void DrawLine(Vector2D start, Vector2D end, RGB color);
 
 private:
@@ -33,28 +64,31 @@ private:
 	Vector2D Transform(Vector3D p)
 	{
 		Vector3D transformed = cameraTransform * p;
-		return {transformed.x/transformed/z, transformed.y/transformed.z} // + plus sscaling and shift
+		return { transformed.x / transformed.z, transformed.y / transformed.z };// + plus sscaling and shift
 	}
 public:
-	void SetPixel(Vector3D, RGB color) { surface->SetPixel(Transform(p), color);
+	void SetPixel(Vector3D p, RGB color)
+	{
+		surface->SetPixel(Transform(p), color);
+	}
+
 	void DrawLine(Vector3D start, Vector3D end, RGB color)
 	{
-		surface->DrawLine(Transform(start), Transfom(end), color);
+		surface->DrawLine(Transform(start), Transform(end), color);
 	}
-}
+};
 
 class Cube
 {
-	Transform tr;
+	Matrix transformation;
 	RGB color;
 
-	void Draw(Surface3D& surf)
+	void Draw(Surface3D& surface)
 	{
-		tr.DrawLine(tr*Vector3D{-1, -1, -1}, tr*{-1, -1, +1}, color);
+		surface.DrawLine(transformation * Vector3D{-1, -1, -1}, transformation * Vector3D{-1, -1, +1}, color);
 		// etc
 	}
+};
 
-}
 
-
-}
+};
