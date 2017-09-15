a461c5def242607298729bb0dd52949ec8acd9d8
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-09-15 16:31:55 +0200

    the party trick

--- course6/meeting05/meeting05.cc
+++ course6/meeting05/meeting05.cc
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
 
-
+void fun(int* p)
+{
+	std::cout << sizeof(p) << std::endl;
+	std::cout << p[2] << std::endl;
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello" << std::endl;
+	std::cout << 2["Hello"] << std::endl;
 
 	int a[3] = { 1, 3, 5};
 	std::cout << sizeof(a) << std::endl;
-
-	std::string b[4] = { "abc", "b", "c", "d" };
-
-	for(auto x : b[0])
-		std::cout << x << std::endl;
-
+	std::cout << a[2] << std::endl;
 	for(auto x : a)
 		std::cout << x << std::endl;
-	for(auto& x : b)
-		std::cout << x << std::endl;
+
+	int* ptr = a;
+
+	fun(a);
 }
 
