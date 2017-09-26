01f74e1f7bc6c4dc4bc793d3420e09b4bbc43939
Maciek Gajewski <maciej.gajewski0@gmail.com>
2017-09-26 10:24:43 +0200

    Meetinfg 6 - the final state

--- course6/meeting06/hello.cc
+++ course6/meeting06/hello.cc
@@ -1,6 +1,20 @@
-#include <iostream>
+#include "point.h"
+
+// Fun 1
+int fun(int x)
+{
+	return x*2;
+}
+
+// Fun 2
+int fun(double, char)
+{
+	return 44;
+}
 
 int main(int, char**)
 {
-	std::cout << "hello!" << std::endl;
+	Point p{5, 6};
+
+	return p.length();
 }
