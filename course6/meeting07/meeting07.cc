b62b14ae4328f40760276200de4e54a1580304f5
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-09-28 16:55:01 +0200

    Template functions + overload

--- course6/meeting07/meeting07.cc
+++ course6/meeting07/meeting07.cc
@@ -1,16 +1,37 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std::literals;
 
-int max(int a, int b) { return a > b ? a : b; }
-double max(double a, double b) { return a > b ? a : b; }
-std::string max(std::string a, std::string b) { return a > b ? a : b; }
+// int max(int a, int b) { return a < b ? b : a; }
+// double max(double a, double b) { return a < b ? b : a; }
+// std::string max(std::string a, std::string b) { return a < b ? b : a; }
+
+template<typename T>
+T Max(const T& a, const T& b)
+{
+	if (a < b)
+		return b;
+	else
+		return a;
+}
+
+const char* Max(const char* a, const char* b)
+{
+	if (std::strcmp(a, b) < 0)
+		return b;
+	else
+		return a;
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << max(7, 99) << std::endl;
-	std::cout << max(5.5, 42.7) << std::endl;
-	std::cout << max("Ariel"s, "Maciek"s) << std::endl;
+	std::cout << Max<int>(7, 99) << std::endl;
+	std::cout << Max(5.5, 42.7) << std::endl;
+	std::cout << Max("Ariel"s, "Maciek"s) << std::endl;
+	std::cout << Max<const float>(1, 1.1) << std::endl;
+
+	std::cout << Max<const char*>("Aris", "Misha") << std::endl;
 }
 
