af41f073786224538f7d4b8f1657a20ad640a4a4
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-10-05 16:56:38 +0200

    unique_ptr basics

--- course6/meeting08/meeting08.cc
+++ course6/meeting08/meeting08.cc
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
+#include <memory>
 
 struct Record
 {
+	Record() { std::cout << "Record(), this=" << this << std::endl; }
+	Record(int x) { id = x; std::cout << "Record(int), this=" << this << std::endl; }
+	~Record() { std::cout << "~Record(), this=" << this << std::endl; }
+	
 	std::string name;
 	int id;
 	float factor;
@@ -10,8 +15,12 @@ struct Record
 
 int main(int argc, char** argv)
 {
-	Record datum;
-	datum.name = "Hello";
-	std::cout << datum.name << std::endl;
+	std::unique_ptr<Record> datumPtr = std::make_unique<Record>(77);
+	datumPtr->name = "Hello 1234567890 1234567890";
+
+	auto p = std::make_unique<int>(7);
+
+	std::cout << datumPtr->name << std::endl;
+	std::cout << *p << std::endl;
 }
 
