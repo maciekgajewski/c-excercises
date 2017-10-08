d7fb5165997802833bd24e783c9f9a8189a38566
Vivike Lapoutre <V.Lapoutre@gmail.com>
2017-10-08 20:33:46 +0200

    Properly unit testing the objects file, except for Rectangle since I'm not too happy with it yet.

--- course6/toyapp-vivike/tests/test_objects.cc
+++ course6/toyapp-vivike/tests/test_objects.cc
@@ -10,21 +10,57 @@ BOOST_AUTO_TEST_SUITE (vector_tests)
 
 BOOST_AUTO_TEST_CASE (constructor_tests)
 {
-  Vector<int, 3> myvec{1, 2, 3};
-  BOOST_REQUIRE(myvec.vec[0] == 1);
-  BOOST_REQUIRE(myvec.vec[1] == 2);
-  BOOST_REQUIRE(myvec.vec[2] == 3);
+  Vector<int, 3> myvec{3, 9, 6};
+  BOOST_REQUIRE(myvec[0] == 3);
+  BOOST_REQUIRE(myvec[1] == 9);
+  BOOST_REQUIRE(myvec[2] == 6);
 }
 
 BOOST_AUTO_TEST_CASE (operator_tests)
 {
-  Vector<int, 3> vec_a{1, 2, 3};
+  Vector<int, 3> vec_a{3, 9, 6};
   Vector<int, 3> vec_b{1, 2, 3};
 
+  int b = 3;
+
   Vector<int, 3> sumvec = vec_a + vec_b;
-  BOOST_REQUIRE(sumvec.vec[0] == 2);
-  BOOST_REQUIRE(sumvec.vec[1] == 4);
-  BOOST_REQUIRE(sumvec.vec[2] == 6);
+  BOOST_REQUIRE(sumvec[0] == 4);
+  BOOST_REQUIRE(sumvec[1] == 11);
+  BOOST_REQUIRE(sumvec[2] == 9);
+
+  Vector<int, 3> timesvec = vec_a * b;
+  BOOST_REQUIRE(timesvec[0] == 9);
+  BOOST_REQUIRE(timesvec[1] == 27);
+  BOOST_REQUIRE(timesvec[2] == 18);
+
+  Vector<int, 3> diffvec1 = vec_a - vec_b;
+  BOOST_REQUIRE(diffvec1[0] == 2);
+  BOOST_REQUIRE(diffvec1[1] == 7);
+  BOOST_REQUIRE(diffvec1[2] == 3);
+
+  Vector<int, 3> diffvec2 = vec_b - vec_a;
+  BOOST_REQUIRE(diffvec2[0] == -2);
+  BOOST_REQUIRE(diffvec2[1] == -7);
+  BOOST_REQUIRE(diffvec2[2] == -3);
+
+  Vector<int, 3> divvec = vec_a / b;
+  BOOST_REQUIRE(divvec[0] == 1);
+  BOOST_REQUIRE(divvec[1] == 3);
+  BOOST_REQUIRE(divvec[2] == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE (camera_tests)
+
+BOOST_AUTO_TEST_CASE (propjection_tests)
+{
+  Vector3D input3d{3, 2, 1};
+  Display::Camera cam;
+  Pixel output2d = cam.project2D(input3d);
+  BOOST_REQUIRE(output2d[0] == 3);
+  BOOST_REQUIRE(output2d[1] == 2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
