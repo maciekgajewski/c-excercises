b7f6aa7fad844dd9173942551340692b14a120a3
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-10-12 17:03:53 +0200

    intro to containers

--- course6/meeting09/meeting09.cc
+++ course6/meeting09/meeting09.cc
@@ -1,7 +1,24 @@
-#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <string>
+
+struct Data
+{
+	std::string name;
+	Data(const std::string& n) : name(n) { std::cout << "Data(string)" << std::endl; }
+	Data(const Data& c) : name(c.name) { std::cout << "Data copy" << std::endl; }
+	Data(Data&& c) noexcept: name(std::move(c.name)) { std::cout << "Data move" << std::endl; }
+	~Data() { std::cout << "~Data" << std::endl; }
+};
 
 int main(int argc, char** argv)
 {
-	::system("banner Hello");
+	std::vector<Data> v;
+	v.push_back(Data{", world"});
+	v.push_back(Data{"Hello"});
+	std::cout << "v holds " << v.size() << " elements" << std::endl;
+
+	for(int i = 0 ; i < v.size(); i++)
+		std::cout << "v[" << i << "] = " << v[i].name << std::endl;
 }
 
