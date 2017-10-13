63d9ae34e1ddff5dac6321d694661b0bedc2984d
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-10-13 09:21:12 +0200

    vector vs list

--- course6/meeting09/meeting09.cc
+++ course6/meeting09/meeting09.cc
@@ -1,24 +1,28 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <list>
 
 struct Data
 {
 	std::string name;
-	Data(const std::string& n) : name(n) { std::cout << "Data(string)" << std::endl; }
-	Data(const Data& c) : name(c.name) { std::cout << "Data copy" << std::endl; }
-	Data(Data&& c) noexcept: name(std::move(c.name)) { std::cout << "Data move" << std::endl; }
-	~Data() { std::cout << "~Data" << std::endl; }
+	Data(const std::string& n) : name(n) { std::cout << "Data(" << n << ")" << std::endl; }
+	Data(const Data& c) : name(c.name) { std::cout << "Data copy " << name << std::endl; }
+	Data(Data&& c) noexcept: name(std::move(c.name)) { std::cout << "Data move " << name << std::endl; }
+	~Data() { std::cout << "~Data " << name << std::endl; }
 };
 
 int main(int argc, char** argv)
 {
 	std::vector<Data> v;
-	v.push_back(Data{", world"});
-	v.push_back(Data{"Hello"});
+
+	v.reserve(argc);
+	for(int i = 0; i < argc; i++)
+		v.push_back(Data(argv[i]));
+
 	std::cout << "v holds " << v.size() << " elements" << std::endl;
 
-	for(int i = 0 ; i < v.size(); i++)
-		std::cout << "v[" << i << "] = " << v[i].name << std::endl;
+	for(const Data& d : v)
+		std::cout << d.name << std::endl;
 }
 
