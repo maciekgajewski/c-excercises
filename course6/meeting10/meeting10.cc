355861a484811748ca71d0a93f36c01b929e6da3
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-10-19 16:23:25 +0200

    Ireration

--- course6/meeting10/meeting10.cc
+++ course6/meeting10/meeting10.cc
@@ -3,5 +3,12 @@
 int main(int argc, char** argv)
 {
 	// TODO - iterate over argv, print every element
+	char** end = argv + argc;
+	char** ptr = argv;
+	for (; ptr != end; ptr++)
+	{
+		std::cout << *ptr << std::endl;
+	}
 }
 
+ 
