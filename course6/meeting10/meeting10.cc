30bdaf622ad35daf0e526efb8e21d472c182a5fc
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-10-19 16:57:36 +0200

    basic ranges

--- course6/meeting10/meeting10.cc
+++ course6/meeting10/meeting10.cc
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <list>
 
-int main(int argc, char** argv)
+template<typename Iterator>
+void print(Iterator begin, Iterator end)
 {
-	// TODO - iterate over argv, print every element
-	char** end = argv + argc;
-	char** ptr = argv;
-	for (; ptr != end; ptr++)
+	for (auto ptr = begin; ptr != end; ptr++)
 	{
 		std::cout << *ptr << std::endl;
 	}
+}
+
+
+int main(int argc, char** argv)
+{
+	print(argv, argv + argc);
+
+	std::vector<int> v({1, 3, 5, 7, 9});
+	print(v.begin(), v.end());
+
+	std::list<std::string> l{"aa", "bb", "cc"};
+	print(l.begin(), l.end());
+
 }
 
  
