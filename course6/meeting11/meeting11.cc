96f1210d01d7767f2ea7785ba687eaeb339853a8
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-10-26 16:49:52 +0200

    nth_element, partial sort

--- course6/meeting11/meeting11.cc
+++ course6/meeting11/meeting11.cc
@@ -13,19 +13,20 @@ int main(int argc, char** argv)
 	std::ifstream random_file("/dev/urandom");
 	const std::size_t COUNT = 10000000;
 
-	std::vector<unsigned int> random_ints;
+	std::vector<unsigned> random_ints;
 	random_ints.resize(COUNT);
-	random_file.read(reinterpret_cast<char*>(random_ints.data()), sizeof(unsigned int) * COUNT);
+	random_file.read(reinterpret_cast<char*>(random_ints.data()), sizeof(unsigned) * COUNT);
 
 	// sort
 	auto start = std::chrono::high_resolution_clock::now();
+	//std::sort(random_ints.begin(), random_ints.end());
 
-	std::sort(random_ints.begin(), random_ints.end());
+	std::nth_element(random_ints.begin(), random_ints.begin() + 20 , random_ints.end());
+	std::sort(random_ints.begin(), random_ints.begin() + 20);
 
 	auto end = std::chrono::high_resolution_clock::now();
 
-	std::cout << COUNT << " numbers sorted in " << (end-start)/1ms << " ms" << std::endl;
-
+	std::cout << COUNT << " numbers sorted in " << 0.001*(end-start)/1us << " ms" << std::endl;
 	auto it = random_ints.begin();
 	for(int i = 0; i < 20; i++, it++)
 		std::cout << "#" << i << " : " << *it << std::endl;
