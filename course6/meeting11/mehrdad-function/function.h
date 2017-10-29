15121868621b734189a161841f1c22ad42a5e847
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2017-10-29 11:10:42 +0100

    implemet empty functions with tests

--- course6/meeting11/mehrdad-function/function.h
+++ course6/meeting11/mehrdad-function/function.h
@@ -10,10 +10,12 @@ template<typename R, typename... Args>
 class Callable
 {
 public:
-	Callable(R ret, Args... args);
+	Callable(R ret, Args... args){}
 	~Callable(){}
 
-	R operator() (Args... args);
+	R operator() (Args... args)
+	{
+	}
 };
 }
 
@@ -22,22 +24,25 @@ template<typename R, typename... Args>
 class Function
 {
 public:
-	Function();
-	Function(std::nullptr_t);
-
-	template<typename F>
-	Function(F functor);
-
-	~Function();
-
-	explicit operator bool() const;
-
-	R operator() (Args... args);
+	Function() {}
+	Function(std::nullptr_t) {}
+	~Function() {}
+	explicit operator bool() const { return (bool)mCallable; }
 
 	Function(const Function& other) = delete;//Copy is not supported for now
 	Function(Function&& other) = delete;//Move is not supported for now
 
+	template<typename F>
+	Function(F functor);
+
+	R operator() (Args... args)
+	{
+		if (!mCallable)
+			throw std::runtime_error("Calling empty function");
+	}
+
+
 private:
-	std::unique_ptr<Callable<R(Args...)>> mCallable;
+	std::unique_ptr<Callable<R, Args...>> mCallable;
 };
 }
--- course6/meeting11/mehrdad-function/test.cc
+++ course6/meeting11/mehrdad-function/test.cc
@@ -1,3 +1,15 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
+
+#include "function.h"
 #include "catch.hpp"
 
+TEST_CASE("Empty functions") {
+	Mehrdad::Function<int, double> f1;
+	Mehrdad::Function<double, int> f2(nullptr);
+
+	REQUIRE( static_cast<bool>(f1) == false );
+	REQUIRE( static_cast<bool>(f2) == false );
+
+	REQUIRE_THROWS_AS(f1(1.0), std::runtime_error);
+	REQUIRE_THROWS_AS(f2(1), std::runtime_error);
+}
