e4b950db22f7b9fafad99dc186749ed6209ac70f
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2017-10-29 10:44:08 +0100

    Define the base interface

--- course6/meeting11/mehrdad-function/function.h
+++ course6/meeting11/mehrdad-function/function.h
@@ -1,8 +1,43 @@
 #pragma once
 
+#include <cstddef>
+#include <memory>
+
 namespace Mehrdad {
+
+namespace {
+template<typename R, typename... Args>
+class Callable
+{
+public:
+	Callable(R ret, Args... args);
+	~Callable(){}
+
+	R operator() (Args... args);
+};
+}
+
+
+template<typename R, typename... Args>
 class Function
 {
+public:
+	Function();
+	Function(std::nullptr_t);
 
+	template<typename F>
+	Function(F functor);
+
+	~Function();
+
+	explicit operator bool() const;
+
+	R operator() (Args... args);
+
+	Function(const Function& other) = delete;//Copy is not supported for now
+	Function(Function&& other) = delete;//Move is not supported for now
+
+private:
+	std::unique_ptr<Callable<R(Args...)>> mCallable;
 };
 }
