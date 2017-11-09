b9fd9a8dbf188ff1c184a3ba17b63c8b5e75e2e1
David Gross <DavidGross@Optiver.com>
2017-11-09 17:22:23 +0100

    meeting13: fixed_vector push_back

--- course6/meeting13/meeting13.cc
+++ course6/meeting13/meeting13.cc
@@ -1,11 +1,61 @@
 #include <cstddef>
+#include <array>
+#include <iostream>
+#include <vector>
 
-template <typename T>
-struct vector
+template <typename ValueT, std::size_t N>
+struct fixed_vector
 {
+	void push_back(const ValueT& value) { _push_back(value); }
+	void push_back(ValueT&& value) {  _push_back(std::move(value)); }
+
+	std::size_t size() const { return mSize; }
+	std::size_t capacity() const { return mElements.size(); }
+
+	ValueT* begin() { return mElements.data(); }
+	ValueT* end() { return mElements.data() + mSize; }
+
+private:
+	template <typename ArgT>
+	void _push_back(ArgT&& value)
+	{
+		if (size() == capacity())
+			throw std::runtime_error("full");
+
+		mElements[mSize] = std::forward<ArgT>(value);
+		++mSize;
+	}
+
+	std::size_t mSize = 0;
+	std::array<ValueT, N> mElements;
+};
+
+struct A
+{
+	A() =default;
+
+	A(const A& a) : mText(a.mText) { std::cout << "A copied" << std::endl; }
+	A& operator=(const A& a) { mText = a.mText; std::cout << "A copy assign" << std::endl; return *this;}
+
+	A(A&& a) : mText(std::move(a.mText)) { std::cout << "A moved" << std::endl; }
+	A& operator=(A&& a) { mText = std::move(a.mText); std::cout << "A move assign" << std::endl; return *this; }
+		
+	std::string mText;
 };
 
 int main()
 {
+	fixed_vector<A, 5> vi; 
+
+	fixed_vector<A, 5> vv; 
+
+	std::cout << "START" << std::endl;
+	vv = std::move(vi);
+	std::cout << "END" << std::endl;
+
+	A a;
+	vi.push_back(std::move(a));
+	vi.push_back(a);
+
 	return 0;
 }
