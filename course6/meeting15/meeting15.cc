c0ff7b8446a534383379c3307e11838de8d271f8
Maciej Gajewski <maciejgajewski@Optiver.com>
2017-11-23 17:45:29 +0100

    multiple inh

--- course6/meeting15/meeting15.cc
+++ course6/meeting15/meeting15.cc
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <memory>
+#include <stdexcept>
 
-class IPrintable
-{
-public:
-	virtual void Print(std::ostream& o) const = 0;
-};
-
-class Person : public IPrintable
+class Person 
 {
 protected:
 	std::string name;
@@ -17,50 +13,43 @@ protected:
 public:
 	Person(const std::string& n, int a) : name(n), age(a) {}
 
-	virtual void Print(std::ostream& o) const 
+	void Print(std::ostream& o) const
 	{ o << name << " (aged " << age << ")"; }
 };
 
-class Employee : public Person
+struct DbEntry
+{
+	int id;
+};
+
+class Employee : public DbEntry, public Person
 {
 private:
 	std::string role;
 public:
 	Employee(const std::string& n, int a, const std::string& r)
-		: Person(n, a), role(r) {}
+		: Person(n, a), role(r) { id = 7; }
 
 	void Print(std::ostream& o) const
 	{ o << name << " the " << role << " (aged " << age << ")"; }
 };
 
-struct Triangle : public IPrintable
-{
-	Triangle(int xx, int yy, int zz) : x(xx), y(yy), z(zz) {}
-	int x, y, z;
-	void Print(std::ostream& o) const { o << "(" << x <<", " << y << ", " << z << " )"; }
-};
-
-std::ostream& operator<<(std::ostream& o, const IPrintable& p)
-{
-	p.Print(o);
-	return o;
-}
 
 int main(int argc, char** argv)
 {
-	std::cout << "size of std::string: " << sizeof(std::string) << std::endl;
-	std::cout << "size of Person: " << sizeof(Person) << std::endl;
-	
 	Employee maciek("Maciek", 37, "Teacher");
-	Person misha("Misha", 31);
-	Triangle t{1, 3, 6};
 
-	std::vector<IPrintable*> printables;
-	printables.push_back(&maciek);
-	printables.push_back(&misha);
-	printables.push_back(&t);
-	
-	for(IPrintable* p : printables)
-		std::cout << "* " << *p << std::endl;
+	std::cout << "&maciek=" << &maciek << std::endl;
+
+	Person* p = &maciek;
+	DbEntry* d = &maciek;
+	std::cout << "p=" << p << std::endl;
+	std::cout << "d=" << d << std::endl;
+
+	// ?
+	Employee* e = static_cast<Employee*>(p);
+	std::cout << "e=" << e << std::endl;
+	e->Print(std::cout);
+
 }
  
