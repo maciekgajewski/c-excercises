31335eb4feb2871e46b3f721613d4719a8fb1366
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-01-18 18:04:06 +0100

    Meeting 1, final stage

--- course7/meeting01/meeting01.cc
+++ course7/meeting01/meeting01.cc
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <cstdint>
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello, planet!" << std::endl;
+	// initialization
+	int i = 7;
+	float f = i;
+
+	int* ip = &i;
+	float* fp = &f;
+
+	*ip = 66;
+	*fp = 66;
+
+	std::cout << "i=" << i
+		<< ", the size of i is: " << sizeof(i)
+		<< ", the address of i is: " << ip
+		<< std::endl;
+
+	std::cout << "f=" << f
+		<< ", the size of f is: " << sizeof(f)
+		<< ", the address of f is: " << fp
+		<< std::endl;
+
+	std::cout << "ip=" << ip
+		<< ", the size of ip is: " << sizeof(ip)
+		<< ", the address of ip is: " << &ip
+		<< std::endl;
+
 }
