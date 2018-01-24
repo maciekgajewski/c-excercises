4054c6cf8a180131bdcac4bb1d37c30644ce6034
Waleed Lotfy <waleed.lutfi90@gmail.com>
2018-01-24 22:20:47 +0100

    Fixing inconsistent naming cases

--- course7/homeworks/waleed/ls/main.cc
+++ course7/homeworks/waleed/ls/main.cc
@@ -4,10 +4,10 @@
 
 int main(int argc, char** argv)
 {
-	boost::filesystem::path CurrPath = boost::filesystem::current_path();
-	boost::filesystem::directory_iterator CurrDir(CurrPath);
+	boost::filesystem::path currPath = boost::filesystem::current_path();
+	boost::filesystem::directory_iterator currDir(currPath);
 
-	for (const auto& it : CurrDir)
+	for (const auto& it : currDir)
 	{
 		std::cout << it.path().filename() << std::endl;
 	}
