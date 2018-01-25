2b7cb1a310c7e18391210854c14b986a620f09dd
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-01-25 17:35:49 +0100

    reintrpret cast

--- course7/meeting02/meeting02.cc
+++ course7/meeting02/meeting02.cc
@@ -2,37 +2,27 @@
 #include <stdexcept>
 #include <string>
 
-int fib_call_counter = 0;
-
-int fib(int i)
+void print(int i)
 {
-	fib_call_counter++;
-	std::cout << "fib, i=" << i << ", &i=" << &i << std::endl;
-	std::cout << "fcc=" << fib_call_counter << ", &fcc=" << &fib_call_counter << std::endl;
-	if (i < 2)
-		return 1;
-	else
-		return fib(i-1) +
-			fib(i-2);
+	std::cout << "int: " << i << std::endl;
+}
+
+void print(float i)
+{
+	std::cout << "float: " << i << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-	const char* msg = "Not enough params";
-	if (argc < 2)
-		throw std::runtime_error(msg);
+	int i = 55;
+	auto pi = &i;
 
-	std::string arg1 = argv[1];
-	int i = std::stoi(arg1);
+	float f = 44;
+	auto pf = &f;
 
-	std::cout << "fib(" << i << ") = " << fib(i) << std::endl;
+	std::cout << "*pi = " << *pi << std::endl;
+	pi = reinterpret_cast<int*>(pf);
+	*pi = *pf;
 
-	std::cout << "&argc=" << &argc << std::endl;
-	std::cout << "&argv=" << &argv << std::endl;
-	std::cout << "&arg1=" << &arg1 << std::endl;
-	std::cout << "&i   =" << &i << std::endl;
-	std::cout << "msg  =" << reinterpret_cast<const void*>(msg) << std::endl;
-
-	std::cout << "&main = " << reinterpret_cast<const void*>(&main) << std::endl;
-	std::cout << "&fib  = " << reinterpret_cast<const void*>(&fib) << std::endl;
+	print(*pi);
 }
