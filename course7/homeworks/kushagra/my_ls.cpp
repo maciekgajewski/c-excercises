c814f01317f5fb3c34c24da81698214284d9061b
Kushagra Singhal <kushagrasinghal@optiver.com>
2018-01-31 18:05:46 +0100

    Remove Debug-gy code

--- course7/homeworks/kushagra/my_ls.cpp
+++ course7/homeworks/kushagra/my_ls.cpp
@@ -9,23 +9,14 @@ int main(int argc, char** argv)
 		full_path.swap(new_path) ;
     }
 	
-    try {
-        if (boost::filesystem::exists(full_path)) {
-            if (boost::filesystem::is_regular_file(full_path)) {
-                std::cout << full_path << std::endl;
-            }
-            else {
-                for (boost::filesystem::directory_entry& x : boost::filesystem::directory_iterator(full_path)) {
-                    std::cout << "\t" << x.path() << std::endl;
-                } 
-            } 
-        }
-        else {
+    if (boost::filesystem::exists(full_path)) {
+        if (boost::filesystem::is_regular_file(full_path)) {
             std::cout << full_path << std::endl;
         }
+        else {
+            for (boost::filesystem::directory_entry& x : boost::filesystem::directory_iterator(full_path)) {
+                std::cout << "\t" << x.path() << std::endl;
+            } 
+        } 
     }
-    catch (const boost::filesystem::filesystem_error& exception) {
-        std::cout << exception.what() << std::endl; 
-    }
-    return 0;
 }
