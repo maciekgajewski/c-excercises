669a8ef0d3592cfc587c4ec74cd67d64585be910
Kush CPP <cpp_kush@mail.fairfrog.me>
2018-02-02 00:30:21 +0100

    Advanced ls without long implementation

--- course7/homeworks/kushagra/hw2/ls_hw2.cpp
+++ course7/homeworks/kushagra/hw2/ls_hw2.cpp
@@ -7,16 +7,17 @@
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
-void print_dir_contents(fs::path full_path, std::string spaces) {
-    if (fs::is_regular_file(full_path)) {
-        std::cout << spaces << full_path.filename() << std::endl;
+
+void print_dir_contents(fs::path display_path, std::string spaces) {
+	if (fs::is_regular_file(display_path)) {
+		std::cout << spaces << display_path.filename().generic_string() << std::endl;
     }
     else {
-        for (fs::directory_entry& x : fs::directory_iterator(full_path)) {
-            std::cout << spaces << x.path().generic_string() << fs::path::preferred_separator
-                     << std::endl;
-            spaces += "\t";
-            print_dir_contents(x, spaces);
+		std::cout << spaces << display_path.filename().generic_string() <<
+			fs::path::preferred_separator << std::endl;
+		spaces += "\t";
+        for (fs::directory_entry& x : fs::directory_iterator(display_path)) {
+            print_dir_contents(x.path(), spaces);
         } 
     } 
 }
@@ -27,14 +28,16 @@ void print_dir_contents(fs::path full_path, std::string spaces) {
 
 void print_dir_contents(fs::path full_path) {
     if (fs::is_regular_file(full_path)) {
-        std::cout << "\t" << full_path.filename() << std::endl;
+        std::cout << full_path.generic_string() << std::endl;
     }
     else {
+		std::cout << full_path.generic_string() << fs::path::preferred_separator
+			 << std::endl;
         for (fs::directory_entry& x : fs::directory_iterator(full_path)) {
             if (fs::is_regular_file(x.path())) {
-                std::cout << "\t\t" << x.path().filename() << std::endl;
+                std::cout << "\t" << x.path().filename().string() << std::endl;
             } else {
-                std::cout << "\t" << x.path().generic_string() << fs::path::preferred_separator
+                std::cout << "\t" << x.path().filename().generic_string() << fs::path::preferred_separator
                     << std::endl;
             }
         } 
@@ -44,23 +47,48 @@ void print_dir_contents(fs::path full_path) {
 
 int main(int argc, char** argv)
 {
-    po::options_description desc("Display contents of a directory with ");
-    desc.add("path", -1);
-    desc.add_options()
-        ("path,p", value<std::string>()->default_value("."), "Path to display")
-        ("long,l", value<bool>()->default_value(false), "Display full details")
-        ("tee,t", value<bool>()->default_value(false), "Recursively display directory contents")
-        ("help,h", "Display help text");
-    po::variables_map vm;
-    std::string spaces = "\t";
-    std::string path_name = ".";
-	fs::path full_path(path_name);
+	std::string path_name = ".";
+	bool details = false, recursive = false;
 	
+    po::options_description desc("Display contents of a directory with ");
+    desc.add_options()
+        ("path_name,p", po::value<std::string>()->default_value("."), "Path to display")
+        ("long,l", po::bool_switch()->default_value(false)->implicit_value(true),
+			"Display full details")
+        ("tee,t", po::bool_switch()->default_value(false)->implicit_value(true),
+			"Recursively display directory contents")
+        ("help,h", "Display help text");
+	po::positional_options_description posOption;
+    posOption.add("path_name", -1);
+    po::variables_map vm;
+	po::store(po::command_line_parser(argc, argv).options(desc).positional(posOption).run(), vm);
+	po::notify(vm);
+
+	if(vm.count("path_name")) {
+		path_name = vm["path_name"].as<std::string>();
+		if (path_name.back() == '/') {
+			path_name.erase(path_name.length()-1);
+		}
+	}
+	if(vm.count("long")) {
+		details = vm["long"].as<bool>();
+	}
+	if(vm.count("tee")) {
+		recursive = vm["tee"].as<bool>();
+	}
+	
+	fs::path full_path(path_name);
     if (fs::exists(full_path)) {
-        if (details) {
-            print_dir_contents(full_path, spaces);
-        } else {
-            print_dir_contents(full_path, spaces);
-        }
+		if (fs::is_regular_file(full_path)) {
+			std::cout << full_path.generic_string() << std::endl;
+		}
+		else {
+			std::string spaces = "";
+			if (recursive) {
+				print_dir_contents(full_path, spaces);
+			} else {
+			   print_dir_contents(full_path);
+			}
+		}
     }
 }
