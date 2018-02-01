153f8495f22f2bddf6d00d9e08c4b0faf11bd0a4
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-02-01 17:10:55 +0100

    the big 6

--- course7/meeting03/meeting03.cc
+++ course7/meeting03/meeting03.cc
@@ -3,8 +3,39 @@
 
 struct Complex
 {
+	// The big 6
 	Complex() { std::cout << "Complex this=" << this << " default-created" << std::endl; }
 	~Complex() { std::cout << "Complex this=" << this << " destroyed" << std::endl; }
+	Complex(const Complex& src) {
+		std::cout << "Complex this=" << this << " constructed as a copy of " << &src << std::endl;
+		r = src.r; i = src.i;
+	}
+	Complex(Complex&& src) {
+		std::cout << "Complex this=" << this << " move-constructed from " << &src << std::endl;
+		r = src.r; i = src.i;
+	}
+
+	Complex& operator =(const Complex& src)
+	{
+		std::cout << "Complex this=" << this << " copy-assigned " << &src << std::endl;
+		r = src.r; i = src.i;
+		return *this;
+	}
+
+	Complex& operator =(Complex&& src)
+	{
+		std::cout << "Complex this=" << this << " move-assigned " << &src << std::endl;
+		r = src.r; i = src.i;
+		return *this;
+	}
+
+	// Option, user defined
+	Complex(double i, double r) 
+	{
+		std::cout << "Complex this=" << this << " created from r,i" << std::endl;
+		this->i = i; this->r = r;
+	}
+
 	void print();
 
 	double i = 0.0;
@@ -17,33 +48,25 @@ struct PairOfComplex
 	Complex second;
 };
 
-void fun1(int x)
+Complex make_complex(double r)
 {
-	if (x > 2)
-		throw std::runtime_error("Boo!");
+	return Complex(r, 0.0);
 }
-
-void fun2(int x)
-{
-	Complex c;
-	c.i = x;
-
-	fun1(x);
-
-	c.print();
-}
-
 
 int main(int argc, char** argv)
 {
-	try
-	{
-		fun2(argc);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "Error: " << e.what() << std::endl;
-	}
+	Complex a(3.3, 4.4);
+	Complex b = a;
+	Complex c(b);
+	Complex d = make_complex(7);
+
+	c = a; // assignment
+	d = make_complex(88);
+
+	a.print();
+	b.print();
+	c.print();
+	d.print();
 }
 
 void Complex::print()
