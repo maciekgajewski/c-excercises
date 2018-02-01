244562258e7727c036b4bdf17c315f157077999c
Patrick Kostjens <patrickkostjens@gmail.com>
2018-02-01 07:30:34 +0100

    Incorporate feedback: don't construct struct using new

--- course7/homeworks/patrickkostjens/ls/ls.cc
+++ course7/homeworks/patrickkostjens/ls/ls.cc
@@ -11,9 +11,9 @@ struct node_result
 	size_t size;
 };
 
-node_result* print_contents(const fs::path& path, bool extensive, bool tree, int level = 0)
+node_result print_contents(const fs::path& path, bool extensive, bool tree, int level = 0)
 {
-	node_result* result = new node_result();
+	node_result result;
 	std::string name = path.filename().string();
 	if (!fs::is_symlink(path))
 	{
@@ -23,14 +23,14 @@ node_result* print_contents(const fs::path& path, bool extensive, bool tree, int
 				for (const auto& it : fs::directory_iterator(path))
 				{
 					auto child_result = print_contents(it, extensive, tree, level + 1);
-					if (extensive) result->size += child_result->size;
-					if (tree) result->output += child_result->output;
+					if (extensive) result.size += child_result.size;
+					if (tree) result.output += child_result.output;
 			}
 		}
-		else if (extensive) result->size = fs::file_size(path);
+		else if (extensive) result.size = fs::file_size(path);
 	}
 
-	result->output = std::string(level * 2, ' ') + name + (extensive ? ": " + std::to_string(result->size) : "") + "\n" + result->output;
+	result.output = std::string(level * 2, ' ') + name + (extensive ? ": " + std::to_string(result.size) : "") + "\n" + result.output;
 	return result;
 }
 
@@ -61,5 +61,5 @@ int main(int argc, char** argv)
 
 	const fs::path path(directory);
 	for (const auto& it : fs::directory_iterator(path))
-		std::cout << print_contents(it, extensive, tree)->output;
+		std::cout << print_contents(it, extensive, tree).output;
 }
