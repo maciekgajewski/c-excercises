3c54764f465cb8128c994a9bf735c412dfc30209
Mart Slot <martslot@optiver.com>
2018-02-03 16:24:07 +0100

    Add boost program_options, with help screen implementation

--- course7/homeworks/mart/ls/ls.cc
+++ course7/homeworks/mart/ls/ls.cc
@@ -1,10 +1,25 @@
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 #include <iostream>
 
 namespace bf = boost::filesystem;
+namespace bpo = boost::program_options;
 
 int main(int argc, char** argv)
 {
+	bpo::options_description description{"Options"};
+	description.add_options()
+		("help,h", "Show help screen");
+
+	bpo::variables_map variablesMap;
+	bpo::store(bpo::parse_command_line(argc, argv, description), variablesMap);
+	bpo::notify(variablesMap);
+
+	if (variablesMap.count("help")) {
+		std::cout << description << std::endl;
+		return 1;
+	}
+
 	auto current_path = bf::current_path();
 
 	for (auto& entry : bf::directory_iterator(current_path))
