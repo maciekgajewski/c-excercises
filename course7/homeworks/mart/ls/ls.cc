f9ca94d682b0a390e53d3b78e9781d5446f80e02
Mart Slot <martslot@optiver.com>
2018-02-03 16:03:03 +0100

    Alias boost filesystem as bf for brevity

--- course7/homeworks/mart/ls/ls.cc
+++ course7/homeworks/mart/ls/ls.cc
@@ -1,15 +1,17 @@
 #include <boost/filesystem.hpp>
 #include <iostream>
 
+namespace bf = boost::filesystem;
+
 int main(int argc, char** argv)
 {
-	auto current_path = boost::filesystem::current_path();
+	auto current_path = bf::current_path();
 
-	for (auto& entry : boost::filesystem::directory_iterator(current_path))
+	for (auto& entry : bf::directory_iterator(current_path))
 	{
 		auto filename = entry.path().filename();
 		std::cout << filename.string();
-		if (boost::filesystem::is_directory(entry.path()))
+		if (bf::is_directory(entry.path()))
 			std::cout << "/";
 		std::cout << std::endl;
 	}
