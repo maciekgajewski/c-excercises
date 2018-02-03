e8fbf3fea2a347a5de0427ee879f847aaf6c5725
Mart Slot <martslot@optiver.com>
2018-02-03 16:00:47 +0100

    Add / after directory names

--- course7/homeworks/mart/ls/ls.cc
+++ course7/homeworks/mart/ls/ls.cc
@@ -8,6 +8,9 @@ int main(int argc, char** argv)
 	for (auto& entry : boost::filesystem::directory_iterator(current_path))
 	{
 		auto filename = entry.path().filename();
-		std::cout << filename.string() << std::endl;
+		std::cout << filename.string();
+		if (boost::filesystem::is_directory(entry.path()))
+			std::cout << "/";
+		std::cout << std::endl;
 	}
 }
