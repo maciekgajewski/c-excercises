74254e52f00140c823d22e55d3087bbf927bd09d
Mart Slot <martslot@optiver.com>
2018-02-03 16:54:48 +0100

    Extract directory printing code to a new function

--- course7/homeworks/mart/ls/ls.cc
+++ course7/homeworks/mart/ls/ls.cc
@@ -5,18 +5,30 @@
 namespace bf = boost::filesystem;
 namespace bpo = boost::program_options;
 
+void listDirectory(const std::string& directory, bool extraInfo, bool recursive)
+{
+	for (auto& entry : bf::directory_iterator(directory))
+	{
+		auto filename = entry.path().filename();
+		std::cout << filename.string();
+		if (bf::is_directory(entry.path()))
+			std::cout << "/";
+		std::cout << std::endl;
+	}
+}
+
 int main(int argc, char** argv)
 {
 	bool help = false;
 	bool extraInfo = false;
-	bool tree = false;
-	std::string directory;
+	bool recursive = false;
+	std::string directory = bf::current_path().string();
 
 	bpo::options_description options{"Options"};
 	options.add_options()
 		("help,h", bpo::bool_switch(&help), "Show help screen")
 		("long,l", bpo::bool_switch(&extraInfo), "Display extra information on each file/directory")
-		("tree,t", bpo::bool_switch(&tree), "Recursively descend into each subdirectory")
+		("tree,t", bpo::bool_switch(&recursive), "Recursively descend into each subdirectory")
 		("directory,d", bpo::value<std::string>(&directory), "Working directory");
 
 	boost::program_options::positional_options_description positionalOptions;
@@ -31,14 +43,5 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	auto current_path = bf::current_path();
-
-	for (auto& entry : bf::directory_iterator(current_path))
-	{
-		auto filename = entry.path().filename();
-		std::cout << filename.string();
-		if (bf::is_directory(entry.path()))
-			std::cout << "/";
-		std::cout << std::endl;
-	}
+	listDirectory(directory, extraInfo, recursive);
 }
