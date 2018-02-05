8fceafabda863d7e3a00222d7de467e508bca548
Mart Slot <martslot@optiver.com>
2018-02-05 22:39:57 +0100

    Write simple calculator class to execute input lines

--- course7/homeworks/mart/complex/complex.cc
+++ course7/homeworks/mart/complex/complex.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stack>
 
 struct Complex
 {
@@ -58,9 +59,92 @@ std::ostream& operator<<(std::ostream& stream, const Complex& complex)
 	return stream;
 }
 
+class Calculator
+{
+public:
+	Calculator(const std::string& l) : line(l) {}
+
+	void execute()
+	{
+		std::size_t i = 0;
+		while (i < line.length())
+		{
+			switch (line[i])
+			{
+			case '+':
+				add();
+				break;
+			case '-':
+				subtract();
+				break;
+			case '*':
+				multiply();
+				break;
+			case '/':
+				divide();
+				break;
+			default:
+				i = parseComplex(i);
+			}
+			i++;
+		}
+	}
+
+private:
+	std::size_t parseComplex(std::size_t& startIndex)
+	{
+		return startIndex;
+	}
+
+	std::pair<Complex, Complex> popPair()
+	{
+		if (stack.size() <= 2)
+			throw std::runtime_error("Stack does not contain 2 items");
+		std::pair<Complex, Complex> pair;
+		pair.first = stack.top();
+		stack.pop();
+		pair.second = stack.top();
+		stack.pop();
+		return pair;
+	}
+
+	void add()
+	{
+		auto pair = popPair();
+		stack.push(pair.first + pair.second);
+	}
+
+	void subtract()
+	{
+		auto pair = popPair();
+		stack.push(pair.first - pair.second);
+	}
+
+	void multiply()
+	{
+		auto pair = popPair();
+		stack.push(pair.first * pair.second);
+	}
+
+	void divide()
+	{
+		auto pair = popPair();
+		stack.push(pair.first / pair.second);
+	}
+
+private:
+	std::string line;
+	std::stack<Complex> stack;
+};
+
 int main(/*int argc, char** argv*/)
 {
 	Complex d = {1.1, 4.4};
 	Complex b(7.7);
 	std::cout << b << std::endl;
+
+	for (std::string line; std::getline(std::cin, line); ) {
+		Calculator c(line);
+		c.execute();
+	}
 }
