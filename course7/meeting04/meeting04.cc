afcc263e9d733f1d2e7182aeaf314e50e83fdeb1
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-02-08 16:40:00 +0100

    work on file interface

--- course7/meeting04/meeting04.cc
+++ course7/meeting04/meeting04.cc
@@ -1,6 +1,44 @@
-#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <stdexcept>
+// - read string
+// - write string
+// - check size
+class File
+{
+public:
+
+	explicit File(const std::string& path /*, mode*/)
+	{
+		std::cout << "Opening file at: " << path << std::endl;
+		// TODO implement, open the file
+	}
+
+	void write(const std::string& s) /* no const */
+	{
+		std::cout << "Writing string: " << s << std::endl;
+		// TODO implement
+	}
+
+	// TODO read
+	// TODO getSize
+
+	~File()
+	{
+		// TODO
+		std::cout << "Closing file" << std::endl;
+	}
+
+private:
+
+	int mFd;
+};
 
 int main(int argc, char** argv)
 {
-	return ::system("banner hello meeting four");
+	if (argc < 2)
+		throw std::runtime_error("Arg required");
+	
+	File f(argv[1]);
+	f.write("Hello!");
 }
