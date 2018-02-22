0fbc2c9e586a4a6a68998454c0add0ddf47d0617
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-02-22 17:18:44 +0100

    Common memory errors

--- course7/meeting06/meeting06.cc
+++ course7/meeting06/meeting06.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <memory>
 
 struct Fred
 {
@@ -6,9 +8,44 @@ struct Fred
 	~Fred() { std::cout << "Fred destroyed, this=" << this << std::endl; }
 };
 
+struct FredsHouse
+{
+	FredsHouse(const std::string& f)
+		: fridge(f) { std::cout << "FredsHouse created, this=" << this << std::endl; }
+	~FredsHouse() { std::cout << "FredsHouse destroyed, this=" << this << std::endl; }
+
+	void state()
+	{
+		if (visitor)
+			std::cout << "Fred this= " << visitor.get() << " is in the house" << std::endl;
+		else
+			std::cout << "No visitors" << std::endl;
+	}
+
+	std::string fridge;
+	Fred f1;
+	Fred f2;
+	std::unique_ptr<Fred> visitor;
+
+};
+
+std::unique_ptr<FredsHouse> houseFactory(const std::string& f)
+{
+	std::unique_ptr<FredsHouse> p = std::make_unique<FredsHouse>(f);
+	return p;
+}
 
 int main(int argc, char** argv)
 {
-	Fred f;
-	std::cout << "hello, world!" << std::endl;
+	FredsHouse* p = nullptr;
+	for (int i = 0; i < 4; i++)
+	{
+	 	p = new FredsHouse("wine" + std::to_string(i));
+		//
+		delete p;
+	}
+
+		// x
+	std::cout << "hello, world!, in the fridge=" << p->fridge << std::endl;
+	delete p;
 }
