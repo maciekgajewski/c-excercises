001d254eb1eadc6bb1c60c3077bc5a81fbdcde64
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-03-02 10:13:50 +0100

    Introducing std::list

--- course7/meeting07/meeting07.cc
+++ course7/meeting07/meeting07.cc
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
+#include <array>
 
 struct Fred
 {
 	Fred() { std::cout << "Fred created, this=" << this << std::endl; }
 	explicit Fred(const std::string& n) : name(n) { std::cout << "Fred created, this=" << this << " : " << name << std::endl; }
 
-	Fred(const Fred& other) : name(other.name) { std::cout << "Fred copied, this=" << this << ", other=" << &other << " : " << other.name << std::endl; }
-	Fred(Fred&& other) noexcept : name(std::move(other.name)) { std::cout << "Fred moved, this=" << this << ", other=" << &other << " : " << name << std::endl; }
+	Fred(const Fred& other) = delete;
+	Fred(Fred&& other) = delete;
+
+//	Fred(const Fred& other) : name(other.name) { std::cout << "Fred copied, this=" << this << ", other=" << &other << " : " << other.name << std::endl; }
+	//Fred(Fred&& other) noexcept : name(std::move(other.name)) { std::cout << "Fred moved, this=" << this << ", other=" << &other << " : " << name << std::endl; }
 
 	~Fred() { std::cout << "Fred destroyed, this=" << this << " : " << name << std::endl; }
 
@@ -18,30 +23,25 @@ struct Fred
 
 int main(int argc, char** argv)
 {
-	std::vector<Fred> freds;
-	freds.reserve(argc-1);
+	std::list<Fred> freds;
+	//freds.reserve(argc-1);
 	for (int i = 1; i < argc; i++)
 	{
-		Fred anton(argv[i]);
-		freds.push_back(anton);
+		freds.emplace_back(argv[i]);
+		//freds.push_back(Fred(argv[i]));
 	}
 	
 	std::cout << "Container contains " << freds.size() << " elements" << std::endl;
-	std::cout << "Container's capacity is " << freds.capacity() << std::endl;
+	//std::cout << "Container's capacity is " << freds.capacity() << std::endl;
 
 	for(const Fred& f : freds)
 		std::cout << f.name << std::endl;
 
-	while(freds.size() > 1)
-		freds.pop_back();
+	//auto dow = std::make_array("mon", "tue", "wed");
+	std::array<std::string, 3> dow({"mon", "tue", "wed"});
 
-	std::cout << "Container contains " << freds.size() << " elements" << std::endl;
-	std::cout << "Container's capacity is " << freds.capacity() << std::endl;
-
-	freds.shrink_to_fit();
-
-	std::cout << "Container contains " << freds.size() << " elements" << std::endl;
-	std::cout << "Container's capacity is " << freds.capacity() << std::endl;
+	for(const auto& d : dow)
+		std::cout << d << std::endl;
 
 }
 
