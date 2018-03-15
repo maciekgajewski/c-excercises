c8185c33aa6bab80d7d8003560915b2dd553c0e3
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-03-15 17:05:37 +0100

    sets

--- course7/meeting09/meeting09.cc
+++ course7/meeting09/meeting09.cc
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <unordered_set>
 
 int main(int argc, char** argv)
 {
 	std::string greeting = "Hello, world!";
-	std::set<char> chars(greeting.begin(), greeting.end());
+	std::unordered_set<char> chars(greeting.begin(), greeting.end());
 
 	for(char c : chars)
 		std::cout << c;
 	std::cout << std::endl;
+
+	// searching!
+	if (argc > 1)
+	{
+		char search = argv[1][0];
+		auto it = chars.find(search);
+		if (it == chars.end())
+			std::cout << search << " not found" << std::endl;
+		else
+		{
+			std::cout << search << " found" << std::endl;
+			chars.erase(search);
+		}
+
+		for(char c : chars)
+			std::cout << c;
+		std::cout << std::endl;
+	}
+
 }
 
 
