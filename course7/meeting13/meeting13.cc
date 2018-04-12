b96b454b8378ca22ab5d999a5f0a90dc9af65881
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-04-12 16:30:07 +0200

    count_if

--- course7/meeting13/meeting13.cc
+++ course7/meeting13/meeting13.cc
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <cstring>
+
+bool is_waldo(const char* cstr)
+{
+	return std::strcmp(cstr, "Waldo") == 0;
+}
 
 int main(int argc, char** argv)
 {
-	std::vector<std::string> params(argv, argv+argc);
-
-	int num_waldos = std::count(params.begin(), params.end(), "Waldo");
+	int num_waldos = std::count_if(argv, argv+argc, is_waldo);
 	std::cout << "Waldos found: " << num_waldos << std::endl;
 }
 
