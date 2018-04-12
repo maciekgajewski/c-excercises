5c22126abbb6913fa0fa77e6955895e12d4034c7
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-04-12 17:35:40 +0200

    lambdas

--- course7/meeting13/meeting13.cc
+++ course7/meeting13/meeting13.cc
@@ -2,15 +2,42 @@
 #include <algorithm>
 #include <cstring>
 
-bool is_waldo(const char* cstr)
+struct is_waldo
 {
-	return std::strcmp(cstr, "Waldo") == 0;
-}
+	bool operator() (const char* cstr) const
+	{
+		_calls++;
+		return std::strcmp(cstr, _search) == 0;
+	}
+	int& _calls;
+	const char* _search;
+};
 
 int main(int argc, char** argv)
 {
-	int num_waldos = std::count_if(argv, argv+argc, is_waldo);
+	if (argc < 3)
+	{
+		std::cerr << "Usage: search WHAT [WHERE...]" << std::endl;
+		return 1;
+	}
+
+	const char* the_search = argv[1];
+	int calls = 0;
+
+	int num_waldos = std::count_if(
+		argv+2, argv+argc,
+		[&, cc=0](const char* cstr) mutable {
+			cc++;
+			calls++;
+			if (cc < 3)
+				return std::strcmp(cstr, the_search) == 0;
+			else
+				return false;
+	});
+	
+	
 	std::cout << "Waldos found: " << num_waldos << std::endl;
+	std::cout << "Calls: " << calls << std::endl;
 }
 
 
