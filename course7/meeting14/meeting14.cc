3772c8a199628499589fe45cec44db4036f8cb57
Maciej Gajewski <maciejgajewski@Optiver.com>
2018-04-19 17:55:19 +0200

    dynamic_cast

--- course7/meeting14/meeting14.cc
+++ course7/meeting14/meeting14.cc
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <string>
-
-struct ID 
-{
-	std::int64_t id;
-};
+#include <memory>
 
 struct IStreamable
 {
 	virtual void ToStream(std::ostream& s) const = 0;
+	virtual ~IStreamable() {}
 };
 
 struct Person : IStreamable
@@ -20,10 +17,10 @@ struct Person : IStreamable
 	virtual void ToStream(std::ostream& s) const {  s << "Person{name: " << name << ", height: " <<height << "m}"; }
 };
 
-struct Employee : ID, Person
+struct Employee : Person
 {
 	std::string role;
-	void ToStream(std::ostream& s) const override {  s << "Employee{name: " << name << ", height: " << height << "m}"; }
+	void ToStream(std::ostream& s) const override {  s << "Employee{name: " << name << ", height: " << height << "m, role: " << role << "}"; }
 };
 
 std::ostream& operator<<(std::ostream& s, const IStreamable& p)
@@ -32,37 +29,27 @@ std::ostream& operator<<(std::ostream& s, const IStreamable& p)
 	return s;
 }
 
-void PrintId(const ID& id)
+std::unique_ptr<Person> MakePerson(const std::string& type, const std::string& name)
 {
-	std::cout << "id=" << id.id << ", &id=" << &id << std::endl;
+	if (type == "employee")
+	{
+		auto e = std::make_unique<Employee>();
+		e->name = name;
+		e->role = "Developerxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+		return e;
+	}
+	else
+	{
+		auto p = std::make_unique<Person>();
+		p->name = name;
+		return p;
+	}
 }
 
 int main(int argc, char** argv)
 {
-	Person maciek;
-	maciek.name = "Maciek";
-	maciek.height = 2.00;
-	std::cout << maciek << std::endl;
+	std::unique_ptr<Person> p = MakePerson(argv[1], argv[2]);
+	std::cout << "Created: " << *p << std::endl;
 
-	Employee fred;
-	fred.role = "Developer";
-	fred.name = "Fred";
-	fred.height = 1.68;
-	fred.id = 666;
-
-	std::cout << "sizeof string: " << sizeof(std::string) << std::endl;
-	std::cout << "sizeof Person: " << sizeof(Person) << std::endl;
-	std::cout << "sizeof Employee: " << sizeof(Employee) << std::endl;
-
-	Person& fredsEvilPersonality = reinterpret_cast<Person&>(fred); // Evil!!!
-	Person& fredAsPerson = static_cast<Person&>(fred);
-	ID& fredAsId = static_cast<ID&>(fred);
-
-	std::cout << "&fred=" << &fred << std::endl;
-	std::cout << "&fredAsPerson=" << &fredAsPerson << std::endl;
-	std::cout << "&fredAsId=" << &fredAsId << std::endl;
-	std::cout << "fredsEvilPersonality=" << &fredsEvilPersonality << std::endl;
-
-	std::cout << fred << std::endl;
-	PrintId(fred);
+	Employee& e = dynamic_cast<Employee&>(*p);
 }
